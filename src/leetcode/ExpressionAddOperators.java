4b9465d5d457164a69edb97b1e2aa0aafed8b086
Denny <duqiang@10.82.16.240>
2018-07-15 19:45:37 -0700

    add more comments

--- src/leetcode/ExpressionAddOperators.java
+++ src/leetcode/ExpressionAddOperators.java
@@ -25,7 +25,9 @@ public class ExpressionAddOperators {
      "00", 0 -> ["0+0", "0-0", "0*0"]
      "3456237490", 9191 -> []
 
-     time : 不知道
+     4 represents  how many operators we can place in this position, + - * and space, space means we can 
+     combine them together
+     time : O(n * 4 ^(n-1)) 
      space : O(n)
 
      
@@ -41,13 +43,22 @@ public class ExpressionAddOperators {
         return res;
     }
 
+    // path: str needed to be in reset set
+    // num is the original number, string
+    // target is the target value
+    //pos is current index when recursive functionality happens, 
+    //val 
     private void helper(List<String> res, String path, String num, int target, int pos, long val, long pre) {
+        // if we reached the end of num
+        // and we find val changed to be the same as target chich means we reached the target
         if (pos == num.length()) {
             if (target == val) {
                 res.add(path);
                 return;
             }
         }
+        // this is permutation templates, for loop visit each position which we can 
+        // use for + - * or space to form integer or binary expressions
         for (int i = pos; i < num.length(); i++) {
             if (i != pos && num.charAt(pos) == '0') break;
             long cur = Long.parseLong(num.substring(pos, i + 1));
