e1a60e8e10c861f15a8f9a428582234ce3ecba64
Denny <duqiang@192.168.0.102>
2018-07-28 14:21:22 -0700

    add more comments

--- src/leetcode/InorderSuccessorinBST.java
+++ src/leetcode/InorderSuccessorinBST.java
@@ -21,14 +21,26 @@ public class InorderSuccessorinBST {
      * @param p
      * @return
      */
-    // inOrder 
+    // how we think about this question:
+    // we visit the tree from left ->parent-> right this way, so if we visit the BST from root,
+    // we have to visit its left branch, so for most use cases we have to go thorugh a number of 
+    // nodes which are smaller than P, then we mark the node to its right child we can be more closer 
+    // to P, 
+    //so finally we will meet one node which is bigger than P, then its successor should be a leaf node,
+    //and our loop should be exit
+    //edge case: thinking about this way if we visit the tree and put into a list, so we just need to find P and 
+    //return its next. so it maybe last one, for this case, we just return null, the previous loop still works
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         TreeNode res = null;
+        // it must be its right branch
         while (root != null) {
             //means they are 
             if (root.val <= p.val) {
                 root = root.right;
             } else {
+                //current node is bigger than p which means 
+                // so it must be its right branch, the successor is its first left at least
+                //child of its right branch
                 res = root;
                 root = root.left;
             }
@@ -36,6 +48,7 @@ public class InorderSuccessorinBST {
         return res;
     }
 
+    // this is the same as previous but with recursive
     public TreeNode successor(TreeNode root, TreeNode p) {
         if (root == null) return null;
         if (root.val <= p.val) {
