272dd7c1231ea9a4f4d2da176e79a85164401b7a
Denny <duqiang@192.168.0.102>
2018-07-29 00:11:59 -0700

    more comments

--- src/leetcode/InsertionSortList.java
+++ src/leetcode/InsertionSortList.java
@@ -38,19 +38,70 @@ Output: -1->0->3->4->5
         ListNode cur = head;
         ListNode temp = null, prev = null;
         while (cur != null && cur.next != null) {
+            
             if (cur.val <= cur.next.val) {
+                //move to next
                 cur = cur.next;
             } else {
+             // cur point to next also
+                // dummy->head->-1->0->3->2-5->7
+                //        cur
+                // temp--> 3, cur->0
                 temp = cur.next;
+                // dummy->head->-1->0->2->5->7
                 cur.next = temp.next;
+                // dummy->head->2->5->7
+                // prev
                 prev = dummy;
+                // if prev next is lower than 3 which means we find the elment which is bigger then temp.
+                //finally pre will point to 2
+                // pre-->2
                 while (prev.next.val <= temp.val) {
                     prev = prev.next;
                 }
+                // temp is 3, so 3-> 5->7
                 temp.next = prev.next;
+                // pre is 2, so 2->3->5->7
                 prev.next = temp;
             }
         }
         return dummy.next;
+    }
+    
+    
+    public ListNode insertionSortList2(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        //record node before insertNode
+        ListNode preNode = head;
+        //record node need to be inserted
+        ListNode insertNode = head.next;
+        
+        while(insertNode != null){
+            //store next insert node
+            ListNode nextInsert = insertNode.next;
+            //insert before head
+            if(insertNode.val <= head.val){
+                preNode.next = insertNode.next;
+                insertNode.next = head;
+                head = insertNode;
+            } else if (insertNode.val >= preNode.val){    //insert after tail
+                preNode = preNode.next;
+            } else {                                      //insert between head and tail
+                ListNode compareNode = head;
+                //start from the node after head, find insert position
+                while(compareNode.next.val < insertNode.val)   {
+                    compareNode = compareNode.next;
+                }
+                //insert
+                preNode.next = insertNode.next;
+                insertNode.next = compareNode.next;
+                compareNode.next = insertNode;
+            }
+            //get next insert node
+            insertNode = nextInsert;
+        }
+        return head;
     }
 }
