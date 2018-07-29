ad7f5a2d124f047c435028ed96aa08434c09b6b1
Denny <duqiang@192.168.0.102>
2018-07-28 20:17:18 -0700

    add more comments and solutions

--- src/leetcode/InsertInterval.java
+++ src/leetcode/InsertInterval.java
@@ -12,7 +12,24 @@ import java.util.List;
  * Description : 57. Insert Interval
  */
 public class InsertInterval {
+    
+    
     /**
+     * 57. Insert Interval
+     * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
      * Example 1:
      Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
 
@@ -27,22 +44,66 @@ public class InsertInterval {
      * @param intervals
      * @param newInterval
      * @return
+     * Facebook interview followup: how to improve if called multiple times
+     *  so we can try to use TreeMap.subTree() subMap(from, false, to, false);
      */
-    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        if (newInterval == null) return intervals;
+    public List<Interval> insert(List<Interval> ins, Interval newIn) {
         List<Interval> res = new ArrayList<>();
-        int i = 0;
-        while (i < intervals.size() && intervals.get(i).end < newInterval.start) {
-            res.add(intervals.get(i++));
+        //edge case
+        if (ins == null || newIn == null) {
+            return res;
         }
-        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
-            newInterval.start = Math.min(newInterval.start, intervals.get(i).start);
-            newInterval.end = Math.max(newInterval.end, intervals.get(i).end);
+        Interval newInv = newIn;
+        int i = 0, len = ins.size();
+        //first we process the interval which is before the newIn
+        while (i < len && newInv.start > ins.get(i).end ) {
+            res.add(ins.get(i++));
         }
-        res.add(newInterval);
-        while (i < intervals.size()) {
-            res.add(intervals.get(i));
+        // a---------------b
+        //    c----------------d 
+        // to make sure above two segment have overlap, it has to satisfy: 
+        // d >= a && c <= b
+        // here process the overlap ones, ins.get(i).end > newIn.start && newIn.end >= ins.get(i).start
+        //newsIn.start <= ins.get(i).end
+        while (i < len && newInv.end >= ins.get(i).start) {
+            newInv.start = Math.min(ins.get(i).start, newInv.start);
+            newInv.end = Math.max(ins.get(i).end, newInv.end);
+            i++;
+        }
+     // add the union of intervals we got
+        res.add(newInv);
+        
+        //add the rest intervals
+        while (i < len) {
+            res.add(ins.get(i++));
         }
         return res;
+    }
+    // simpler way of solutions
+    public List<Interval> insert2(List<Interval> intervals, Interval newInterval) {
+        List<Interval> result = new ArrayList<Interval>();
+        //edge case
+        if (intervals == null || newInterval == null) {
+            return result;
+        }
+  
+        for(Interval interval: intervals){
+            // here is a good example how can we write if /else in complicated situations
+            // new start bigger than interval in original list, so we just add it
+            if(interval.end < newInterval.start){
+                result.add(interval);
+            // here means internal end > newIn.start && interval.start > newIn.end
+            // so means interval start bigger then newIn.end, should be like after merge done
+            }else if(interval.start > newInterval.end){
+                result.add(newInterval);
+                // we use interval to replace newInterval
+                newInterval = interval;
+            }else if(interval.end >= newInterval.start || interval.start <= newInterval.end){
+                newInterval = new Interval(Math.min(interval.start, newInterval.start), Math.max(newInterval.end, interval.end));
+            }
+        }
+  
+        result.add(newInterval); 
+        return result;
     }
 }
--- src/leetcode/InsertionSortList.java
+++ src/leetcode/InsertionSortList.java
@@ -10,6 +10,21 @@ package leetcode;
  */
 public class InsertionSortList {
     /**
+     * 
+     * Algorithm of Insertion Sort:
+
+Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list.
+At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there.
+It repeats until no input elements remain.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
      * time : O(n^2)
      * space : O(1)
      *
