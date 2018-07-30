abcff7d75b8a03b35c304ab740cd13005b77cf71
Denny <duqiang@192.168.0.102>
2018-07-29 17:13:38 -0700

    add more solutions

--- src/leetcode/IntegertoEnglishWords.java
+++ src/leetcode/IntegertoEnglishWords.java
@@ -49,4 +49,27 @@ public class IntegertoEnglishWords {
             return less20[num / 100] + " Hundred " + helper(num % 100);
         }
     }
+    
+    
+    // another easy understand solutions
+    private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    
+    public String numberToWords2(int num) {
+        if (num == 0) return "Zero";
+        return helper2(num); 
+    }
+    
+    private String helper2(int num) {
+        String result = new String();
+        if (num < 10) result = belowTen[num];
+        else if (num < 20) result = belowTwenty[num -10];
+        else if (num < 100) result = belowHundred[num/10] + " " + helper2(num % 10);
+        else if (num < 1000) result = helper2(num/100) + " Hundred " +  helper2(num % 100);
+        else if (num < 1000000) result = helper2(num/1000) + " Thousand " +  helper2(num % 1000);
+        else if (num < 1000000000) result = helper2(num/1000000) + " Million " +  helper2(num % 1000000);
+        else result = helper2(num/1000000000) + " Billion " + helper2(num % 1000000000);
+        return result.trim();
+    }
 }
