35ff6eb47645bb1be0b105531a594a4ddbd8fcf2
Denny <duqiang@192.168.0.100>
2018-08-02 20:04:23 -0700

    more comments

--- src/leetcode/LargestBSTSubtree.java
+++ src/leetcode/LargestBSTSubtree.java
@@ -42,13 +42,16 @@ Can you figure out ways to solve it with O(n) time complexity?
      7 : 1,7,7
      15 : -1,0,0
 
-     time : O(n)
+     time : O(n) because we go through the whole tree
      space : O(n)
 
      */
 
     int res = 0;
 
+    // key points:
+    //a. post order, mainly help to collect child info then parent info
+    //b. BST critirial, how can we use lower and upper to achieve this.
     public int largestBSTSubtree(TreeNode root) {
         if (root == null) return 0;
         helper(root);
@@ -57,15 +60,24 @@ Can you figure out ways to solve it with O(n) time complexity?
 
     private SearchNode helper(TreeNode root) {
         if (root == null) {
+            // this means the node will not be qualified, this node will be ignored, so 
+            //for recursive function that requires returned value, we should 
+            // consider return the node which will not be considered
             return new SearchNode(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
         }
         SearchNode left = helper(root.left);
         SearchNode right = helper(root.right);
-        if (left.size == -1 || right.size == -1 || root.val <= left.upper || root.val >= right.lower) {
+        // means the left child, right child has no more other nodes 
+        if (left.size == -1 || right.size == -1 
+                // this means root smaller than left child max or right child lower which not satisfy BST
+                || root.val <= left.upper || root.val >= right.lower) {
             return new SearchNode(-1, 0, 0);
         }
+        // size is the root nodes #
         int size = left.size + right.size + 1;
+        // we update the nodes result
         res = Math.max(size, res);
+        // return the node stands for the root node. 
         return new SearchNode(size, Math.min(left.lower, root.val), Math.max(right.upper, root.val));
     }
 
@@ -75,8 +87,11 @@ Can you figure out ways to solve it with O(n) time complexity?
         int upper;
 
         SearchNode(int size, int lower, int upper) {
+            // nodes num
             this.size = size;
+            // in this tree, the min value of each node's val
             this.lower = lower;
+            // the max value in the tree
             this.upper = upper;
         }
     }
