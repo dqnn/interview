9dc120cbac6840edb9aae66eb5cd9b7d254f56b5
Denny <duqiang@172.31.98.206>
2018-08-20 01:47:38 -0700

    more coments

--- src/leetcode/MajorityElementII.java
+++ src/leetcode/MajorityElementII.java
@@ -1,14 +1,18 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Project Name : Leetcode
  * Package Name : leetcode
  * File Name : MajorityElementII
  * Creator : duqiang
- * Date : Nov, 2017
+ * Date : Aug, 2018
  * Description : 229. Majority Element II
  */
 public class MajorityElementII {
@@ -22,6 +26,29 @@ public class MajorityElementII {
      * @param nums
      * @return
      */
+    
+    //using map and set to store the count and avoid duplicate integer
+    public List<Integer> majorityElement2(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length < 1) {
+            return res;
+        }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            int cnt = map.getOrDefault(num, 0) + 1;
+            if (cnt > nums.length / 3) {
+                set.add(num);
+            }
+            map.put(num, cnt);
+        }
+        
+        res.addAll(set);
+        return res;
+    }
+    
+    
     public static List<Integer> majorityElement(int[] nums) {
         if (nums == null || nums.length == 0) {
             return new ArrayList<>();
