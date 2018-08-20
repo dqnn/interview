35fdf7af042a52597bb9100f6bd02c919493ec22
Denny <duqiang@172.31.98.206>
2018-08-19 20:03:49 -0700

    more comments and solutions

--- src/leetcode/LongestValidParentheses.java
+++ src/leetcode/LongestValidParentheses.java
@@ -70,4 +70,48 @@ Explanation: The longest valid parentheses substring is "()()"
         
         return res;
     }
+    
+    
+    /*
+     * My solution uses DP. The main idea is as follows: I construct a array longest[], for any longest[i], it stores the longest length of valid parentheses which is end at i.
+
+And the DP idea is :
+
+If s[i] is '(', set longest[i] to 0,because any string end with '(' cannot be a valid one.
+
+Else if s[i] is ')'
+
+     If s[i-1] is '(', longest[i] = longest[i-2] + 2
+
+     Else if s[i-1] is ')' and s[i-longest[i-1]-1] == '(', longest[i] = longest[i-1] + 2 + longest[i-longest[i-1]-2]
+
+For example, input "()(())", at i = 5, longest array is [0,2,0,0,2,0], longest[5] = longest[4] + 2 + longest[1] = 6.
+     */
+    public int longestValidParentheses2(String s) {
+        if (s == null || s.length() == 0) return 0;
+        
+        // build && init dp[i]: longest valid string that ends at index i
+        int n = s.length();
+        int[] dp = new int[n];
+
+        // calculate dp && track global max
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == '(') continue;
+
+            boolean consecutiveEnd = s.charAt(i - 1) == '(';
+            int priorIndex = consecutiveEnd ? i - 1 : i - dp[i - 1] - 1;
+            if (priorIndex < 0 || s.charAt(priorIndex) != '(') continue;
+
+            dp[i] = 2 + addPriorLength(priorIndex, dp);
+            dp[i] += consecutiveEnd ? 0 : dp[i - 1]; //add length from (priorIndex, i - 1]
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+
+    private int addPriorLength(int priorIndex, int[] dp) {
+        return (priorIndex - 1 >= 0) ? dp[priorIndex - 1] : 0;
+    }
 }
