4c3857a451039e578059cea2d0934bd8ddd88e3c
Denny <duqiang@172.31.98.206>
2018-08-19 19:52:26 -0700

    more comments and solutions

--- src/leetcode/LongestValidParentheses.java
+++ src/leetcode/LongestValidParentheses.java
@@ -38,25 +38,36 @@ Explanation: The longest valid parentheses substring is "()()"
      */
 
     public int longestValidParentheses(String s) {
+        if (s == null || s.length() < 1) {
+            return 0;
+        }
+        
         Stack<Integer> stack = new Stack<>();
-        int res = 0;
-        int start = -1;
-        for (int i = 0; i < s.length(); i++) {
+        int start = -1, res = 0;
+        for(int i = 0; i< s.length(); i++) {
+            //System.out.println(stack);
             if (s.charAt(i) == '(') {
                 stack.push(i);
             } else {
+                // so if stack is empty, then we know we already matched all "C", so current i as idx 
+                // the position in S is end pointer, 
+                //means we need to start new round of matching or just end
                 if (stack.isEmpty()) {
                     start = i;
                 } else {
+                    //if stack is not empty, means we still have "C" not matched, so we continue to move and  
+                    // use top in stack as start pointer to calc the length of the qualified string
                     stack.pop();
                     if (stack.isEmpty()) {
                         res = Math.max(res, i - start);
+                    // this is if stack is not empty, we calc from the current top ( idx
                     } else {
                         res = Math.max(res, i - stack.peek());
                     }
                 }
             }
         }
+        
         return res;
     }
 }
