6d9e6cf13987fb064b28dff9463919cab4384e5d
Denny <duqiang@172.31.98.206>
2018-08-22 01:05:42 -0700

    more comments and solutions

--- src/leetcode/LRUCache.java
+++ src/leetcode/LRUCache.java
@@ -51,7 +51,8 @@ public class LRUCache {
      **/
 /*
  * 1.The key to solve this problem is using a double linked list which enables us to quickly move nodes.
-2.The LRU cache is a hash table of keys and double linked nodes. The hash table makes the time of get() to be O(1). The list of double linked nodes make the nodes adding/removal operations O(1).
+2.The LRU cache is a hash table of keys and double linked nodes. The hash table makes the time of get() to be O(1). 
+The list of double linked nodes make the nodes adding/removal operations O(1).
 
 class Node {
 int key;
@@ -60,40 +61,45 @@ Node pre;
 Node next;
  */
     class Node {
-        int key;
-        int value;
-        Node next;
-        Node pre;
+        int key, value;
+        Node next, pre;
         public Node(int key, int value) {
             this.key = key;
             this.value = value;
         }
     }
-
-    private HashMap<Integer, Node> map;
+    
+    private Map<Integer, Node> map;
     private int capacity;
-    private Node head;
-    private Node tail;
-
+    private Node head, tail;
+    
+    // interview friendly 
+    //
     public LRUCache(int capacity) {
         map = new HashMap<>();
         this.capacity = capacity;
         head = null;
         tail = null;
     }
-
+    
     public int get(int key) {
         Node node = map.get(key);
         if (node == null) {
             return -1;
         }
+        // means it has pre and next, if it equals tail, then we just return the tail 
+        // 
         if (node != tail) {
+            // how could it be head?
             if (node == head) {
                 head = head.next;
-            } else {
+            } else { // this is to remove the node from the chain
+                // node.pre point to node.next
                 node.pre.next = node.next;
+                // node next pre point to node.pre
                 node.next.pre = node.pre;
             }
+            // insert node into tail
             tail.next = node;
             node.pre = tail;
             node.next = null;
@@ -101,9 +107,10 @@ Node next;
         }
         return node.value;
     }
-
+    
     public void put(int key, int value) {
         Node node = map.get(key);
+        // this is the same as get 
         if (node != null) {
             node.value = value;
             if (node != tail) {
@@ -114,25 +121,30 @@ Node next;
                     node.next.pre = node.pre;
                 }
                 tail.next = node;
-                node.pre = tail;
                 node.next = null;
+                node.pre = tail;
                 tail = node;
             }
         } else {
             Node newNode = new Node(key, value);
+            // capacity means how many capactiy we left for new elements,if 0 then we need to remove
+            // from double list, we remove the first one, head
             if (capacity == 0) {
                 Node temp = head;
                 head = head.next;
                 map.remove(temp.key);
                 capacity++;
             }
-            if (head == null && tail == null) {
+            //means first one
+            if (head == null || tail == null) {
                 head = newNode;
+            // if not we just add to the tail
             } else {
                 tail.next = newNode;
                 newNode.pre = tail;
                 newNode.next = null;
             }
+            // this part of code is shared with if (head == null || tail == null)  
             tail = newNode;
             map.put(key, newNode);
             capacity--;
