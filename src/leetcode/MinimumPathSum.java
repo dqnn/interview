4557bc69652842330aa17d3056ba05a09391ea56
Denny <duqiang@192.168.0.102>
2018-08-28 01:46:41 -0700

    more comments on the solutions

--- src/leetcode/MinimumPathSum.java
+++ src/leetcode/MinimumPathSum.java
@@ -5,21 +5,25 @@ package leetcode;
  * Package Name : leetcode
  * File Name : MinimumPathSum
  * Creator : duqiang
- * Date : Nov, 2017
+ * Date : Aug, 2018
  * Description : 64. Minimum Path Sum
  */
 public class MinimumPathSum {
     /**
-     * Given a m x n grid filled with non-negative numbers,
-     * find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
 
-     Note: You can only move either down or right at any point in time.
+Note: You can only move either down or right at any point in time.
 
-     Example 1:
-     [[1,3,1],
-     [1,5,1],
-     [4,2,1]]
-     Given the above grid map, return 7. Because the path 1→3→1→1→1 minimizes the sum.
+Example:
+
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 
      time : O(m * n)
      space : O(1)
@@ -27,16 +31,26 @@ public class MinimumPathSum {
      * @param grid
      * @return
      */
-    public int minPathSum(int[][] grid) {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (i == 0 && j != 0) grid[i][j] += grid[i][j - 1];
-                if (i != 0 && j == 0) grid[i][j] += grid[i - 1][j];
+    public int minPathSum(int[][] g) {
+        if (g == null || g.length < 1) {
+            return 0;
+        }
+        int row = g.length, column = g[0].length;
+        //scan left to right, then down
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < column; j++) {
+                if (i == 0 && j != 0) {
+                    g[i][j] +=g[i][j-1];
+                }
+                if (i !=0 && j ==0) {
+                    g[i][j] += g[i-1][j];
+                }
                 if (i != 0 && j != 0) {
-                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                    g[i][j] += Math.min(g[i-1][j], g[i][j-1]);
                 }
             }
         }
-        return grid[grid.length - 1][grid[0].length - 1];
+        
+        return g[row - 1][column - 1];
     }
 }
