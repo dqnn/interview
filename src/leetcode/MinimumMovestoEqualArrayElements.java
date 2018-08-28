ff3a1c15091627b13be00014e61a74959536808b
Denny <duqiang@192.168.0.102>
2018-08-27 23:09:51 -0700

    more detailed explanations

--- src/leetcode/MinimumMovestoEqualArrayElements.java
+++ src/leetcode/MinimumMovestoEqualArrayElements.java
@@ -5,21 +5,26 @@ package leetcode;
  * Package Name : leetcode
  * File Name : MinimumMovestoEqualArrayElements
  * Creator : duqiang
- * Date : Nov, 2017
+ * Date : Aug, 2018
  * Description : 453. Minimum Moves to Equal Array Elements
  */
 public class MinimumMovestoEqualArrayElements {
     /**
-     * Given a non-empty integer array of size n, find the minimum number of moves required to
-     * make all array elements equal, where a move is incrementing n - 1 elements by 1.
+Given a non-empty integer array of size n, find the minimum number of moves required to make all array 
+elements equal, where a move is incrementing n - 1 elements by 1.
 
-     Example:
+Example:
 
-     Input:
-     [1,2,3]
+Input:
+[1,2,3]
 
-     Output:
-     3
+Output:
+3
+
+Explanation:
+Only three moves are needed (remember each move increments two elements):
+
+[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
 
      Explanation:
      Only three moves are needed (remember each move increments two elements):
@@ -37,6 +42,28 @@ public class MinimumMovestoEqualArrayElements {
      * @param nums
      * @return
      */
+/*
+ let's define sum as the sum of all the numbers, before any moves; minNum as the min number in the list; 
+ n is the length of the list;
+
+After, say m moves, we get all the numbers as x , and we will get the following equation
+
+ sum + m * (n - 1) = x * n
+and actually,
+
+  x = minNum + m
+This part may be a little confusing, but @shijungg explained very well. 
+let me explain a little again. it comes from two observations:
+
+the minum number will always be minum until it reachs the final number, 
+because every move, other numbers (besides the max) will be increamented too;
+from above, we can get, the minum number will be incremented in every move. 
+So, if the final number is x, it would be minNum + moves;
+and finally, we will get
+
+  sum - minNum * n = m
+This is just a math calculation.
+ */
     public int minMoves(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         int min = nums[0];
