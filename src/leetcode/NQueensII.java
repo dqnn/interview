5b8453f6f599b00d9b813a1dc84aa94376057024
Denny <duqiang@192.168.0.103>
2018-09-01 01:28:37 -0700

    more

--- src/leetcode/NQueensII.java
+++ src/leetcode/NQueensII.java
@@ -26,10 +26,16 @@ public class NQueensII {
     // why this is shorter since we only requires return integer
     //Start row by row, and loop through columns. At each decision point, 
     //skip unsafe positions by using three boolean arrays.
+    
+/*常规n-queens解法, 数答案个数.
+    用column标记此行之前的哪些column已经放置了queen. 棋盘坐标(row, col)对应column的第col位(LSB --> MSB, 下同).
+    用diag标记此位置之前的哪些主对角线已经放置了queen. 棋盘坐标(row, col)对应diag的第(n - 1 + row - col)位.
+    用antiDiag标记此位置之前的哪些副对角线已经放置了queen. 棋盘坐标(row, col)对应antiDiag的第(row + col)位.
+*/
     public int totalNQueens(int n) {
         boolean[] cols = new boolean[n];// columns   |
-        boolean[] d1 = new boolean[2 * n]; // diagonals \
-        boolean[] d2 = new boolean[2 * n]; // diagonals /
+        boolean[] d1 = new boolean[2 * n]; // top left -> bottom right
+        boolean[] d2 = new boolean[2 * n]; // top right -> bottom left
         helper(0, cols, d1, d2, n);
         return res;
     }
@@ -41,6 +47,7 @@ public class NQueensII {
             return;
         }
         for (int col = 0; col < n; col++) {
+            // 
             int id1 = col - row + n;
             int id2 = col + row;
             if (cols[col] || d1[id1] || d2[id2]) continue;
