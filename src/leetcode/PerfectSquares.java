338d269ddd95d297725acaf5c4950adf83ecc883
Denny <duqiang@192.168.0.103>
2018-09-03 19:01:58 -0700

    more solutions

--- src/leetcode/PerfectSquares.java
+++ src/leetcode/PerfectSquares.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  * Package Name : leetcode
  * File Name : PerfectSquares
  * Creator : duqiang
- * Date : Dec, 2017
+ * Date : Sep, 2018
  * Description : 279. Perfect Squares
  */
 public class PerfectSquares {
@@ -25,15 +25,38 @@ public class PerfectSquares {
      * @return
      */
 
+    /*
+dp[0] = 0 
+dp[1] = dp[0]+1 = 1
+dp[2] = dp[1]+1 = 2
+dp[3] = dp[2]+1 = 3
+dp[4] = Min{ dp[4-1*1]+1, dp[4-2*2]+1 } 
+      = Min{ dp[3]+1, dp[0]+1 } 
+      = 1               
+dp[5] = Min{ dp[5-1*1]+1, dp[5-2*2]+1 } 
+      = Min{ dp[4]+1, dp[1]+1 } 
+      = 2
+                        .
+                        .
+                        .
+dp[13] = Min{ dp[13-1*1]+1, dp[13-2*2]+1, dp[13-3*3]+1 } 
+       = Min{ dp[12]+1, dp[9]+1, dp[4]+1 } 
+       = 2
+                        .
+                        .
+                        .
+dp[n] = Min{ dp[n - i*i] + 1 },  n - i*i >=0 && i >= 1
+     */
     public int numSquares(int n) {
-        int[] res = new int[n + 1];
-        Arrays.fill(res, Integer.MAX_VALUE);
-        res[0] = 0;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         for (int i = 0; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
-                res[i] = Math.min(res[i], res[i - j * j] + 1);
+             // we use j * j to re-calc the res array
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
-        return res[n];
+        return dp[n];
     }
 }
