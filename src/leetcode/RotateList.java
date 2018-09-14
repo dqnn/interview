0f91cb35fb2013dca7a3fbc973f877271c2ce2a7
Denny <duqiang@192.168.3.96>
2018-09-14 02:49:58 -0700

    more

--- src/leetcode/RotateList.java
+++ src/leetcode/RotateList.java
@@ -10,9 +10,26 @@ package leetcode;
  */
 public class RotateList {
     /**
-     * For example:
-     Given 1->2->3->4->5->NULL and k = 2,
-     return 4->5->1->2->3->NULL.
+Given a linked list, rotate the list to the right by k places, 
+where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+
 
      time : O(n);
      space : O(1);
@@ -21,6 +38,11 @@ public class RotateList {
      * @param k
      * @return
      */
+    // the whole logic is to rotate last k % len elements into front of the 
+    // linkedlist so we have to get the length of the list and k = k % len;
+    //also link last element to head
+    // then next is another pointer to len - k % len - 1 elements from last,it point 
+    // previous element which will become the head of the new linkedlist
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null) return head;
         ListNode index = head;
--- src/leetcode/RussianDollEnvelopes.java
+++ src/leetcode/RussianDollEnvelopes.java
@@ -8,19 +8,34 @@ import java.util.Comparator;
  * Package Name : leetcode
  * File Name : RussianDollEnvelopes
  * Creator : duqiang
- * Date : Jan, 2018
+ * Date : Sep, 2018
  * Description : 354. Russian Doll Envelopes
  */
 public class RussianDollEnvelopes {
     /**
-     * You have a number of envelopes with widths and heights given as a pair of integers (w, h).
-     * One envelope can fit into another if and only if both the width and height of one envelope
-     * is greater than the width and height of the other envelope.
+You have a number of envelopes with widths and heights given as a pair of integers (w, h). 
+One envelope can fit into another if and only if both the width and height of 
+one envelope is greater than the width and height of the other envelope.
 
-     What is the maximum number of envelopes can you Russian doll? (put one inside other)
+What is the maximum number of envelopes can you Russian doll? (put one inside 
+other)
+
+Note:
+Rotation is not allowed.
+
+Example:
+
+Input: [[5,4],[6,4],[6,7],[2,3]]
+Output: 3 
+Explanation: The maximum number of envelopes you can Russian doll is 3 
+([2,3] => [5,4] => [6,7]).
+
+     What is the maximum number of envelopes can you Russian doll? 
+     (put one inside other)
 
      Example:
-     Given envelopes = [[5,4],[6,4],[6,7],[2,3]], the maximum number of envelopes you can Russian doll
+     Given envelopes = [[5,4],[6,4],[6,7],[2,3]], the maximum number of 
+     envelopes you can Russian doll
      is 3 ([2,3] => [5,4] => [6,7]).
 
      排序
@@ -39,7 +54,14 @@ public class RussianDollEnvelopes {
      * @param envelopes
      * @return
      */
-
+/*
+Sort the array. Ascend on width and descend on height if width are same.
+Find the longest increasing subsequence based on height.
+Since the width is increasing, we only need to consider height.
+[3, 4] cannot contains [3, 3], so we need to put [3, 4] before [3, 3] when 
+sorting otherwise it will be counted as
+ an increasing number if the order is [3, 3], [3, 4]
+ */
     public int maxEnvelopes(int[][] envelopes) {
         if (envelopes == null || envelopes.length == 0) return 0;
         Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
