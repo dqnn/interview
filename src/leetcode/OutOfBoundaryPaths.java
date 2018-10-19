69abbd6ad800b4edf71538e6370c14d071784b52
Denny <duqiang@192.168.0.100>
2018-10-19 02:17:26 -0700

    more solutions

--- src/leetcode/OutOfBoundaryPaths.java
+++ src/leetcode/OutOfBoundaryPaths.java
@@ -1,4 +1,7 @@
 package leetcode;
+
+import java.util.Arrays;
+
 /*
  * author Denny Du 
  * Date 10/18/2018
@@ -32,21 +35,81 @@ The length and height of the grid is in range [1,50].
 N is in range [0,50].
  */
 public class OutOfBoundaryPaths {
+    //DP[i][j][k] stands for how many possible ways to walk into cell j,k in step i, DP[i][j][k] 
+    //only depends on DP[i - 1][j][k], so we can compress 3 dimensional dp array to 2 dimensional.
+    public int findPaths(int m, int n, int N, int i, int j) {
+        if (N <= 0) return 0;
+        
+        final int MOD = 1000000007;
+        int[][] count = new int[m][n];
+        // i and j is the ball coordination
+        count[i][j] = 1;
+        int result = 0;
+        
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        //we turn steps into a for loop
+        for (int step = 0; step < N; step++) {
+            int[][] temp = new int[m][n];
+            // the two for are visit each cell in the matrix
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    // 4 directions, r c is original
+                    for (int[] d : dirs) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+                        if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
+                            // 
+                            result = (result + count[r][c]) % MOD;
+                        }
+                        else {
+                            temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
+                        }
+                    }
+                }
+            }
+            count = temp;
+        }
+        
+        return result;
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
+    //memo[i][j][k] is used to store the number of possible moves leading to a path 
+    //out of the boundary if the current position is given by the indices (i, j)(i,j) 
+    //and number of moves left is kk.
+    //Thus, now if a function call with some parameters is repeated, the memomemo array 
+    //will already contain valid values corresponding to that function call resulting 
+    //in pruning of the search space.
+
+    int M = 1000000007;
+
+    public int findPaths3(int m, int n, int N, int i, int j) {
+        int[][][] memo = new int[m][n][N + 1];
+        for (int[][] l : memo)
+            for (int[] sl : l)
+                Arrays.fill(sl, -1);
+        return findPaths(m, n, N, i, j, memo);
+    }
+
+    public int findPaths(int m, int n, int N, int i, int j, int[][][] memo) {
+        if (i == m || j == n || i < 0 || j < 0)
+            return 1;
+        if (N == 0)
+            return 0;
+        if (memo[i][j][N] >= 0)
+            return memo[i][j][N];
+        memo[i][j][N] = (( findPaths(m, n, N - 1, i - 1, j, memo) 
+                         + findPaths(m, n, N - 1, i + 1, j, memo)) % M
+                         + (findPaths(m, n, N - 1, i, j - 1, memo) 
+                         + findPaths(m, n, N - 1, i, j + 1, memo)) % M) % M;
+        return memo[i][j][N];
+    }
     
     //this is TLE, the problem for the case:
     // if N > 0 while we return to the cell we visited still this can be correct, as example 2. 
     
     int count = 0;
-    public int findPaths(int m, int n, int N, int i, int j) {
+    public int findPaths2(int m, int n, int N, int i, int j) {
         int[][] visited = new int[m][n];
         dfs(m, n, N, i, j, visited);
         return count;
