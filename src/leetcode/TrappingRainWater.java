e6af5199a33e6400bfe511ab077bc65fdfaf2508
Denny <duqiang@172.31.98.206>
2018-10-27 23:59:41 -0700

    more comments

--- src/leetcode/TrappingRainWater.java
+++ src/leetcode/TrappingRainWater.java
@@ -55,11 +55,19 @@ we filled the whole container.
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
         int res = 0;
+        
         while(left < right) {
+            //if height[left] < height[right], we move to right, because 
+            //it is decided by the smaller one not big one. if some in the middle are bigger 
+            //then the calculation is correct, if smaller,also correct
             //we scan from left to right and find the max bin, 
             //the max maybe current one or previous one, so res will be means how much can fill the gaps
             
-            //so if right will be taller so one side wall, 
+            //split the bottle where we can fill the water and add them together, 
+            //so we use maxium length to subtract each one, then we get each piece of water we can fill
+            //if we meet bigger one, then bigger one will become the base. 
+            
+            //this problems combines DP and divide and conqure
             if (height[left] < height[right]) {
                 leftMax = Math.max(height[left], leftMax);
                 res += leftMax - height[left];
@@ -72,7 +80,14 @@ we filled the whole container.
         }
         return res;
     }
-    // this is double scan. 
+    // this is double scan.
+    
+    //thinking about this way: 
+    
+    //we have one virtual array,from left to right, each la[i] means 0->i the max, 
+    //another virtual array is ra, each ra[i] will be i->n-1, max, 
+    //so if we put these 2 arrays together, we get min of two values in the i, 
+    //
     public int trap2(int[] height) {
         if (height == null || height.length < 1) {
             return 0;
@@ -116,16 +131,20 @@ and the previous bar is il.
         if (heights == null || heights.length < 1) {
             return 0;
         }
+        //we push index into stack
         Stack<Integer> s = new Stack<Integer>();
         int i = 0, maxWater = 0, maxBotWater = 0;
         while (i < heights.length){
-            if (s.isEmpty() || heights[i]<=heights[s.peek()]){
+            //if decrease, then no way to store water, 
+            if (s.isEmpty() || heights[i] <= heights[s.peek()]) {
                 s.push(i++);
-            }
-            else {
+            //if we found anything bigger then we need to visit back until we find previous bigger one
+                //actually this is scan from right to left
+            } else {
                 int bot = s.pop();
-                maxBotWater = s.isEmpty()? // empty means no il
-                0:(Math.min(heights[s.peek()],heights[i])-heights[bot])*(i-s.peek()-1);
+                maxBotWater = s.isEmpty() ? // empty means no il
+                        //i actually here stable, we want to find smaller one, and compare to previous
+                        0 : (Math.min(heights[s.peek()], heights[i]) - heights[bot]) * (i - s.peek() - 1);
                 maxWater += maxBotWater;
             }
         }
