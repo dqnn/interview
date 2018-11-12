14bafac0ce964d4edeabf9765fc2a6a147d2d8ba
Denny <duqiang@192.168.0.100>
2018-11-11 20:46:33 -0800

    more comments and solutions

--- src/leetcode/ScrambleString.java
+++ src/leetcode/ScrambleString.java
@@ -92,6 +92,8 @@ Output: false
     }
     
     // this is better solution on Complexity O()n ^ 4
+    
+    // this problem is needed to remembered and discussed
     public boolean isScramble2(String s1, String s2) {
         if (s1.length() != s2.length()) return false;
         int len = s1.length();
