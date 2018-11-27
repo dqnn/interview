8596801c3a9197e52e3dd52ceab0c05b336ca078
Denny <duqiang@192.168.0.100>
2018-11-27 07:02:02 -0800

    more probelms and code

--- src/leetcode/BuddySystemBitMap.java
+++ src/leetcode/BuddySystemBitMap.java
@@ -23,15 +23,25 @@ public  class BuddySystemBitMap {
         1  1 1   0 1
         Since it's complete binary tree, the nodes can be stored in an array:
         [0,0,1,1,0,1,1,1,1,1,0,1] 
+        offest and len means the last level
+ */
+/*
+就是一个二维数组，但是每层长度不一样，A[0]长度为1，A[1]为2，A[2]为4，依次类
+推。每个元素可以为0或者为1，clearBit或者setBit都是说的最后一层从offset开始到
+offset+len-1置为1或0
  */
     public void set_bit(int[] t, int start, int len) {
        if (t == null || t.length< 1) return;
+       //complete binary tree leaf may not complete so we want to check which is smaller
        int n = t.length -1, end = Math.min(n + 1, Math.min(start + len,  2 * start + 1));
        for(int i = start; i < end; i++) {
            if (t[i] == 1) continue;
            t[i] = 1;
+           //subs
            set_subTree(t, i, n);
+           //ancestors, for example, 
            while(i> 0) {
+               //make sure its sibling is 1, if its sibling is 0, cannot set ancestors
                if (i % 2 == 0 && t[i-1] == 1 
                    || (i % 2 == 1 && i < n && t[i+1] == 1)) {
                    t[(i - 1)/2] = 1;
@@ -77,5 +87,37 @@ public  class BuddySystemBitMap {
         BuddySystemBitMap buddy = new BuddySystemBitMap();
         buddy.set_bit(t, 1, 3);
         System.out.println(Arrays.toString(t));
+    }
+    
+    //not verified code
+    public static void clearBit(boolean[][] matrix,  int offset, int length) {
+        int curLevel = matrix.length-1;
+        int left = offset;
+        int right = offset+length;
+        while (curLevel >= 0){
+            for (int i = left; i <= right; i++) {
+                matrix[curLevel][i] = false;
+            }
+            curLevel--;
+            left = left/2;
+            right = right/2;
+        }
+    }
+    public static void setBit(boolean[][] matrix,  int offset, int length) {
+        int curLevel = matrix.length-1;
+        int left = offset;
+        int right = offset+length;
+        while (curLevel >= 0){
+            for (int i = left; i <= right; i++) {
+                if (curLevel == matrix.length-1)
+                    matrix[curLevel][i] = true;
+                else
+                    if (matrix[curLevel+1][i*2]&&matrix[curLevel+1][i*2+1])
+                        matrix[curLevel][i] = true;
+            }
+            curLevel--;
+            left = left/2;
+            right = right/2;
+        }
     }
 }
--- src/leetcode/ValidSquare.java
+++ src/leetcode/ValidSquare.java
@@ -36,7 +36,8 @@ Input points have no order.
         return check(p1, p2, p3, p4) || check(p1, p3, p2, p4) || check(p1, p2, p4, p3);
     }
     
-    //this is better, we use sort, follow up: 
+    //this is better, we use sort, follow up: followup: 
+    //给n个点，问总共正方形的个数，最后会要求优化到O(n^2)
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
         int[][] p={p1,p2,p3,p4};
         //if x is the same, then we sort on y asc
