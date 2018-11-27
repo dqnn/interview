5b7e40af798aadf71de85ee215760191f51e78a4
Denny <duqiang@192.168.0.100>
2018-11-27 02:59:31 -0800

    more comments

--- src/leetcode/BinaryTreeVerticalOrderTraversal.java
+++ src/leetcode/BinaryTreeVerticalOrderTraversal.java
@@ -15,9 +15,11 @@ import java.util.Queue;
  */
 public class BinaryTreeVerticalOrderTraversal {
     /**
-     *Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+     *Given a binary tree, return the vertical order traversal of 
+     *its nodes' values. (ie, from top to bottom, column by column).
 
-     If two nodes are in the same row and column, the order should be from left to right.
+     If two nodes are in the same row and column, the order should be from 
+     left to right.
 
      Examples:
 
