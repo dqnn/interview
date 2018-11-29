b22b2ae1add65d4128f7980ac638478ab512f7e3
Denny <duqiang@192.168.0.100>
2018-11-28 19:10:40 -0800

    more comments and solutions

--- src/leetcode/BinaryTreeVerticalOrderTraversal.java
+++ src/leetcode/BinaryTreeVerticalOrderTraversal.java
@@ -1,8 +1,11 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -68,7 +71,12 @@ public class BinaryTreeVerticalOrderTraversal {
      */
     private int min = 0;
     private int max = 0;
-
+    //thinking process:  the problem is to output the List which contains list which is 
+    //scan vertically, notes, there are some elements overlapped but they should be in order
+    //so last requirement is key that we have to add the elements when we access it, 
+    //if we +1 or -1, the elment will lose the info of accessing order. 
+    //so we use map: idx-> List<Integer> to record each vertical List,idx is verrical idx
+    //
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) {
@@ -107,4 +115,120 @@ public class BinaryTreeVerticalOrderTraversal {
         helper(root.left, idx - 1);
         helper(root.right, idx + 1);
     }
+    
+    
+  //interview friendly: 
+    //this solution is iterative, not recursive 
+    
+    //
+    class Node2{
+        int idx;
+        TreeNode node;
+        public Node2(int idx,TreeNode node){
+            this.idx = idx;
+            this.node = node;
+        }
+    }
+    public List<List<Integer>> verticalOrder3(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Map<Integer, List<Node2>> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        Queue<Node2> q = new LinkedList<>();
+        q.offer(new Node2(0,root));
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int i = 0; i< size;i++) {
+                Node2 node = q.poll();
+                map.computeIfAbsent(node.idx, v->new ArrayList<>()).add(node);
+                min = Math.min(min, node.idx);
+                max = Math.max(max, node.idx);
+                if (node.node.left != null){
+                    q.offer(new Node2(node.idx - 1, node.node.left));
+                }
+                if (node.node.right != null){
+                    q.offer(new Node2(node.idx + 1, node.node.right));
+                }
+            }
+        }
+        
+        for(int i =min; i<=max;i++) {
+             List<Integer> temp = new ArrayList<>();
+            for(Node2 node : map.get(i)) {
+                temp.add(node.node.val);
+            }
+            res.add(temp);
+                
+        }
+        return res;
+    }
+    
+    
+    
+    
+    
+    class Node {
+        int level;
+        int val;
+        int idx;
+        //left is 1, right is 0, compare to its parent, -1 is root
+        int pos;
+        public Node(int level, int val, int idx, int pos) {
+            this.level = level;
+            this.val = val;
+            this.idx = idx;
+            this.pos = pos;
+        }
+        public String toString() {
+            return String.format("%s:%s:%s:%s", idx, level, pos, val);
+        }
+    }
+    //this solution has 1 use case cannot pass, the reason is 
+    //when we visit the tree recursively. if two nodes are same 
+    //level, same idx, so they must be ordered by access order, like pre order
+    public List<List<Integer>> verticalOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        //idx, level and left, right position sort
+        PriorityQueue<Node> pq = 
+            new PriorityQueue<>(
+            (a,b)->(a.idx == b.idx ? 
+                    (a.level == b.level ? a.pos - b.pos : a.level - b.level)
+                    : a.idx-b.idx));
+        helper(root, pq, 0, 0, 0);
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            System.out.println(node);
+            List<Integer> list = new ArrayList<>();
+            list.add(node.val);
+            int preIdx = node.idx;
+            while(!pq.isEmpty() && pq.peek().idx == preIdx) {
+                System.out.println(pq.peek());
+                list.add(pq.poll().val);
+            }
+            //this is just for [1,2,3,null,5,6] case to pass leetcode submit
+            //which [[2],[1,6,5],[3]] vs [[2],[1,5,6],[3]]
+            //1.6.5 should be ok
+            //Collections.sort(list);
+            res.add(list);
+        }
+        return res;
+    }
+    
+    private void helper(TreeNode node, PriorityQueue<Node> pq, int level, int idx, int pos) {
+        if (node == null) return;
+        pq.offer(new Node(level, node.val, idx, pos));
+        if (node.left != null) {
+            helper(node.left, pq, level + 1, idx - 1, pos + 1);
+        }
+        if (node.right != null) {
+            helper(node.right, pq, level + 1, idx + 1,pos- 1);
+        }
+    }
+    
 }
