89d4aa3783e88b2a87e2b21f0be47d6b10976dbc
Denny <duqiang@192.168.0.100>
2018-12-04 21:23:21 -0800

    more problems and solutions

--- src/leetcode/AutocompleteSystem.java
+++ src/leetcode/AutocompleteSystem.java
@@ -72,6 +72,7 @@ class AutocompleteSystem {
         arr = new HashMap[26];
         for (int i = 0; i < 26; i++)
             arr[i] = new HashMap<>();
+        //for each  chatacter in sentence, if duplicate one ,then it would override
         for (int i = 0; i < sentences.length; i++)
             arr[sentences[i].charAt(0) - 'a'].put(sentences[i], times[i]);
     }
@@ -83,14 +84,16 @@ class AutocompleteSystem {
             arr[cur_sent.charAt(0) - 'a'].put(cur_sent, arr[cur_sent.charAt(0) - 'a'].getOrDefault(cur_sent, 0) + 1);
             cur_sent = "";
         } else {
-            List < Node > list = new ArrayList < > ();
+            List <Node> list = new ArrayList<>();
             cur_sent += c;
             for (String key: arr[cur_sent.charAt(0) - 'a'].keySet()) {
                 if (key.indexOf(cur_sent) == 0) {
                     list.add(new Node(key, arr[cur_sent.charAt(0) - 'a'].get(key)));
                 }
             }
-            Collections.sort(list, (a, b) -> a.times == b.times ? a.sentence.compareTo(b.sentence) : b.times - a.times);
+            //result sort
+            Collections.sort(list, (a, b) -> a.times == b.times ? 
+                    a.sentence.compareTo(b.sentence) : b.times - a.times);
             for (int i = 0; i < Math.min(3, list.size()); i++)
                 res.add(list.get(i).sentence);
         }
