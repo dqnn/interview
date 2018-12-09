9e4d33e8ef9b7b85f4c3465be253c5ebfe1c9309
Denny <duqiang@192.168.0.100>
2018-12-09 04:49:53 -0800

    more comments

--- src/leetcode/ReversePairs.java
+++ src/leetcode/ReversePairs.java
@@ -20,13 +20,22 @@ Output: 3
     //TreeMap, 2Dimension, BIT, this is 2D sorting we want to keep its position while want to 
     //query its value
     //O(nlgn)
-    public int reversePairs(int[] nums) {
+    public static int reversePairs(int[] nums) {
         return mergeSort(nums, 0, nums.length-1);
     }
-    private int mergeSort(int[] nums, int s, int e){
+    private static int mergeSort(int[] nums, int s, int e){
         if(s>=e) return 0; 
         int mid = s + (e-s)/2; 
         int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e); 
+        //position like: i mid j,  s<= i <= mid,that's to say, we want to compare
+        //nums[i] and nums[j], if we can find one, then j need to j++, until loop break, then the 
+        //number of qualified num is j - (mid +1) + 1 -1(last j is not qualified)
+        
+        //for example [1,3,2,3,1], when the loop it would be cnt = 0. s = 0, e= 1,
+        //i = 0 <= mid, j = mid + 1 means the first on another half. so we would only loop 1 time
+        // and then we would sort [0, 2(e + 1)] this part and return. 
+        
+        //
         for(int i = s, j = mid+1; i<=mid; i++){
             while(j<=e && nums[i]/2.0 > nums[j]) j++; 
             cnt += j-(mid+1); 
@@ -35,4 +44,9 @@ Output: 3
         return cnt; 
     }
     //BST can also work, but O(n^2)
+    
+    public static void main(String[] args) {
+        int[] in = {1,3,2,3,1};
+        System.out.println(reversePairs(in));
+    }
 }
