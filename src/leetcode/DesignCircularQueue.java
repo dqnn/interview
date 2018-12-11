c08207b1543d2dec8c1c9f302618da9d441bc228
Denny <duqiang@192.168.0.100>
2018-12-10 16:24:05 -0800

    more problems and solutions

---
+++ src/leetcode/DesignCircularQueue.java
@@ -0,0 +1,155 @@
+package leetcode;
+public class DesignCircularQueue {
+/*
+622. Design Circular Queue
+Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Your implementation should support following operations:
+
+MyCircularQueue(k): Constructor, set the size of the queue to be k.
+Front: Get the front item from the queue. If the queue is empty, return -1.
+Rear: Get the last item from the queue. If the queue is empty, return -1.
+enQueue(value): Insert an element into the circular queue. Return true if the operation is successful.
+deQueue(): Delete an element from the circular queue. Return true if the operation is successful.
+isEmpty(): Checks whether the circular queue is empty or not.
+isFull(): Checks whether the circular queue is full or not.
+ 
+
+Example:
+
+MyCircularQueue circularQueue = new MyCircularQueue(3); // set the size to be 3
+circularQueue.enQueue(1);  // return true
+circularQueue.enQueue(2);  // return true
+circularQueue.enQueue(3);  // return true
+circularQueue.enQueue(4);  // return false, the queue is full
+circularQueue.Rear();  // return 3
+circularQueue.isFull();  // return true
+circularQueue.deQueue();  // return true
+circularQueue.enQueue(4);  // return true
+circularQueue.Rear();  // return 4
+*/
+
+    //thinking process: the problem is want us to implement the ds to have O(1)
+    //so i come up with an array, head, tail pointer and size, 
+    //first idea is to use head and tail to guard the array, but then problem came as we 
+    //endqueue and dequeue in random, so tail maybe before front, and we have no way to tell
+    //how many elements are in array, so we use size to indicate this. most tricky here is 
+    //1. tail begin with -1. most for indicate current tail is last elment
+    //2 we always use size to indicate isFull(). isEmpty(). also we should use these functions
+    //as many as possible
+    int[] a;
+    int head, tail,size;
+    public DesignCircularQueue(int k) {
+        this.a = new int[k];
+        this.head = 0;
+        //here is the key, we tail will always point to last element in array. 
+        this.tail = -1;
+        //this is the key to indicate how many elments in curent array
+        this.size = 0;
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    //[1,2,3]
+    public boolean enQueue(int value) {
+        if (isFull()) return false;
+        tail = (tail + 1) % a.length;
+        a[tail] = value;
+        size++;
+        return true;
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        else {
+            head = (head + 1) % a.length;
+            size--;
+            return true;
+        }
+    }
+    
+    /** Get the front item from the queue. */
+    public int Front() {
+        if (!isEmpty()) return a[head];
+        else return -1;
+    }
+    
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if (!isEmpty()) return a[tail];
+        else return -1;
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        if (size == 0) return true;
+        else return false;
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        if (a.length == size) return true;
+        else return false;
+    }
+    
+    //another implementation of circular queue, we use write as index to indicate
+    //last's elment's next
+    
+    //the key is to (write - count + K) % K means front
+    //(write - 1 + K) % K means tail
+    //third key is write starts 0, and always point to last element's next
+    class MyCircularQueue {
+        private int[] array;
+        private int count;  //used count
+        private int write;
+
+        /** Initialize your data structure here. Set the size of the queue to be k. */
+        public MyCircularQueue(int k) {
+            array = new int[k];
+            count = 0;
+            write = 0;
+        }
+        
+        /** Insert an element into the circular queue. Return true if the operation is successful. */
+        public boolean enQueue(int value) {
+            if (count == array.length) {
+                return false;
+            }
+            array[write] = value;
+            count++;
+            write = (write + 1) % array.length;
+            return true;
+        }
+        
+        /** Delete an element from the circular queue. Return true if the operation is successful. */
+        public boolean deQueue() {
+            if (count == 0) {
+                return false;
+            }
+            count--;
+            return true;
+        }
+        
+        /** Get the front item from the queue. */
+        public int Front() {
+            return count == 0 ? -1 : array[(write - count + array.length) % array.length];
+        }
+        
+        /** Get the last item from the queue. */
+        public int Rear() {
+            return count == 0 ? -1 : array[(write - 1 + array.length) % array.length];
+        }
+        
+        /** Checks whether the circular queue is empty or not. */
+        public boolean isEmpty() {
+            return count == 0;
+        }
+        
+        /** Checks whether the circular queue is full or not. */
+        public boolean isFull() {
+            return count == array.length;
+        }
+    }
+}
---
+++ src/leetcode/SmallestRange.java
@@ -0,0 +1,157 @@
+package leetcode;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+class SmallestRange {
+    /*
+     * 632. Smallest Range You have k lists of sorted integers in ascending order.
+     * Find the smallest range that includes at least one number from each of the k
+     * lists.
+     * 
+     * We define the range [a,b] is smaller than range [c,d] if b-a < d-c or a < c
+     * if b-a == d-c.
+     * 
+     * Example 1: Input:[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]] Output: [20,24]
+     * Explanation: List 1: [4, 10, 15, 24,26], 24 is in range [20,24]. List 2: [0,
+     * 9, 12, 20], 20 is in range [20,24]. List 3: [5, 18, 22, 30], 22 is in range
+     * [20,24].
+     */
+    //thinking process: given K sorted array, find smallest range which satisfy: 1: range 
+    //is smallest, 2: the range at least contain one elment from K lists
+    
+    //so the range is smallest means it has fewest number,also means the diff between last and 
+    //first elment is smallest
+    // so suppose we get each element from the list, and we compare smallest to largest, then 
+    //we can have 1 range could satisfy 2, for 1 we need a loop to make sure we always 
+    //sort the list easily
+    
+    //the process is exactly the same as merge sorted K list. 
+    
+    //the heap help us to maintain each element from one list, if one list is done, then we 
+    //should end the loop because we need to satisfy 2. 
+    
+    //the easier understanding solution is to have min and max heap, min always pop the elment
+    //max always offer elements in while it always keep max elment no matter how many added into 
+    //min heap, we use max elment to compare first elment in min heap, so we can know for 
+    //each range, it is bigger or smaller
+    public int[] smallestRange(List<List<Integer>> nums) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b)->(a.val - b.val));
+        //max indicate the max value in current heap
+        int max = Integer.MIN_VALUE;
+        //add each row head into the min heap
+        for (int row = 0; row < nums.size(); row++) {
+            int val = nums.get(row).get(0);
+            Node node = new Node(row, 0, val);
+            pq.offer(node);
+            max = Math.max(max, val);
+        }
+        int range = Integer.MAX_VALUE;
+        //output
+        int start = -1, end = -1;
+        //this is like sliding window, the size is list size,which means each sorted 
+        //would only have 1 element in the heap, we always choose min one and comarpe the 
+        //max in this heap, and we update the range, and we will add rest of the array into 
+        //this heap unti we get all possible ranges
+        while (pq.size() == nums.size()) {
+            Node curr = pq.poll();
+            //if new range smaller than previous one, update it
+            if (max - curr.val < range) {
+                range = max - curr.val;
+                start = curr.val;
+                end = max;
+            }
+            //if the list has more elements after this index, then add to queue
+            if (curr.idx + 1 < nums.get(curr.row).size()) {
+                curr.idx = curr.idx + 1;
+                curr.val = nums.get(curr.row).get(curr.idx);
+                pq.offer(curr);
+                //
+                if (curr.val > max) {
+                    max = curr.val;
+                }
+            }
+        }
+
+        return new int[] {start, end };
+    }
+
+    class Node {
+        int val;
+        int idx;
+        int row;
+        public Node(int row, int idx, int val) {
+            this.val = val;
+            this.idx = idx;
+            this.row = row;
+        }
+    }
+    //sliding window
+    public static int[] smallestRange2(List<List<Integer>> nums) {
+        List<int[]> list = IntStream.range(0, nums.size())
+                .mapToObj( i -> nums.get(i).stream().map(x -> new int[]{x, i}))
+                .flatMap(y -> y)
+                .sorted(Comparator.comparingInt(p -> p[0])).collect(toList());
+        int[] counts = new int[nums.size()];
+        BitSet set = new BitSet(nums.size());
+        int start = -1;
+        int[] res = new int[2];
+        for(int i = 0; i < list.size(); i++) {
+            int[] p = list.get(i);
+            set.set(p[1]);
+            counts[p[1]] += 1;
+            if(start == -1) { start = 0; }
+            while(start < i && counts[list.get(start)[1]] > 1) {
+                counts[list.get(start)[1]]--;
+                start++;
+            }
+            if(set.cardinality() == nums.size()) {
+                if( (res[0] == 0 && res[1] == 0) || (list.get(i)[0] - list.get(start)[0]) < res[1] - res[0]) {
+                    res[0] = list.get(start)[0];
+                    res[1] = list.get(i)[0];
+                }
+            }
+        }
+        return res;
+    }
+    //use max heap and min heap
+    public int[] smallestRange3(List<List<Integer>> nums) {
+        //min heap
+        PriorityQueue<int[]> min=new PriorityQueue<>(1,new Comparator<int[]>(){
+            public int compare(int[] n1,int[] n2){
+                return nums.get(n1[0]).get(n1[1])-nums.get(n2[0]).get(n2[1]);
+            }              
+        });
+        //max heap
+        PriorityQueue<int[]> max=new PriorityQueue<>(1,new Comparator<int[]>(){
+            public int compare(int[] n1,int[] n2){
+                return nums.get(n2[0]).get(n2[1])-nums.get(n1[0]).get(n1[1]);
+            }
+        });
+        //first we add first element in each list in nums
+        for(int i=0;i<nums.size();i++){
+              int[] tmp=new int[]{i,0};
+              min.offer(tmp);
+              max.offer(tmp);
+        }
+        //result
+        int[] res=new int[]{Integer.MIN_VALUE,Integer.MAX_VALUE};
+        
+        while(min.size()==nums.size()){
+            int[] m1=max.peek();
+            int[] m2=min.poll();
+            if((long)nums.get(m1[0]).get(m1[1])-(long)nums.get(m2[0]).get(m2[1]) 
+                    < (long)res[1]-(long)res[0]){
+                res[0]=nums.get(m2[0]).get(m2[1]);
+                res[1]=nums.get(m1[0]).get(m1[1]);
+            }
+            
+            if(m2[1]+1<nums.get(m2[0]).size()){
+                int[] m3=new int[]{m2[0],m2[1]+1};
+                min.offer(m3);
+                max.offer(m3);
+                max.remove(m2);
+            }
+        }
+        return res;
+  }
+}
