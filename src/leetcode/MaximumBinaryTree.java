2449fef18e69ce124897b3d1623dfda58aef0177
Denny <duqiang@192.168.0.100>
2018-12-11 17:13:38 -0800

    more problems and solutions

---
+++ src/leetcode/MaximumBinaryTree.java
@@ -0,0 +1,114 @@
+package leetcode;
+import java.util.*;
+public class MaximumBinaryTree {
+/*
+654. Maximum Binary Tree
+Given an integer array with no duplicates. A maximum tree building on this array 
+is defined as follow:
+
+The root is the maximum number in the array.
+The left subtree is the maximum tree constructed from left part subarray divided by 
+the maximum number.
+The right subtree is the maximum tree constructed from right part subarray divided 
+by the maximum number.
+Construct the maximum tree by the given array and output the root node of this tree.
+
+Example 1:
+Input: [3,2,1,6,0,5]
+Output: return the tree root node representing the following tree:
+*/
+    //O(n)/O(n)
+    //the parent of a node = min(nearest max to the left, nearest max to the right)
+/*
+If we have built the max binary tree for nums[0] ~ nums[i - 1], how can we insert 
+nums[i] to the binary tree?
+Say the max binary tree for nums[0] ~ nums[i - 1] looks like:
+
+      A
+     / \
+  ...   B
+       / \
+    ...   C
+         / \
+      ...   ...
+Say the node for nums[i] is D.
+If D.val > A.val, then because A.val is at the left of D.val, we can just move the 
+tree rooted at A to the left child of D.
+
+        D
+       /
+      A
+     / \
+  ...   B
+       / \
+    ...   C
+         / \
+      ...   ...
+If D.val < A.val, then because D.val is at the right of A.val, 
+D must be put into the right subtree of A.
+Similarly, if D.val < B.val, then D must be put into the right subtree of B.
+Say B.val > D.val > C.val, then D should be the right child of B. (because D.val 
+is at the right of B.val, and D.val is the biggest among the numbers at the right of B.val.)
+Because C.val < D.val, and C.val is at the left of D.val, C should become left child of D.
+
+      A
+     / \
+  ...   B
+       / \
+     ...  D
+         /
+        C
+       / \
+    ...   ...
+So to update the max binary tree for nums[0] ~ nums[i - 1], we need to know the 
+nodes on the right path of the tree. (A, B, C, ...)
+How to maintain the path?
+Let's look at the property of the nodes.
+A is the biggest among nums[0] ~ nums[i - 1].
+B is the biggest for the numbers between A and nums[i] (exclusive).
+C is the biggest for the numbers between B and nums[i] (exclusive).
+Let's use a stack, and assume that the content of the stack contains the "right path" of 
+nodes before the node for the current number.
+For the node of the new number, we should remove the nodes in the stack which are smaller 
+than the current number.
+So we pop the stack until the top element of the stack is greater than the current number.
+Then, add the node for the current number to the stack.
+ */
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        Deque<TreeNode> dq = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++) {
+            TreeNode curr = new TreeNode(nums[i]);
+            while(!dq.isEmpty() && dq.peek().val < nums[i]) {
+                curr.left = dq.pop();
+            }
+            if(!dq.isEmpty()) {
+                dq.peek().right = curr;
+            }
+            dq.push(curr);
+        }
+        
+        return dq.isEmpty() ? null : dq.removeLast();
+    }
+    //[1]--
+    //[2,1]--> 
+    //O(n^2)/ O(n), if sorted, O(nlgn)/O(lgn)
+    public TreeNode constructMaximumBinaryTree2(int[] nums) {
+        if (nums == null || nums.length < 1) return null;
+        return helper(nums, 0, nums.length -1);
+    }
+    
+    private TreeNode helper(int[] nums, int start, int end) {
+        if (start > end) return null;
+        else if (start == end) return new TreeNode(nums[start]);
+        int index = start;
+        for(int i = start; i<=end; i++) {
+            if (nums[i] > nums[index]) {
+                index = i;
+            }
+        }
+        TreeNode root = new TreeNode(nums[index]);
+        root.left = helper(nums, start, index-1);
+        root.right = helper(nums, index+1, end);
+        return root;
+    }
+}
---
+++ src/leetcode/RemoveKDigits.java
@@ -0,0 +1,79 @@
+package leetcode;
+import java.util.*;
+public class RemoveKDigits {
+/*
+402. Remove K Digits
+given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+
+Note:
+The length of num is less than 10002 and will be ≥ k.
+The given num does not contain any leading zero.
+Example 1:
+
+Input: num = "1432219", k = 3
+Output: "1219"
+Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+Example 2:
+
+Input: num = "10200", k = 1
+Output: "200"
+Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+*/
+    
+    //O(n)
+    public String removeKdigits(String num, int k) {
+        int digits = num.length() - k;
+        char[] stk = new char[num.length()];
+        int top = 0;
+        // k keeps track of how many characters we can remove
+        // if the previous character in stk is larger than the current one
+        // then removing it will get a smaller number
+        // but we can only do so when k is larger than 0
+        for (int i = 0; i < num.length(); ++i) {
+            char c = num.charAt(i);
+            while (top > 0 && stk[top-1] > c && k > 0) {
+                top -= 1;
+                k -= 1;
+            }
+            stk[top++] = c;
+        }
+        // find the index of first non-zero digit
+        int idx = 0;
+        while (idx < digits && stk[idx] == '0') idx++;
+        return idx == digits? "0": new String(stk, idx, digits - idx);
+    }
+    
+    public String removeKdigits2(String num, int k) {
+        int len = num.length();
+        // corner case
+        if (k == len)
+            return "0";
+
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        while (i < num.length()) {
+            // whenever meet a digit which is less than the previous digit, discard the
+            // previous one
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+        // corner case like "1111"
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        // construct the number from the stack
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty())
+            sb.append(stack.pop());
+        sb.reverse();
+        // remove all the 0 at the head
+        while (sb.length() > 1 && sb.charAt(0) == '0')
+            sb.deleteCharAt(0);
+        return sb.toString();
+    }
+}
