15bb4de6f570eda05e87cd56df027f242f03c9b5
Denny <duqiang@192.168.0.100>
2018-12-14 14:59:15 -0800

    more problems and solutions using fast and slow pointers

--- src/leetcode/CircularArrayLoop.java
+++ src/leetcode/CircularArrayLoop.java
@@ -14,42 +14,78 @@ public class CircularArrayLoop {
      * 2 -> 3 -> 0.
      * 
      * Example 2: Given the array [-1, 2], there is no loop.
-     * 
-     * 
      */
-    public boolean circularArrayLoop(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                continue;
-            }
-            // slow/fast pointer
-            int j = i, k = getIndex(i, nums);
-            while (nums[k] * nums[i] > 0 && nums[getIndex(k, nums)] * nums[i] > 0) {
-                if (j == k) {
-                    // check for loop with only one element
-                    if (j == getIndex(j, nums)) {
-                        break;
-                    }
-                    return true;
-                }
-                j = getIndex(j, nums);
-                k = getIndex(getIndex(k, nums), nums);
-            }
-            // loop not found, set all element along the way to 0
-            j = i;
-            int val = nums[i];
-            while (nums[j] * val > 0) {
-                int next = getIndex(j, nums);
-                nums[j] = 0;
-                j = next;
-            }
-        }
-        return false;
+    //thinking process: given a array, the array tail and head could be connected. 
+    
+    // 
+    int len;
+    /**
+     * Moves the pointer 'i' ahead one iteration.
+     */
+    private int next(int[] nums, int i) {
+        i += nums[i];
+        if (i < 0) i += len;
+        else if (i > len - 1) i %= len;
+        return i;
     }
     
-    public int getIndex(int i, int[] nums) {
-        int n = nums.length;
-        return i + nums[i] >= 0? (i + nums[i]) % n: n + ((i + nums[i]) % n);
+    public boolean circularArrayLoop(int[] nums) {
+        // Handle bad input
+        if (nums == null || nums.length < 2) return false;
+        
+        len = nums.length;
+        
+        /**
+         * Check every possible start location.
+         * We may start at a short-loop, for instance, but the Array
+         * may still contain a valid loop.
+         */
+        for (int i = 0; i < len; i++) {
+            /**
+             * We set elements to 0 which are on known non-loop paths.
+             * So, if we encounter a 0, we know we're not on a loop path.
+             * So, move to the next start location in the list.
+             */
+            if (nums[i] == 0) continue;
+            
+            // Stagger our starts, so we don't conclude we've found a loop,
+            // as we might otherwise when slow == fast.
+            int slow = i, fast = next(nums, slow);
+            
+            /** 
+             * Whether i is positive or negative defines our direction, so if
+             * the directions differ, so too will the signs.
+             * If the signs differ, we can't be in a 'forward' or a 'backward'
+             * loop, so we exit the traverse.
+             */
+            while (nums[i] * nums[fast] > 0 &&
+                    nums[i] * nums[next(nums, fast)] > 0) {
+                if (slow == fast) {
+                    if (slow == next(nums, slow)) break; // 1-element loop
+                    return true;
+                }
+                slow = next(nums, slow);
+                //fast is moving at 2x
+                fast = next(nums, next(nums, fast));
+            }
+            
+            /**
+             * If we're here, we didn't find a loop, so we know this path
+             * doesn't have a loop, so we re-traverse it until we reverse
+             * direction or encounter a '0' element.
+             * During the re-traverse, we set each element we see to 0.
+             */
+            slow = i;
+            int sgn = nums[i];
+            while (sgn * nums[slow] > 0) {
+                int tmp = next(nums, slow);
+                nums[slow] = 0;
+                slow = tmp;
+            }
+        }
+        
+        // We've tested the whole array and have not found a loop,
+        // therefore there isn't one, so return false.
+        return false;
     }
 }
