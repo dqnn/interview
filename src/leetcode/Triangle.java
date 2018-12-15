795be5d4d6eaf7153053745e12f5234b727dff0c
Denny <duqiang@192.168.0.100>
2018-12-14 17:46:00 -0800

    more comments

--- src/leetcode/Triangle.java
+++ src/leetcode/Triangle.java
@@ -12,7 +12,8 @@ import java.util.List;
  */
 public class Triangle {
     /**
-     * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers
+     * Given a triangle, find the minimum path sum from top to bottom. 
+     * Each step you may move to adjacent numbers
      * on the row below.
 
      For example, given the following triangle
