7ce573be47e9d9ec6a34d5c88e6543ee5725608f
Denny <duqiang@192.168.0.100>
2018-12-16 12:15:32 -0800

    more comments

--- src/leetcode/MedianofTwoSortedArrays.java
+++ src/leetcode/MedianofTwoSortedArrays.java
@@ -13,7 +13,8 @@ public class MedianofTwoSortedArrays {
     /**
      * There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
-     Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+     Find the median of the two sorted arrays. The overall run time 
+     complexity should be O(log (m+n)).
 
      Example 1:
      nums1 = [1, 3]
