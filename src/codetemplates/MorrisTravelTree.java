618f79e6a42b76e8faec3923a21bd8d26a239009
Denny <duqiang@192.168.0.101>
2018-12-30 13:47:39 -0800

    more comments on Morrios travel

--- src/codetemplates/MorrisTravelTree.java
+++ src/codetemplates/MorrisTravelTree.java
@@ -20,7 +20,17 @@ public class MorrisTravelTree {
     void morrisTraversalPreorder(){
         morrisTraversalPreorder(root);
     }
- 
+/*
+二、前序遍历
+前序遍历与中序遍历相似，代码上只有一行不同，不同就在于输出的顺序。
+
+步骤：
+1. 如果当前节点的左孩子为空，则输出当前节点并将其右孩子作为当前节点。
+2. 如果当前节点的左孩子不为空，在当前节点的左子树中找到当前节点在中序遍历下的前驱节点。
+   a) 如果前驱节点的右孩子为空，将它的右孩子设置为当前节点。输出当前节点（在这里输出，这是与中序遍历唯一一点不同）。当前节点更新为当前节点的左孩子。
+   b) 如果前驱节点的右孩子为当前节点，将它的右孩子重新设为空。当前节点更新为当前节点的右孩子。
+3. 重复以上1、2直到当前节点为空。
+ */
     // Preorder traversal without recursion and without stack
     void morrisTraversalPreorder(Node node) {
         while (node != null) {
@@ -30,7 +40,7 @@ public class MorrisTravelTree {
                 System.out.print(node.data + " ");
                 node = node.right;
             } else {
-                // Find preorder predecessor
+                // Find inorder predecessor for node
                 Node current = node.left;
                 while (current.right != null && current.right != node) {
                     current = current.right;
