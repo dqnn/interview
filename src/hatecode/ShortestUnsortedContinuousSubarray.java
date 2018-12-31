62ba8b6da12aee4b0b81249b24baf62e3ac64ac4
Denny <duqiang@192.168.0.101>
2018-12-30 19:24:34 -0800

    add more problems and solutions

---
+++ src/hatecode/ShortestUnsortedContinuousSubarray.java
@@ -0,0 +1,34 @@
+package hatecode;
+import java.util.*;
+
+public class ShortestUnsortedContinuousSubarray {
+/*
+581. Shortest Unsorted Continuous Subarray
+Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+
+You need to find the shortest such subarray and output its length.
+
+Example 1:
+Input: [2, 6, 4, 8, 10, 9, 15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+*/
+    public int findUnsortedSubarray(int[] nums) {
+        //edge case
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+        
+        int len = nums.length;
+        int[] numsSorted = Arrays.copyOf(nums, len);
+        Arrays.sort(numsSorted);
+        
+        int l = 0, r = len - 1;
+        
+        while ((l <= len - 1) && nums[l] == numsSorted[l]) l++;
+        while (r >= 0 && nums[r] == numsSorted[r]) r --;
+        
+        return r > l ? r -l + 1 : 0;
+        
+    }
+}
