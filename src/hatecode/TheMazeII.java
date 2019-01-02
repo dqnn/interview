b38fc8213ae1022fe2d74fe38fda101bb3526f6f
Denny <duqiang@192.168.0.101>
2019-01-02 01:52:25 -0800

    more comments and solutions

--- src/hatecode/TheMazeII.java
+++ src/hatecode/TheMazeII.java
@@ -82,15 +82,17 @@ Output: 12
     }
     public int shortestDistance2(int[][] maze, int[] start, int[] destination) {
         int m=maze.length, n=maze[0].length;
-        int[][] length=new int[m][n]; // record length
-        for (int i=0;i<m*n;i++) length[i/n][i%n]=Integer.MAX_VALUE;
+        int[][] dist = new int[m][n]; // record length
+        for (int i=0;i<m;i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
         int[][] dir=new int[][] {{-1,0},{0,1},{1,0},{0,-1}};
+        
+        //sort asc
         PriorityQueue<Point> list=new PriorityQueue<>((o1,o2)->o1.l-o2.l); // using priority queue
         list.offer(new Point(start[0], start[1], 0));
         while (!list.isEmpty()) {
             Point p=list.poll();
-            if (length[p.x][p.y]<=p.l) continue; // if we have already found a route shorter
-            length[p.x][p.y]=p.l;
+            if (dist[p.x][p.y]<=p.l) continue; // if we have already found a route shorter
+            dist[p.x][p.y]=p.l;
             for (int i=0;i<4;i++) {
                 int xx=p.x, yy=p.y, l=p.l;
                 while (xx>=0 && xx<m && yy>=0 && yy<n && maze[xx][yy]==0) {
@@ -104,6 +106,7 @@ Output: 12
                 list.offer(new Point(xx, yy, l));
             }
         }
-        return length[destination[0]][destination[1]]==Integer.MAX_VALUE?-1:length[destination[0]][destination[1]];
+        return dist[destination[0]][destination[1]]
+                ==Integer.MAX_VALUE?-1:dist[destination[0]][destination[1]];
     }
 }
