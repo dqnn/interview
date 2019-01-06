14f898674bf3409d4fb116961d03d7cefff8b9e6
Denny <duqiang@192.168.1.114>
2019-01-06 04:12:12 -0800

    more problems and solutions

--- src/hatecode/BasicCalculatorIV.java
+++ src/hatecode/BasicCalculatorIV.java
@@ -114,104 +114,105 @@ against average/general test cases.
 /**
     @Term represents a list of variables: example: a * a * b * c * d -> [a, a, b, c, d]
 */
- private static class Term implements Comparable<Term> {
-    List<String> vars;
-    static final Term C = new Term(Arrays.asList()); // this is the term for pure numbers
+    private static class Term implements Comparable<Term> {
+        List<String> vars;
+        static final Term C = new Term(Arrays.asList()); // this is the term for pure numbers
 
-    Term(List<String> vars) {
-        this.vars = vars;
-    }
-        
-    public int hashCode() {
-        return vars.hashCode();
-    }
-        
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-            
-        if (!(obj instanceof Term)) return false;
-            
-        Term other = (Term)obj;
-        
-        return this.vars.equals(other.vars);
-    }
-        
-    public String toString() {
-        return String.join("*", vars);
-    }
-    /**
-    Compares two Terms according to the following:
-    1. returns 0 if the two Terms has the same number of variables and all 
-    corresponding variables are the same.
-    2. return +ve if either:
-        i. @that term has more variables than @this term, otherwise,
-        ii. if @this term is lexicorgraphically smaller than @that
-    3. return -ve otherwise.
-*/
-    public int compareTo(Term other) {
-        if (this.vars.size() != other.vars.size()) {
-            return Integer.compare(other.vars.size(), this.vars.size());
-        } else {
-            for (int i = 0; i < this.vars.size(); i++) {
-                int cmp = this.vars.get(i).compareTo(other.vars.get(i));
-                if (cmp != 0) return cmp;
-            }
+        Term(List<String> vars) {
+            this.vars = vars;
         }
-        return 0;
-    }
-}
-    //stands for C1 * Term1 + C2 * Term2,  C1, C2 表示系数
-private static class Expression {
-    Map<Term, Integer> terms;
-    
-    Expression(Term term, int coeff) {
-        terms = new HashMap<>();
-        terms.put(term, coeff);
-    }
-    /** Adds a term to this expression:
-    1- If the term already exists in this expression then its coefficient 
-    is updated (added to the new coeeficient)
-    2- If the coefficient is/becomes zero then this term is removed from the expression
-*/
-    void addTerm(Term term, int coeff) {
-        terms.put(term, coeff + terms.getOrDefault(term, 0));
-    }
-}
 
-private Term merge(Term term1, Term term2) {
-    List<String> vars = new ArrayList<>();
-        
-    vars.addAll(term1.vars);
-    vars.addAll(term2.vars);
-    Collections.sort(vars);
-        
-    return new Term(vars);
-}
+        public int hashCode() {
+            return vars.hashCode();
+        }
+
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (!(obj instanceof Term)) return false;
+
+            Term other = (Term) obj;
+            return this.vars.equals(other.vars);
+        }
+
+        public String toString() {
+            return String.join("*", vars);
+        }
+
+        /**
+         * Compares two Terms according to the following: 1. returns 0 if the two Terms
+         * has the same number of variables and all corresponding variables are the
+         * same. 2. return +ve if either: i. @that term has more variables than @this
+         * term, otherwise, ii. if @this term is lexicorgraphically smaller than @that
+         * 3. return -ve otherwise.
+         */
+        public int compareTo(Term other) {
+            if (this.vars.size() != other.vars.size()) {
+                return Integer.compare(other.vars.size(), this.vars.size());
+            } else {
+                for (int i = 0; i < this.vars.size(); i++) {
+                    int cmp = this.vars.get(i).compareTo(other.vars.get(i));
+                    if (cmp != 0)
+                        return cmp;
+                }
+            }
+            return 0;
+        }
+    }
+    //stands for C1 * Term1 + C2 * Term2,  C1, C2 表示系数
+    private class Expression {
+        Map<Term, Integer> terms;
+
+        Expression(Term term, int coeff) {
+            terms = new HashMap<>();
+            terms.put(term, coeff);
+        }
+
+        /**
+         * Adds a term to this expression: 1- If the term already exists in this
+         * expression then its coefficient is updated (added to the new coeeficient) 2-
+         * If the coefficient is/becomes zero then this term is removed from the
+         * expression
+         */
+        void addTerm(Term term, int coeff) {
+            terms.put(term, coeff + terms.getOrDefault(term, 0));
+        }
+    }
+
+    private Term merge(Term term1, Term term2) {
+        List<String> vars = new ArrayList<>();
+
+        vars.addAll(term1.vars);
+        vars.addAll(term2.vars);
+        Collections.sort(vars);
+
+        return new Term(vars);
+    }
 /** Add terms of the @ex expression to the current expression
 Example: Ex1 = C1 * Term1  + C2 * Term2 + C3 * Term3, 
          Ex2 = C4 * Term2  + C5 * Term8 + C6 * Term9
          Result = C1 * Term1  + (C2 + C4) * Term2 + C3 * Term3 + C5 * Term8  + C6 * Term9
 */
-private Expression add(Expression expression1, Expression expression2, int sign) {
-    for (Map.Entry<Term, Integer> e : expression2.terms.entrySet()) {
-        expression1.addTerm(e.getKey(), sign * e.getValue());
-    }
-    
-    return expression1;
-}
-// multiply two expressions: ex: @a = C1 * Term1, @b = C2 * Term2 
-//--> @ex = (C1 * C2) * (Term1 * Term2)
-private Expression mult(Expression expression1, Expression expression2) {
-    Expression res = new Expression(Term.C, 0);
-        
-    for (Map.Entry<Term, Integer> e1 : expression1.terms.entrySet()) {
-        for (Map.Entry<Term, Integer> e2 : expression2.terms.entrySet()) {
-            res.addTerm(merge(e1.getKey(), e2.getKey()), e1.getValue() * e2.getValue());
+    private Expression add(Expression expression1, Expression expression2, int sign) {
+        for (Map.Entry<Term, Integer> e : expression2.terms.entrySet()) {
+            expression1.addTerm(e.getKey(), sign * e.getValue());
         }
+
+        return expression1;
     }
-    
-    return res;
-}
+
+    // multiply two expressions: ex: @a = C1 * Term1, @b = C2 * Term2
+    // --> @ex = (C1 * C2) * (Term1 * Term2)
+    private  Expression mult(Expression expression1, Expression expression2) {
+        Expression res = new Expression(Term.C, 0);
+
+        for (Map.Entry<Term, Integer> e1 : expression1.terms.entrySet()) {
+            for (Map.Entry<Term, Integer> e2 : expression2.terms.entrySet()) {
+                res.addTerm(merge(e1.getKey(), e2.getKey()), e1.getValue() * e2.getValue());
+            }
+        }
+
+        return res;
+    }
 
     private Expression calculate(String s, Map<String, Integer> map) {
         Expression l1 = new Expression(Term.C, 0);
@@ -247,18 +248,14 @@ private Expression mult(Expression expression1, Expression expression2) {
                 l2 = mult(l2, new Expression(term, num));
             } else if (c == '(') { // this is a subexpression
                 int j = i;
-
+                //to find the ")" closure to recursive calculate
                 for (int cnt = 0; i < s.length(); i++) {
-                    if (s.charAt(i) == '(')
-                        cnt++;
-                    if (s.charAt(i) == ')')
-                        cnt--;
-                    if (cnt == 0)
-                        break;
+                    if (s.charAt(i) == '(')  cnt++;
+                    if (s.charAt(i) == ')')  cnt--;
+                    if (cnt == 0) break;
                 }
                 //recursive to calc the expressions in ()
                 l2 = mult(l2, calculate(s.substring(j + 1, i), map));
-
             } else if (c == '+' || c == '-') { // level one operators
                 l1 = add(l1, l2, o1);
                 o1 = (c == '+' ? 1 : -1);
@@ -266,31 +263,40 @@ private Expression mult(Expression expression1, Expression expression2) {
                 l2 = new Expression(Term.C, 1);
             }
         }
-
         return add(l1, l2, o1);
     }
 
-private List<String> format(Expression expression) {
-    List<Term> terms = new ArrayList<>(expression.terms.keySet());
-    Collections.sort(terms);
-    List<String> res = new ArrayList<>(terms.size());
+    private  List<String> format(Expression expression) {
+        List<Term> terms = new ArrayList<>(expression.terms.keySet());
+        Collections.sort(terms);
+        List<String> res = new ArrayList<>(terms.size());
 
-    for (Term term : terms) {
-        int coeff = expression.terms.get(term);
-        if (coeff == 0) continue;
-        res.add(coeff + (term.equals(Term.C) ? "" : "*" + term.toString()));
+        for (Term term : terms) {
+            int coeff = expression.terms.get(term);
+            if (coeff == 0) continue;
+            res.add(coeff + (term.equals(Term.C) ? "" : "*" + term.toString()));
+        }
+
+        return res;
     }
 
-    return res;
-}
+    public  List<String> basicCalculatorIV(String expression, String[] evalvars, int[] evalints) {
+        Map<String, Integer> map = new HashMap<>();
 
-public List<String> basicCalculatorIV(String expression, String[] evalvars, int[] evalints) {
-    Map<String, Integer> map = new HashMap<>();
-        
-    for (int i = 0; i < evalvars.length; i++) {
-        map.put(evalvars[i], evalints[i]);
+        for (int i = 0; i < evalvars.length; i++) {
+            map.put(evalvars[i], evalints[i]);
+        }
+
+        return format(calculate(expression, map));
     }
+    
+    public static void main(String[] args) {
+        BasicCalculatorIV s= new BasicCalculatorIV();
+        String in = "e + 8 - a + 5";
+        String[] evalvars = {"e"};
+        int[] evalints = {1};
         
-    return format(calculate(expression, map));
-}
+        System.out.println(s.basicCalculatorIV(in, evalvars, evalints));
+    }
+    
 }
