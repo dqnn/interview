743ed772ea56cee9ab93c40a59256a11040966b4
Denny <duqiang@192.168.0.101>
2019-01-20 22:09:07 -0800

    more solutions but needs comments

---
+++ src/hatecode/RectangleAreaII.java
@@ -0,0 +1,62 @@
+package hatecode;
+import java.util.*;
+public class RectangleAreaII {
+/*
+850. Rectangle Area II
+We are given a list of (axis-aligned) rectangles.  Each rectangle[i] = [x1, y1, x2, y2] , where (x1, y1) are the coordinates of the bottom-left corner, and (x2, y2) are the coordinates of the top-right corner of the ith rectangle.
+
+Find the total area covered by all rectangles in the plane.  Since the answer may be too large, return it modulo 10^9 + 7.
+*/
+    class Point {
+        int x, y, val;
+        Point(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+    }
+    public int rectangleArea(int[][] rectangles) {
+        int M = 1000000007;
+        List<Point> data = new ArrayList<>();
+        for (int[] r : rectangles) {
+            data.add(new Point(r[0], r[1], 1));
+            data.add(new Point(r[0], r[3], -1));
+            data.add(new Point(r[2], r[1], -1));
+            data.add(new Point(r[2], r[3], 1));
+        }
+        Collections.sort(data, (a, b) -> {
+            if (a.x == b.x) {
+                return b.y - a.y;
+            }
+            return a.x - b.x;
+        });
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int preX = -1;
+        int preY = -1;
+        int result = 0;
+        for (int i = 0; i < data.size(); i++) {
+            Point p = data.get(i);
+            map.put(p.y, map.getOrDefault(p.y, 0) + p.val);
+            if (i == data.size() - 1 || data.get(i + 1).x > p.x) {
+                if (preX > -1) {
+                    result += ((long)preY * (p.x - preX)) % M;
+                    result %= M;
+                }
+                preY = calcY(map);
+                preX = p.x;
+            }
+        }
+        return result;
+    }
+    private int calcY(TreeMap<Integer, Integer> map) {
+        int result = 0, pre = -1, count = 0;
+        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+            if (pre >= 0 && count > 0) {
+                result += e.getKey() - pre;
+            }
+            count += e.getValue();
+            pre = e.getKey();
+        }
+        return result;
+    }
+}
