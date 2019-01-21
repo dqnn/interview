82a218bc8adf80b5a501e92e2c2f9120f49c2242
Denny <duqiang@192.168.0.101>
2019-01-20 19:04:53 -0800

    more comments on dp formula

--- src/hatecode/RaceCar.java
+++ src/hatecode/RaceCar.java
@@ -71,34 +71,42 @@ private int racecar(int i, int[] dp) {
     
   //DP, O(t^2)/O(t)
   //m[t][d] : min steps to reach target and facing d (0 = right, 1 = left). speed direction
-  private static int[][] m;
+  private static int[][] dp;
   public int racecar_DP2D(int target) {
-    if (m == null) {
+    if (dp == null) {
       //target will be less than 10000
       int kMaxT = 10000;
-      m = new int[kMaxT + 1][2];
+      dp = new int[kMaxT + 1][2];
       for (int t = 1; t <= kMaxT; ++t) {
         int n = (int)Math.ceil(Math.log(t + 1) / Math.log(2));
         //the position would be 0 ,1, 3, 7, 15, 31... 2^n - 1. n means steps
-        //so this would mean we reached the target
-        if (1 << n == t + 1) {
-          m[t][0] = n;
-          //if we face left means we have turned around, 
-          m[t][1] = n + 1;
+        //so this would mean we reached the target, every possible 2^t how many possible combinations
+        //it is very likely coin changes problems
+        if ((1 << n) - 1 == t) {
+          dp[t][0] = n;
+          //if we face left means we have turned around at position target, position not 
+          //change but just turn around, so need to +1
+          dp[t][1] = n + 1;
           continue;
         }
+        //l means the distance between t closet 2^n, t<= 2^n - 1, so l >=0, and means
+       //            |-> l<-|
+        //|----------|------|
+       //            t      2^n - 1
+        //how many steps can reach this part of distance
         int l = (1 << n) - 1 - t;
-        m[t][0] = n + 1 + Math.min(m[l][1], m[l][0] + 1);
-        m[t][1] = n + 1 + Math.min(m[l][0], m[l][1] + 1);
+        //dp[l][1] means at l position face left, 
+        dp[t][0] = n + 1 + Math.min(dp[l][1], dp[l][0] + 1);
+        dp[t][1] = n + 1 + Math.min(dp[l][0], dp[l][1] + 1);
         for (int i = 1; i < t; ++i) 
           for (int d = 0; d <= 1; d++)
             //m[t][d] = min(m[i][0] + 2 +m[t-i][d], m[i][1] + 1 + m[t-i][d])
-            m[t][d] = Math.min(m[t][d], Math.min(
-                m[i][0] + 2 + m[t - i][d],
-                m[i][1] + 1 + m[t - i][d]));
+            dp[t][d] = Math.min(dp[t][d], Math.min(
+                dp[i][0] + 2 + dp[t - i][d],
+                dp[i][1] + 1 + dp[t - i][d]));
       }
     } 
-    return Math.min(m[target][0], m[target][1]);
+    return Math.min(dp[target][0], dp[target][1]);
   }
     
     //BSF solution, O(target * log(target))/O(target * log(target))
