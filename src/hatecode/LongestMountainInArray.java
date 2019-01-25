94cf2f97772be4b3b85a5d91e0e468190e234ffa
Denny <duqiang@10.85.120.217>
2019-01-25 03:38:32 -0800

    more problems and solutions

---
+++ src/hatecode/LongestMountainInArray.java
@@ -0,0 +1,63 @@
+package hatecode;
+public class LongestMountainInArray {
+/*
+845. Longest Mountain in Array
+Let's call any (contiguous) subarray B (of A) a mountain if the following properties hold:
+
+B.length >= 3
+There exists some 0 < i < B.length - 1 such that B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]
+(Note that B could be any subarray of A, including the entire array A.)
+
+Given an array A of integers, return the length of the longest mountain. 
+
+Return 0 if there is no mountain.
+
+Example 1:
+
+Input: [2,1,4,7,3,2,5]
+Output: 5
+Explanation: The largest mountain is [1,4,7,3,2] which has length 5.
+*/
+    //O(n)/O(n)
+    //thinking process: two pass, like previous histrom problem, we know whatis going to happen in future,so we know
+    //at index what we can process
+    public int longestMountain2(int[] A) {
+        int N = A.length, res = 0;
+        int[] up = new int[N], down = new int[N];
+        //we scan from right to left, down means decrease, 
+        for (int i = N - 2; i >= 0; --i) if (A[i] > A[i + 1]) down[i] = down[i + 1] + 1;
+        //we scan from left to right, increase 
+        for (int i = 0; i < N; ++i) {
+            if (i > 0 && A[i] > A[i - 1]) up[i] = up[i - 1] + 1;
+            //and we know from i, there is increase and decrease from i, so we can know the length of the array
+            if (up[i] > 0 && down[i] > 0) res = Math.max(res, up[i] + down[i] + 1);
+        }
+        return res;
+    }
+    //O(n)/O(1), this is interview best, but not easy to maintain
+    public int longestMountain(int[] A) {
+        int N = A.length;
+        int ans = 0, base = 0;
+        while (base < N) {
+            int end = base;
+            // if base is a left-boundary, this is the templare to look for the peak
+            if (end + 1 < N && A[end] < A[end + 1]) {
+                // set end to the peak of this potential mountain
+                while (end + 1 < N && A[end] < A[end + 1]) end++;
+
+                // if end is really a peak..
+                if (end + 1 < N && A[end] > A[end + 1]) {
+                    // set end to the right-boundary of mountain
+                    while (end + 1 < N && A[end] > A[end + 1]) end++;
+                    // record candidate answer
+                    ans = Math.max(ans, end - base + 1);
+                }
+            }
+
+            base = Math.max(end, base + 1);
+        }
+
+        return ans;
+    }
+    
+}
---
+++ src/hatecode/RedundantConnectionII.java
@@ -0,0 +1,106 @@
+package hatecode;
+import java.util.*;
+public class RedundantConnectionII {
+    static class DUS {
+        int[] parent;
+        int[] size;
+        public DUS(int size) {
+            this.parent = new int[size];
+            this.size = new int[size];
+            for(int i = 0; i< size;i++) parent[i] = i;
+            Arrays.fill(this.size, 1);
+        }
+        
+        public int find(int x) {
+            while(x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+        
+        public boolean union(int x, int y) {
+            x = find(x);
+            y = find(y);
+            if (x == y) return false;
+            if (size[x] < size[y]) {
+                parent[x] = y;
+                size[y] += size[x];
+            } else {
+                parent[y] = x;
+                size[x] += size[y];
+            }
+            return true;
+        }
+    }
+    //O(n) 2 pass, 
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        if (edges == null || edges.length < 1) return new int[]{};
+        
+        // 因为可能有节点存在两个父亲节点，此时答案必定在这两条边之中
+        int[] candidate1 = new int[]{-1, -1};
+        int[] candidate2 = new int[]{-1, -1};
+        int N = edges.length;
+        // 记录各个节点的父亲节点
+        int[] parent = new int[N + 1];
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            // 如果节点 v 的父亲不唯一，则记录下两个可能的答案
+            if (parent[v] != 0) {
+                candidate1 = new int[]{parent[v], v};
+                candidate2 = new int[]{u, v};
+                // 无效化第二条边 (trick)
+                edge[0] = -1;
+                edge[1] = -1;
+            }
+            parent[v] = u;
+        }
+
+        // Do Union Find
+        DUS dus = new DUS(N + 1);
+        for (int[] edge : edges) {
+            // 如果遇到被无效化的边直接跳过
+            if (edge[0] == -1 && edge[1] == -1) {
+                continue;
+            }
+            if (!dus.union(edge[0], edge[1])) {
+                // 如果在无效化了第二条边之后（存在的话）
+                // 仍然遇到了环，那么要么是无重复父亲节点，由该条边产生
+                // 要么是 存在重复父亲节点，并且由 第一条边 产生
+                return candidate1[0] == -1 ? edge : candidate1;
+            }
+        }
+        // 对应 Case2.1 与 Case2.2
+        return candidate2;
+            
+        }
+    
+    //O(n) one pass
+    public int[] findRedundantDirectedConnection2(int[][] edges) {
+        int n = edges.length;
+        int[] parent = new int[n+1], ds = new int[n+1];
+        Arrays.fill(parent, -1);
+        int first = -1, second = -1, last = -1;
+        for(int i = 0; i < n; i++) {
+            int p = edges[i][0], c = edges[i][1];
+            if (parent[c] != -1) {
+                first = parent[c];
+                second = i;
+                continue;
+            }
+            parent[c] = i;
+            
+            int p1 = find(ds, p);
+            if (p1 == c) last = i;
+            else ds[c] = p1;
+        }
+
+        if (last == -1) return edges[second]; // no cycle found by removing second
+        if (second == -1) return edges[last]; // no edge removed
+        return edges[first];
+    }
+    
+    private int find(int[] ds, int i) {
+        return ds[i] == 0 ? i : (ds[i] = find(ds, ds[i]));
+    }
+}
