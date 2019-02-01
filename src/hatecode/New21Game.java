80efcfeed457250e7f20fe41a77e1291447904df
Denny <duqiang@192.168.7.48>
2019-02-01 04:03:06 -0800

    more problems and solutions

---
+++ src/hatecode/New21Game.java
@@ -0,0 +1,99 @@
+package hatecode;
+import java.util.*;
+public class New21Game {
+/*
+ * 837. New 21 Game
+Alice plays the following game, loosely based on the card game "21".
+
+Alice starts with 0 points, and draws numbers while she has less than K points.  During each draw, she gains an integer number of points randomly from the range [1, W], where W is an integer.  Each draw is independent and the outcomes have equal probabilities.
+
+Alice stops drawing numbers when she gets K or more points.  What is the probability that she has N or less points?
+
+Example 1:
+
+Input: N = 21, K = 17, W = 10
+Output: 0.73278
+*/
+     //O(n)/O(n)
+    //thinking process: 
+    //so N, K, W 3 parameters, if score i < K, it will be 100% because we would not stop 
+    //playing the game, if i>=K means there will probability, but since we only 1 chance to play
+    //so the range of scores will be [K, K +W -1], we cannot reach K +W because if we have reached
+    //K we would stop
+    
+    //so if N is not of the scope then it will be N < K, will be 0, N >= K +W - 1, 1
+     public double new21Game2(int N, int K, int W) {
+        if (K <= 0 || N >= K + W -1) return 1;
+        //dp[i] means get points i probability
+         //dp[i] = sum(last W dp values) / W
+        //Wsum means  sum(last W dp values), we can maintain a sliding window with size at most K.
+        double dp[] = new double[N + 1],  Wsum = 1, res = 0;
+        dp[0] = 1;
+        for (int i = 1; i <= N; ++i) {
+            dp[i] = Wsum / W;
+            if (i < K) Wsum += dp[i]; 
+            else res += dp[i];
+            
+            if (i - W >= 0) Wsum -= dp[i - W];
+        }
+        return res;
+    }
+    //thinking process:
+    
+    // sum <= N probability, if sum >=K, then we stop, every time, we would get [1, W], probability for 
+    //each number is same
+    
+    //
+    public double new21Game(int N, int K, int W) {
+        if(N >= K+W-1) return 1.0; // all possibilities of positions after alice stops playing are from [K, K+W-1]
+    
+    double p[] = new double[K+W];
+    double prob = 1/(W+0.0); // single elem probability
+    
+    double prev = 0;
+    //p[i] means sum = i probability
+    p[0] = 1; // Since we start from 0, initialize it to 1
+    
+     //if i <= k,
+     //p[i] = 1/W (p(x-1) + ....p(x-W))
+    for(int i = 1; i <= K; i++) {
+        prev = prev - (i-W-1 >= 0 ? p[i - W -1] : 0) + p[i-1];
+        p[i] = prev*prob;
+    }
+    
+    double req = p[K];
+    
+    // From K+1, we don't add the p[i-1] term here as it is >= K
+    //if i > k,
+     //p[i] = 1/W (p(K-1) + ....p(x-W))
+    for(int i = K+1; i <= N; i++) {
+        prev = prev - (i-W-1 >= 0 ? p[i - W -1] : 0);
+        p[i] = prev * prob;
+        req += p[i];
+        //System.out.println(p[i]);
+    }
+    
+    return req;
+    }
+    //brute force, not correct, just for thinking
+    public double new21Game4(int N, int K, int W) {
+        double res;
+        Set<Integer> list = new HashSet<>();
+        helper(W, K, 0, list);
+        int count = 0; 
+        for (int n : list) {
+            if (n <= N) count++;
+        }
+        res = (double)count / (double)list.size();
+        return res;
+    }
+    public void helper(int W, int K, int val, Set<Integer> set) {
+        if (val >= K) {
+            set.add(val);
+            return;
+        }
+        for (int i = 1; i <= W; i++) {
+            helper(W, K, val + i, set);
+        }
+    }
+}
