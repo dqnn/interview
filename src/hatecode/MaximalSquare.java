c059c4c3be49b26c794bc860ed5a2625b8a7652a
Denny <duqiang@10.85.120.152>
2019-02-14 00:02:58 -0800

    add another solution

--- src/hatecode/MaximalSquare.java
+++ src/hatecode/MaximalSquare.java
@@ -34,7 +34,17 @@ public class MaximalSquare {
     //'1's there, for dp we have several steps. subproblems/result, formual, initilize, compute
     
     //dp[i][j] means square as (i,j) as bottom right coordination, so given 
-    //matrix, the max square in matrix is dp[r-1][c-1]
+    //matrix, the max square in matrix is dp[r-1][c-1], 
+ /*
+ understand the formula
+Square ending at i,j is intersection of
+
+Square ending at i-1,j-1
+Square ending at i-1,j
+Square ending at i,j-1
+So maximum size of square ending at i,j will be minimum of the above three squares plus 1
+  */
+    //O(mn)/O(mn)
     public int maximalSquare(char[][] matrix) {
         if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return 0;
         
@@ -51,5 +61,25 @@ public class MaximalSquare {
             }
         }
         return res * res;
+    }
+    
+    //here is the same solution but space complexity reduce to O(n)
+    public int maximalSquare2(char[][] matrix) {
+        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[] dp = new int[cols + 1];
+        int maxsqlen = 0, prev = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int temp = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j - 1], prev), dp[j]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[j]);
+                } else {
+                    dp[j] = 0;
+                }
+                prev = temp;
+            }
+        }
+        return maxsqlen * maxsqlen;
     }
 }
