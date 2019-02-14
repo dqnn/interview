4de6dc8e61c15391fdc67a6e56a3ad78f93c285b
Denny <duqiang@10.85.120.152>
2019-02-14 02:42:06 -0800

    more solutions and problems

---
+++ src/hatecode/MinimumGeneticMutation.java
@@ -0,0 +1,52 @@
+package hatecode;
+import java.util.*;
+public class  MinimumGeneticMutation {
+/*
+433. Minimum Genetic Mutation
+start: "AACCGGTT"
+end:   "AACCGGTA"
+bank: ["AACCGGTA"]
+
+return: 1
+A gene string can be represented by an 8-character long string, with choices from "A", "C", "G", "T".
+each time we can only change one char
+*/
+    public int minMutation(String start, String end, String[] bank) {
+        if(start.equals(end)) return 0;
+        
+        Set<String> bankSet = new HashSet<>(Arrays.asList(bank));
+        
+        char[] charSet = new char[]{'A', 'C', 'G', 'T'};
+        
+        int level = 0;
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(start);
+        visited.add(start);
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int k = 0; k< size;k++) {
+                String curr = queue.poll();
+                if(curr.equals(end)) return level;
+                
+                char[] currArray = curr.toCharArray();
+                for(int i = 0; i < currArray.length; i++) {
+                    char old = currArray[i];
+                    for(char c: charSet) {
+                        currArray[i] = c;
+                        String next = new String(currArray);
+                        if(!visited.contains(next) && bankSet.contains(next)) {
+                            visited.add(next);
+                            queue.offer(next);
+                        }
+                    }
+                    currArray[i] = old;
+                }
+            }
+            //since we move step and check later, so last step is to + 1
+            level++;
+        }
+        return -1;
+    }
+}
