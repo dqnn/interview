ccef5729fdf80fdd2a3f73ddd291bede3d7ccb85
Denny <duqiang@10.85.120.152>
2019-02-17 03:45:12 -0800

    add more solutions

--- src/hatecode/KClosestPointsToOrigin.java
+++ src/hatecode/KClosestPointsToOrigin.java
@@ -38,9 +38,40 @@ Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
         return res;
     }
     
+    //O(n) solution, quick sort
+    public int[][] kClosest(int[][] points, int K) {
+        int len =  points.length, l = 0, r = len - 1;
+        while (l <= r) {
+            int mid = helper(points, l, r);
+            if (mid == K) break;
+            if (mid < K) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return Arrays.copyOfRange(points, 0, K);
+    }
+
+    private int helper(int[][] A, int l, int r) {
+        int[] pivot = A[l];
+        while (l < r) {
+            while (l < r && compare(A[r], pivot) >= 0) r--;
+            A[l] = A[r];
+            while (l < r && compare(A[l], pivot) <= 0) l++;
+            A[r] = A[l];
+        }
+        A[l] = pivot;
+        return l;
+    }
+
+    private int compare(int[] p1, int[] p2) {
+        return p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+    }
+    
     //O(N) solution, quick sort
     private Random random = new Random();
-    public int[][] kClosest(int[][] points, int K) {
+    public int[][] kClosest_RandomPartition(int[][] points, int K) {
         int start = 0, end = points.length - 1;
         int idx = 0;
         while (start <= end) {
