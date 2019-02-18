57ee30f17657c1e0bf1c6b85807d3ab47ef231a2
Denny <duqiang@10.85.120.152>
2019-02-18 00:47:06 -0800

    more problems and solutions

--- src/hatecode/NetworkDelayTime.java
+++ src/hatecode/NetworkDelayTime.java
@@ -20,65 +20,76 @@ Note:
         //for times array, we 
         Arrays.stream(times).forEach(e->graph.get(e[0]).add(new int[]{e[1],e[2]}));
         
-        int[] disTo = new int[N+1];
-        Arrays.fill(disTo, Integer.MAX_VALUE);
+        int[] distTo = new int[N+1];
+        Arrays.fill(distTo, Integer.MAX_VALUE);
         
         Queue<Integer> q = new LinkedList<>();
         
-        boolean[] onQ = new boolean[N + 1];
+        boolean[] inQueue = new boolean[N + 1];
         q.offer(K);
-        disTo[K] = 0;
-        //onQ[K] = true;
+        distTo[K] = 0;
+        inQueue[K] = true;
         
         while(!q.isEmpty()) {
             int u = q.poll();
-            onQ[u] =false;
+            inQueue[u] =false;
             for(int[] e: graph.get(u)) {
                 int v =e[0], w=e[1];
-                if (disTo[u] != Integer.MAX_VALUE && disTo[u] + w < disTo[v]) {
-                    disTo[v] = disTo[u] + w;
-                    if (!onQ[v]) {
+                if (distTo[u] != Integer.MAX_VALUE && distTo[u] + w < distTo[v]) {
+                    distTo[v] = distTo[u] + w;
+                    if (!inQueue[v]) {
                         q.offer(v);
-                        onQ[v] = true;
+                        inQueue[v] = true;
                     }
                 }
             }
         }
         
-        int res = 0;
-        for(int i = 1; i<=N;i++) res = Math.max(res, disTo[i]);
+        int res = IntStream.range(1, N+1).map(i->distTo[i]).max().getAsInt();
         return res == Integer.MAX_VALUE ? -1 : res;
     }
-    //dp solution, but it is the Bellman ford solution
+
+    // dp solution, but it is the Bellman ford solution
     public int networkDelayTime(int[][] times, int N, int K) {
-    if (times == null || times.length == 0) return -1;
+        if (times == null || times.length == 0) return -1;
 
+        /*
+         * Subproblem: dp(i) represents minimum distance from K to i (iteratively update
+         * dp(i) when we find another shorter distance from K to i)
+         */
+        int[] dp = new int[N + 1];
 
-    /* Subproblem: dp(i) represents minimum distance from K to i (iteratively update dp(i) when we find another shorter distance from K to i)*/
-    int[] dp = new int[N + 1];
+        /*
+         * Base case 1: initialize MAX value as initial minimum distance from K to every
+         * point
+         */
+        /* Base case 2: initialize 0 as min distance to K itself */
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[K] = 0;
 
-    /* Base case 1: initialize MAX value as initial minimum distance from K to every point*/
-    /* Base case 2: initialize 0 as min distance to K itself*/
-    Arrays.fill(dp, Integer.MAX_VALUE);
-    dp[K] = 0;
+        /* traverse all destinations */
+        for (int i = 0; i < N; i++) {
+            for (int[] edge : times) {
+                int u = edge[0], v = edge[1], w = edge[2];
 
-    /* traverse all destinations*/
-    for (int i = 0; i < N; i++) {
-        for (int[] edge : times) {
-            int u = edge[0], v = edge[1], w = edge[2];
-
-            /* if dp[u] (starting point of current edge) has already been visited, and new distance from u to v is smaller than previous saved distance
-            we should update minimum distance from u to v*/
-            if (dp[u] != Integer.MAX_VALUE && dp[u] + w < dp[v]) dp[v] = dp[u] + w;
+                /*
+                 * if dp[u] (starting point of current edge) has already been visited, and new
+                 * distance from u to v is smaller than previous saved distance we should update
+                 * minimum distance from u to v
+                 */
+                if (dp[u] != Integer.MAX_VALUE && dp[u] + w < dp[v])
+                    dp[v] = dp[u] + w;
+            }
         }
-    }
 
-    /* after getting minimum distance (travel time) to all points, we should retrieve the max distance from these minimum distance, 
-     * to ensure that we can travel all points in a specific travel time
-    */
-    int res = IntStream.range(1, N+1).map(i->dp[i]).max().getAsInt();
-    
-    /* if we do not visit all points, we should return -1*/
-    return res == Integer.MAX_VALUE ? -1 : res;
-}         
+        /*
+         * after getting minimum distance (travel time) to all points, we should
+         * retrieve the max distance from these minimum distance, to ensure that we can
+         * travel all points in a specific travel time
+         */
+        int res = IntStream.range(1, N + 1).map(i -> dp[i]).max().getAsInt();
+
+        /* if we do not visit all points, we should return -1 */
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
 }
