1cf6f173122a97f7515cf3c80eb079080b4e5489
Denny <duqiang@10.88.101.33>
2019-02-22 05:40:27 -0800

    add one more problems

---
+++ src/hatecode/TrappingRainWaterII.java
@@ -0,0 +1,71 @@
+package hatecode;
+import java.util.*;
+public class TrappingRainWaterII {
+/*
+407. Trapping Rain Water II
+Given the following 3x6 height map:
+[
+  [1,4,3,1,3,2],
+  [3,2,1,3,2,4],
+  [2,3,3,2,3,1]
+]
+
+Return 4.
+*/
+    public class Cell {
+        int x;
+        int y;
+        int h;
+        public Cell(int x, int y, int h) {
+            this.x = x;
+            this.y = y;
+            this.h = h;
+        }
+    }
+
+    public int trapRainWater(int[][] heights) {
+        if (heights == null || heights.length == 0 || heights[0].length == 0)
+            return 0;
+
+        PriorityQueue<Cell> queue = new PriorityQueue<>((a, b)->(a.h - b.h));
+        
+        int m = heights.length;
+        int n = heights[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        // Initially, add all the Cells which are on borders to the queue.
+        for (int i = 0; i < m; i++) {
+            visited[i][0] = true;
+            visited[i][n - 1] = true;
+            queue.offer(new Cell(i, 0, heights[i][0]));
+            queue.offer(new Cell(i, n - 1, heights[i][n - 1]));
+        }
+
+        for (int i = 0; i < n; i++) {
+            visited[0][i] = true;
+            visited[m - 1][i] = true;
+            queue.offer(new Cell(0, i, heights[0][i]));
+            queue.offer(new Cell(m - 1, i, heights[m - 1][i]));
+        }
+
+        // from the borders, pick the shortest cell visited and check its neighbors:
+        // if the neighbor is shorter, collect the water it can trap and update its height as its height plus the water trapped
+       // add all its neighbors to the queue.
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int res = 0;
+        while (!queue.isEmpty()) {
+            Cell cell = queue.poll();
+            for (int[] dir : dirs) {
+                int i = cell.x + dir[0];
+                int j = cell.y + dir[1];
+                if (i >= 0 && i < m && j >= 0 && j < n && !visited[i][j]) {
+                    visited[i][j] = true;
+                    res += Math.max(0, cell.h - heights[i][j]);
+                    queue.offer(new Cell(i, j, Math.max(heights[i][j], cell.h)));
+                }
+            }
+        }
+        
+        return res;
+    }
+}
