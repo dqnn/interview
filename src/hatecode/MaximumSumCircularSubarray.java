da212dbde5b72ab111c26d249f37ba1abb7db3a3
Denny <duqiang@10.88.101.33>
2019-02-24 19:44:16 -0800

    more comments and solutions

---
+++ src/hatecode/MaximumSumCircularSubarray.java
@@ -0,0 +1,60 @@
+package hatecode;
+import java.util.*;
+public class MaximumSumCircularSubarray {
+/*
+918. Maximum Sum Circular Subarray
+Input: [1,-2,3,-2]
+Output: 3
+Explanation: Subarray [3] has maximum sum 3
+Input: [5,-3,5]
+Output: 10
+Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10
+*/
+    
+    public int maxSubarraySumCircular(int[] A) {
+        int total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
+        for (int a : A) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+
+    
+   public int maxSubarraySumCircular_Solution(int[] A) {
+        int N = A.length;
+
+        // Compute P[j] = B[0] + B[1] + ... + B[j-1]
+        // for fixed array B = A+A
+        int[] P = new int[2*N+1];
+        for (int i = 0; i < 2*N; ++i)
+            P[i+1] = P[i] + A[i % N];
+
+        // Want largest P[j] - P[i] with 1 <= j-i <= N
+        // For each j, want smallest P[i] with i >= j-N
+        int ans = A[0];
+        // deque: i's, increasing by P[i]
+        Deque<Integer> deque = new ArrayDeque();
+        deque.offer(0);
+
+        for (int j = 1; j <= 2*N; ++j) {
+            // If the smallest i is too small, remove it.
+            if (deque.peekFirst() < j-N)
+                deque.pollFirst();
+
+            // The optimal i is deque[0], for cand. answer P[j] - P[i].
+            ans = Math.max(ans, P[j] - P[deque.peekFirst()]);
+
+            // Remove any i1's with P[i2] <= P[i1].
+            while (!deque.isEmpty() && P[j] <= P[deque.peekLast()])
+                deque.pollLast();
+
+            deque.offerLast(j);
+        }
+
+        return ans;
+    }
+}
