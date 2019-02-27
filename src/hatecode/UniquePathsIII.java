59a6eca5aabe074ff669b8cb9fdf6fe267927fc5
Denny <duqiang@10.88.101.33>
2019-02-27 00:49:09 -0800

    add pretty un-mature DP solution

--- src/hatecode/UniquePathsIII.java
+++ src/hatecode/UniquePathsIII.java
@@ -69,4 +69,65 @@ Output: 2
         int m = grid.length, n = grid[0].length;
         return 0 <= i && i < m && 0 <= j && j < n && grid[i][j] >= 0;
     }
+    
+    //DP solution O(N2^N)/O(N)  N =RC
+    class Solution {
+        int ans;
+        int[][] grid;
+        int R, C;
+        int tr, tc, target;
+        int[] dr = new int[]{0, -1, 0, 1};
+        int[] dc = new int[]{1, 0, -1, 0};
+        Integer[][][] memo;
+
+        public int uniquePathsIII(int[][] grid) {
+            this.grid = grid;
+            R = grid.length;
+            C = grid[0].length;
+            target = 0;
+
+            int sr = 0, sc = 0;
+            for (int r = 0; r < R; ++r)
+                for (int c = 0; c < C; ++c) {
+                    if (grid[r][c] % 2 == 0)
+                        target |= code(r, c);
+
+                    if (grid[r][c] == 1) {
+                        sr = r;
+                        sc = c;
+                    } else if (grid[r][c] == 2) {
+                        tr = r;
+                        tc = c;
+                    }
+                }
+
+            memo = new Integer[R][C][1 << R*C];
+            return dp(sr, sc, target);
+        }
+
+        public int code(int r, int c) {
+            return 1 << (r * C + c);
+        }
+
+        public Integer dp(int r, int c, int todo) {
+            if (memo[r][c][todo] != null)
+                return memo[r][c][todo];
+
+            if (r == tr && c == tc) {
+                return todo == 0 ? 1 : 0;
+            }
+
+            int res = 0;
+            for (int k = 0; k < 4; ++k) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                if (0 <= nr && nr < R && 0 <= nc && nc < C) {
+                    if ((todo & code(nr, nc)) != 0)
+                        res += dp(nr, nc, todo ^ code(nr, nc));
+                }
+            }
+            memo[r][c][todo] = res;
+            return res;
+        }
+    }
 }
