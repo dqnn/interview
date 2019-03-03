e652fad20abc4721869b6ee74e6f04f0c08d2cf5
Denny <duqiang@172.31.98.207>
2019-03-02 23:48:42 -0800

    more comments and solutions

--- src/hatecode/AndroidUnlockPatterns.java
+++ src/hatecode/AndroidUnlockPatterns.java
@@ -67,7 +67,10 @@ public class AndroidUnlockPatterns {
         skip[1][7] = skip[7][1] = 4;
         skip[3][9] = skip[9][3] = 6;
         skip[7][9] = skip[9][7] = 8;
-        skip[1][9] = skip[9][1] = skip[2][8] = skip[8][2] = skip[3][7] = skip[7][3] = skip[4][6] = skip[6][4] = 5;
+        skip[1][9] = skip[9][1] 
+                = skip[2][8] = skip[8][2] 
+                = skip[3][7] = skip[7][3] 
+                = skip[4][6] = skip[6][4] = 5;
         boolean[] visited = new boolean[10];
         int res = 0;
         for (int i = m; i <= n; i++) {
@@ -79,11 +82,14 @@ public class AndroidUnlockPatterns {
     }
 
     public int DFS(boolean[] visited, int[][] skip, int cur, int remain) {
+        //
         if (remain < 0) return 0;
+        //find one combinations
         if (remain == 0) return 1;
         visited[cur] = true;
         int res = 0;
         for (int i = 1; i <= 9; i++) {
+            //we have not visited this number or we have visited the middle one
             if (!visited[i] && (skip[cur][i] == 0 || visited[skip[cur][i]])) {
                 res += DFS(visited, skip, i, remain - 1);
             }
