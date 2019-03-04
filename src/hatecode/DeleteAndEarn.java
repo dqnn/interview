b0ff5435a949d023475f0bd55c0a076f2c3bf341
Denny <duqiang@172.31.98.207>
2019-03-04 04:28:14 -0800

    more comments and solutions

--- src/hatecode/DeleteAndEarn.java
+++ src/hatecode/DeleteAndEarn.java
@@ -15,6 +15,22 @@ Input: nums = [3, 4, 2]
 Output: 6
 */
     //O(n)/O(1)
+/*
+If we sort all the numbers into buckets indexed by these numbers, this is essentially asking you to repetitively take an bucket while giving up the 2 buckets next to it. (the range of these numbers is [1, 10000])
+
+The optimal final result can be derived by keep updating 2 variables skip_i, take_i, 
+which stands for:
+skip_i : the best result for sub-problem of first (i+1) buckets from 0 to i, 
+while you skip the ith bucket.
+take_i : the best result for sub-problem of first (i+1) buckets from 0 to i, 
+while you take the ith bucket.
+
+DP formula:
+take[i] = skip[i-1] + values[i];
+skip[i] = Math.max(skip[i-1], take[i-1]);
+take[i] can only be derived from: if you skipped the [i-1]th bucket, and you take bucket[i].
+skip[i] through, can be derived from either take[i-1] or skip[i-1], whatever the bigger;
+ */
      public int deleteAndEarn_DP(int[] nums) {
         int n = 10001;
         int[] values = new int[n];
@@ -37,6 +53,7 @@ Output: 6
          if(n == 0) return 0;
          
          int max = Arrays.stream(nums).max().getAsInt();
+         //dp[i] means 
          int[] dp = new int[max+1];
          //this is bucket, store same number value sum
          
