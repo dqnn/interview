b46fbd114ee8329e1e4200540232d8a6c49d7f56
Denny <duqiang@192.168.0.101>
2019-03-09 21:10:47 -0800

    more comments and solutions

--- src/hatecode/GuessNumberHigherorLowerII.java
+++ src/hatecode/GuessNumberHigherorLowerII.java
@@ -88,7 +88,7 @@ so to guarantee you have enough money to find the target, you need to prepare th
     //and k has to be [i,j-1]
     public int getMoneyAmount2(int n) {
         //dp[i][j] means from range(i...j) the minimal cost,
-        //so if we guess k, then we need to 
+        //so if we guess k, then we need to try to 
         int[][] dp = new int[n + 1][n + 1];
         for (int i = n - 1; i >= 1; i--) {
             for (int j = i + 1; j <= n; j++) {
