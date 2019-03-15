30a315e1d68a1972b59cc8fc39394af50b3a67cb
Denny <duqiang@192.168.0.101>
2019-03-14 19:47:27 -0700

    more comments

--- src/hatecode/OutOfBoundaryPaths.java
+++ src/hatecode/OutOfBoundaryPaths.java
@@ -86,20 +86,20 @@ public class OutOfBoundaryPaths {
         for (int[][] l : memo)
             for (int[] sl : l)
                 Arrays.fill(sl, -1);
-        return findPaths(m, n, N, i, j, memo);
+        return helper(m, n, N, i, j, memo);
     }
 
-    public int findPaths(int m, int n, int N, int i, int j, int[][][] memo) {
+    public int helper(int m, int n, int N, int i, int j, int[][][] memo) {
         if (i == m || j == n || i < 0 || j < 0)
             return 1;
         if (N == 0)
             return 0;
         if (memo[i][j][N] >= 0)
             return memo[i][j][N];
-        memo[i][j][N] = (( findPaths(m, n, N - 1, i - 1, j, memo) 
-                         + findPaths(m, n, N - 1, i + 1, j, memo)) % M
-                         + (findPaths(m, n, N - 1, i, j - 1, memo) 
-                         + findPaths(m, n, N - 1, i, j + 1, memo)) % M) % M;
+        memo[i][j][N] = (( helper(m, n, N - 1, i - 1, j, memo) 
+                         + helper(m, n, N - 1, i + 1, j, memo)) % M
+                         + (helper(m, n, N - 1, i, j - 1, memo) 
+                         + helper(m, n, N - 1, i, j + 1, memo)) % M) % M;
         return memo[i][j][N];
     }
     
