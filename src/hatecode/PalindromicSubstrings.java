b78ed0b7c04bf2270f48585ecc091d754ebd0eb0
Denny <duqiang@192.168.0.101>
2019-03-14 21:53:42 -0700

    more comments and solutions

--- src/hatecode/PalindromicSubstrings.java
+++ src/hatecode/PalindromicSubstrings.java
@@ -50,21 +50,23 @@ The input string length won't exceed 1000.
 
     int count = 0;
 
-    // space : O(1)
-    public int countSubstrings2(String s) {
-        if (s == null || s.length() == 0) return 0;
-        for (int i = 0; i < s.length(); i++) {
-            helper(s, i, i);
-            helper(s, i, i + 1);
+    // space : O(1)/O(n^2)
+    int res = 1;
+    public int countSubstrings_Recursive(String s) {
+        if (s.length() == 0) return 0;
+        
+        for(int i = 0; i< s.length() - 1; i++) {
+            helper(s, i, i); ////To check the palindrome of odd length palindromic sub-string
+            helper(s, i, i+1); ////To check the palindrome of even length palindromic sub-string
         }
-        return count;
+        return res;
     }
-
-    public void helper(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            count++;
-            left--;
-            right++;
+    
+    public void helper(String s, int i, int j) {
+        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j)){    //Check for the palindrome string 
+            res++;    //Increment the count if palindromin substring found
+            i--;    //To trace string in left direction
+            j++;    //To trace string in right direction
         }
     }
     
