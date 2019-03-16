da0c3ac04ef50d9fa449e14510533c7bbb3bd243
Denny <duqiang@192.168.0.101>
2019-03-16 14:59:44 -0700

    more comments and solutions

--- src/hatecode/CherryPickup.java
+++ src/hatecode/CherryPickup.java
@@ -39,6 +39,7 @@ Output: 5
         }
         //we initialize the 1 1 1 same as 0 0 0 if length is n
         dp[1][1][1] = g[0][0];
+        //DP computing is also key, we scan row by row, and we use last result to update next ones
         for(int x1 =1; x1 <=r; x1++) {
             for(int y1 =1; y1 <=r; y1++) {
                 for(int x2= 1; x2<=r;x2++) {
@@ -46,7 +47,12 @@ Output: 5
                     if (dp[x1][y1][x2] > 0 || y2 < 1 || y2 > r || g[x1-1][y1-1] == -1 || g[x2-1][y2-1] == -1) {
                         continue;
                     }
-                    int cur = Math.max(Math.max(dp[x1 - 1][y1][x2], dp[x1 - 1][y1][x2 - 1]), Math.max(dp[x1][y1 - 1][x2], dp[x1][y1 - 1][x2 - 1]));
+                    //for point x1,y1,x2,y2, there are two points, means 1 go to x1,y1, 2 go to x2,y2
+                    //they are independent, just need to check x1 == x2, since x1 + y1 = x2 + y2, so 
+                    //x1,y1 has 2 ways, for x2,y2, it also have 2 ways, but if we fix x1 and y1, then for 2nd
+                    //point, there will be only 1 factor, x2-1, x2
+                    int cur = Math.max(Math.max(dp[x1 - 1][y1][x2], dp[x1 - 1][y1][x2 - 1]), 
+                                       Math.max(dp[x1][y1 - 1][x2], dp[x1][y1 - 1][x2 - 1]));
                     if (cur < 0) continue;
                     
                     dp[x1][y1][x2] = cur + g[x1-1][y1-1];
