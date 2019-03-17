d3bb5afb7f0a407bb96a6b5956400ee949c4b3c9
Denny <duqiang@192.168.0.101>
2019-03-17 05:42:56 -0700

    more comments and solutions

--- src/hatecode/MinimumSwapsToMakeSequencesIncreasing.java
+++ src/hatecode/MinimumSwapsToMakeSequencesIncreasing.java
@@ -17,7 +17,15 @@ Example:
 Input: A = [1,3,5,4], B = [1,2,3,7]
 Output: 1
 */
-    //thinking process: 
+    //thinking process:
+    //A=[1,3,5,4], B = [1,2,3,7]
+/*
+Obviously, swapRecord[0] = 1 and fixRecord[0] = 0.
+
+For i = 1, either swap or fix is OK. So we take the minimum previous result, min = min(swapRecord[0], fixRecord[0]) = 0. swapRecord[1] = min + 1 = 1, fixRecord[1] = min = 0
+For i = 2, notice that A[1] >= B[2], which means the manipulation of A[2] and B[2] should be same as A[1] and B[1], if A[1] and B[1] swap, A[2] and B[2] should swap, vice versa. Make sense, right? So swapRecord[2] = swapRecord[1] + 1 and fixRecord[2] = fixRecord[1]
+For i = 3, notice that A[2] >= A[3], which mean the manipulation of A[3] and B[3] and A[2] and B[2] should be opposite. In this case, swapRecord[3] = fixRecord[2] + 1 and fixRecord[3] = swapRecord[2]
+ */
      public int minSwap(int[] A, int[] B) {
         int swapRecord = 1, fixRecord = 0;
         for (int i = 1; i < A.length; i++) {
