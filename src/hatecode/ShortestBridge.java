0510bcd3a9f8c0ce6c9ac51227b690313c827b78
Denny <duqiang@192.168.0.101>
2019-03-27 03:38:20 -0700

    more comments and solutions

---
+++ src/hatecode/ShortestBridge.java
@@ -0,0 +1,72 @@
+package hatecode;
+import java.util.*;
+public class ShortestBridge {
+/*
+934. Shortest Bridge
+In a given 2D binary array A, there are two islands.  (An island is a 4-directionally connected group of 1s not connected to any other 1s.)
+
+Now, we may change 0s to 1s so as to connect the two islands together to form 1 island.
+
+Return the smallest number of 0s that must be flipped.  (It is guaranteed that the answer is at least 1.)
+
+ 
+
+Example 1:
+
+Input: [[0,1],[1,0]]
+Output: 1
+
+*/
+    //this should be a hard problem
+    public int shortestBridge(int[][] A) {
+        int r = A.length, c = A[0].length;
+        boolean[][] visited = new boolean[r][c];
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Queue<int[]> q = new LinkedList<>();
+        boolean found = false;
+        // 1. dfs to find an island, mark it in `visited`
+        for (int i = 0; i < r; i++) {
+            if (found) {
+                break;
+            }
+            for (int j = 0; j < c; j++) {
+                if (A[i][j] == 1) {
+                    dfs(A, visited, q, i, j, dirs);
+                    found = true;
+                    break;
+                }
+            }
+        }
+        // 2. bfs to expand this island
+        int step = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int[] cur = q.poll();
+                for (int[] dir : dirs) {
+                    int i = cur[0] + dir[0];
+                    int j = cur[1] + dir[1];
+                    if (i >= 0 && j >= 0 && i < r && j < c && !visited[i][j]) {
+                        if (A[i][j] == 1) {
+                            return step;
+                        }
+                        q.offer(new int[]{i, j});
+                        visited[i][j] = true;
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+    private void dfs(int[][] A, boolean[][] visited, Queue<int[]> q, int i, int j, int[][] dirs) {
+        if (i < 0 || j < 0 || i >= A.length || j >= A[0].length || visited[i][j] || A[i][j] == 0) {
+            return;
+        }
+        visited[i][j] = true;
+        q.offer(new int[]{i, j});
+        for (int[] dir : dirs) {
+            dfs(A, visited, q, i + dir[0], j + dir[1], dirs);
+        }
+    }
+}
