be9d6cc63fb699a4870b5c062745a1c9a65cbec6
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-03 05:20:20 -0700

    more comments and solutions

--- src/hatecode/BulbSwitcher.java
+++ src/hatecode/BulbSwitcher.java
@@ -33,6 +33,17 @@ public class BulbSwitcher {
      * @param n
      * @return
      */
+/*
+对于第n个灯泡，只有当次数是n的因子的之后，才能改变灯泡的状态，即n能被当前次数整除，
+比如当n为36时，它的因数有(1,36), (2,18), (3,12), (4,9), (6,6), 
+可以看到前四个括号里成对出现的因数各不相同，括号中前面的数改变了灯泡状态，后面的数又变回去了，
+等于灯泡的状态没有发生变化，只有最后那个(6,6)，在次数6的时候改变了一次状态，
+没有对应其它的状态能将其变回去了，所以灯泡就一直是点亮状态的。所以所有平方数都有这么一个相等的因数对，
+即所有平方数的灯泡都将会是点亮的状态。
+
+那么问题就简化为了求1到n之间完全平方数的个数，我们可以用force brute来比较从1开始的完全平方数和
+n的大小
+ */
     // so thinking about this way 10= 1x10 or 2x5, 1 will be ignored, so there are 3
     // times
     // toggle, so we want to know the even time of toggle, only sqrt number
