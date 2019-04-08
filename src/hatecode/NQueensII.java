904e48194df74ba924a6eb136898a8c01b81b209
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-08 04:39:10 -0700

    more comments and solutions

--- src/hatecode/NQueensII.java
+++ src/hatecode/NQueensII.java
@@ -13,12 +13,17 @@ import java.util.List;
  */
 public class NQueensII {
     /**
-     * Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+     * follow up: Given an integer n, return the number of distinct solutions to the n-queens puzzle.
      *
 
      time : O(n^n)
      space : O(n)
-
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+ 
+ [1,3,0,2] means 1 combination
      * @param n
      * @return
      */
@@ -32,13 +37,15 @@ public class NQueensII {
     用column标记此行之前的哪些column已经放置了queen. 棋盘坐标(row, col)对应column的第col位(LSB --> MSB, 下同).
     用diag标记此位置之前的哪些主对角线已经放置了queen. 棋盘坐标(row, col)对应diag的第(n - 1 + row - col)位.
     用antiDiag标记此位置之前的哪些副对角线已经放置了queen. 棋盘坐标(row, col)对应antiDiag的第(row + col)位.
+    Start row by row, and loop through columns. 
+    At each decision point, skip unsafe positions by using three boolean arrays.
 */
     public int totalNQueens(int n) {
         boolean[] cols = new boolean[n];// columns   |
         // we use one 2 * n dimension to store the 2D data
-        boolean[] d1 = new boolean[2 * n]; // top left -> bottom right
+        boolean[] d1 = new boolean[2 * n]; // top left -> bottom right dialog
      // we use one 2 * n dimension to store the 2D data
-        boolean[] d2 = new boolean[2 * n]; // top right -> bottom left
+        boolean[] d2 = new boolean[2 * n]; // top right -> bottom left, anti-dialogue
         helper(0, cols, d1, d2, n);
         return res;
     }
@@ -50,14 +57,17 @@ public class NQueensII {
             return;
         }
         for (int col = 0; col < n; col++) {
-            // 
+            // dialgue i -j = 0 always
             int id1 = col - row + n;
+            //if it is anti-dialogue, i + j = n - 1, always
             int id2 = col + row;
             if (cols[col] || d1[id1] || d2[id2]) continue;
 
             cols[col] = true; d1[id1] = true; d2[id2] = true;
             helper(row + 1, cols, d1, d2, n);
-            cols[col] = false; d1[id1] = false; d2[id2] = false;
+            cols[col] = false; 
+            d1[id1] = false; 
+            d2[id2] = false;
         }
     }
 }
