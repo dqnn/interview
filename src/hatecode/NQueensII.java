87958f4ffb078ab7389e7e787d8ae37568ec7bcd
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-08 03:01:33 -0700

    more comments and solutions

--- src/hatecode/NQueensII.java
+++ src/hatecode/NQueensII.java
@@ -13,6 +13,7 @@ import java.util.List;
  */
 public class NQueensII {
     /**
+     * Given an integer n, return the number of distinct solutions to the n-queens puzzle.
      *
 
      time : O(n^n)
