b00c315f5b914875e027f912bf434941b96e2df3
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-13 15:02:39 -0700

    more comments and solutions

--- src/hatecode/SuperUglyNumber.java
+++ src/hatecode/SuperUglyNumber.java
@@ -35,6 +35,15 @@ public class SuperUglyNumber {
     // so we do not need to trace back to each prime number so we can accerlerate 
     //the computation, we use res as array progressively to get result
     //this problems just to remember the for and while loop 
+    
+    //also should be small one thats why we use PQ to poll smallest 
+    /*
+     * n = 12, primes = [2,7,13,19], 
+     * first we add [2,1,2], [7,1,7], [13,1,13], [19,1,19] to pq
+     * then we poll first, res[1] = 2, then we add [4, 2, 2] into queue, and found this is smallest then continue
+     * res[2] = 4, then we will add[16, 3, 2],
+     * for each res[i], 
+     */
     public int nthSuperUglyNumber(int n, int[] primes) {
         int[] res = new int[n];
         res[0] = 1;
