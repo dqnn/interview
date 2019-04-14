76efbb6bd200bbc7a82e76d17cd98afdcd40dca5
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-13 17:10:19 -0700

    more comments and solutions

---
+++ src/hatecode/SwimInRisingWater.java
@@ -0,0 +1,52 @@
+package hatecode;
+
+import java.util.*;
+class SwimInRisingWater {
+/*
+778. Swim in Rising Water
+On an N x N grid, each square grid[i][j] represents the elevation at that point (i,j).
+
+Now rain starts to fall. At time t, the depth of the water everywhere is t. You can swim from a square to another 4-directionally adjacent square if and only if the elevation of both squares individually are at most t. You can swim infinite distance in zero time. Of course, you must stay within the boundaries of the grid during your swim.
+
+You start at the top left square (0, 0). What is the least time until you can reach the bottom right square (N-1, N-1)?
+
+Example 1:
+
+Input: [[0,2],[1,3]]
+Output: 3
+Explanation:
+At time 0, you are in grid location (0, 0).
+You cannot go anywhere else because 4-directionally adjacent neighbors have a higher elevation than t = 0.
+
+You cannot reach point (1, 1) until time 3.
+When the depth of water is 3, we can swim anywhere inside the grid.
+
+
+//the problem is to say in a 2D matrix, rain is falling and grid[i][j] means the time the rain needed to covere which means the time you have to wait, then you can across, src->dst, so BFS
+*/
+  
+    //O(n^2 lgn)
+    public int swimInWater(int[][] grid) {
+        if (grid == null || grid.length < 1 || grid[0].length < 1) return 0;
+        
+        int n = grid.length;
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b)->(a[2] - b[2]));
+        boolean[][] visited = new boolean[n][n];
+        
+        q.offer(new int[]{0,0,grid[0][0]});
+        visited[0][0] = true;
+        int[][] dirs = {{-1,0}, {1, 0}, {0, -1}, {0, 1}};
+        while(!q.isEmpty()) {
+            int[] node = q.poll();
+            if (node[0] == n-1 && node[1] == n-1) return node[2];
+            for(int[] dir : dirs) {
+                int x = node[0] + dir[0];
+                int y = node[1] + dir[1];
+                if (x <0 || x>=n || y < 0 || y >=n || visited[x][y]) continue;
+                visited[x][y] = true;
+                q.offer(new int[]{x, y, Math.max(node[2], grid[x][y])});
+            }
+        }
+        return -1;
+    }
+}
