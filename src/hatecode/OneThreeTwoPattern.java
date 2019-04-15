3206938531aa8624433d03d30539eac2ef0c781d
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-14 17:57:57 -0700

    more comments and solutions

--- src/hatecode/OneThreeTwoPattern.java
+++ src/hatecode/OneThreeTwoPattern.java
@@ -13,16 +13,14 @@ Input: [1, 2, 3, 4]
 
 Output: False
 */
-    //interview friendly, 
+    //interview friendly, O(n) 
     public boolean find132pattern(int[] nums) {
         int s3 = Integer.MIN_VALUE;
         Stack<Integer> stack = new Stack<>();
         for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] < s3) {
-                return true;
-            } else {
-                while (!stack.isEmpty() && 
-                       nums[i] > stack.peek()) {
+            if (nums[i] < s3) return true;
+            else {
+                while (!stack.isEmpty() && nums[i] > stack.peek()) {
                     s3 = stack.pop();
                 }
                 stack.push(nums[i]);
