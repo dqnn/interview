371a16551c64fef93dbc4f4702ccf5145937ed94
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-14 17:54:56 -0700

    more comments and solutions

---
+++ src/hatecode/OneThreeTwoPattern.java
@@ -0,0 +1,33 @@
+package hatecode;
+
+import java.util.*;
+class OneThreeTwoPattern {
+/*
+456. 132 Pattern
+Given a sequence of n integers a1, a2, ..., an, a 132 pattern is a subsequence ai, aj, ak such that i < j < k and ai < ak < aj. Design an algorithm that takes a list of n numbers as input and checks whether there is a 132 pattern in the list.
+
+Note: n will be less than 15,000.
+
+Example 1:
+Input: [1, 2, 3, 4]
+
+Output: False
+*/
+    //interview friendly, 
+    public boolean find132pattern(int[] nums) {
+        int s3 = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < s3) {
+                return true;
+            } else {
+                while (!stack.isEmpty() && 
+                       nums[i] > stack.peek()) {
+                    s3 = stack.pop();
+                }
+                stack.push(nums[i]);
+            }
+        }
+        return false;
+    }
+}
