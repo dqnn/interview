d552dbcb2ece89f5d4425cdc132f4bdeae4ca99d
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-16 01:30:14 -0700

    more comments and solutions

--- src/design/RateLimiter.java
+++ src/design/RateLimiter.java
@@ -1,6 +1,9 @@
 package design;
 public abstract class RateLimiter {
-
+/*
+ * dive deep reading:
+ * https://hechao.li/2018/06/27/Rate-Limiter-Part2/
+ */
   protected final int maxRequestPerSec;
 
   protected RateLimiter(int maxRequestPerSec) {
