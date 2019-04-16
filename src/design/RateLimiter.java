ba3f53dd78e4ea50671bf0231a0195f7739f592b
Denny <duqiang@lex3-od-acc-rsw202-fe-0.amazon.com>
2019-04-16 01:26:12 -0700

    some design questions and sample code

---
+++ src/design/RateLimiter.java
@@ -0,0 +1,11 @@
+package design;
+public abstract class RateLimiter {
+
+  protected final int maxRequestPerSec;
+
+  protected RateLimiter(int maxRequestPerSec) {
+    this.maxRequestPerSec = maxRequestPerSec;
+  }
+
+  abstract boolean allow();
+}
