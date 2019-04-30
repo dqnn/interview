107c09084f2cbf10b6328087ce62f7707ce9b057
Denny <qiangddu@gmail.com>
2019-04-30 03:50:48 -0700

    more comments

--- src/hatecode/MaximumAverageSubarrayII.java
+++ src/hatecode/MaximumAverageSubarrayII.java
@@ -21,8 +21,8 @@ Thus return 12.75.
     //into minimum density segments of longest length [hull[i], hull[i+1]-1], and we discard 
     //these segments as appropriate. That is, for each i in increasing order, hull[i+1] is the 
     //largest index in [hull[i], j-K+1] so that [hull[i], hull[i+1]-1] has minimum density.
-    int[] preSum;
-    public double findMaxAverage_Best(int[] nums, int k) {
+    static int[] preSum;
+    public static double findMaxAverage_Best(int[] nums, int k) {
         int len = nums.length;
         preSum = new int[len];
         // get preSum array
@@ -55,27 +55,38 @@ Thus return 12.75.
         return res ;
     }
     //Let d(x, y) be the density of segment [x, y], ie. d(x, y) = (A[x]+...+A[y]) / (y-x+1)
-    public double getDensity(int l, int r) {
+    public static double getDensity(int l, int r) {
         double res = 0.0;
         if (l == 0) res = ((double) preSum[r]) / (1.0 + r);
         else res = ((double) preSum[r] - preSum[l - 1]) / (1.0 + r - l);
         return res;
     }
     
+    public static void main(String[] args) {
+        System.out.println(findMaxAverage_Best(new int[] {1,12,-5,-6,50,3}, 4));
+    }
+    
     
     //thinking process: 
-    //
+    //O(nlgm) 这里m是nums[]最大值和最小值的差。
+    //if return true, then means there is sub array which has avg larger than x, else is false
+    //to find a subarray which is larger than x or less than x, 
+    //we have a loop for 
     boolean check(int[] nums, int k, double x) {// Check whether we can find a subarray whose average is bigger than x
         int n = nums.length;
         double[] a = new double[n];
         // Transfer to a[i], find whether there is a subarray whose sum is bigger than 0
         IntStream.range(0, n).forEach(i -> a[i] = nums[i] - x);
+        //now and last are two 
         double now = 0, last = 0;
         for (int i = 0; i < k; i++) now += a[i];
         if (now >= 0) return true;
+        //two pointers move, every time, right moves to next while we detect current avg >= 0 or not.
+        //
         for (int i = k; i < n; i++) {
             now += a[i];
             last += a[i - k];
+            //means we want to remove a[i-k] from curren subarray
             if (last < 0) {
                 now -= last;
                 last = 0;
@@ -87,9 +98,12 @@ Thus return 12.75.
     public double findMaxAverage(int[] nums, int k) {
         double l=Integer.MIN_VALUE,r=Integer.MAX_VALUE;
         //mimic l + 1 < r
-        while (l + 0.000004 < r) {//Binary search the answer 
-            double mid=(l+r)/2;
-            if (check(nums,k,mid)) l=mid; else r=mid;
+        while (l + 0.000004 < r) {// Binary search the answer
+            double mid = l + (r - l) / 2;
+            if (check(nums, k, mid))
+                l = mid;
+            else
+                r = mid;
         }
         return r;
     }
