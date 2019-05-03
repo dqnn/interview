b9b5367c2db34b955218c53344379ab261926c80
Denny <qiangddu@gmail.com>
2019-05-03 00:35:20 -0700

    more comments and solutions

--- src/hatecode/KthSymbolInGrammar.java
+++ src/hatecode/KthSymbolInGrammar.java
@@ -56,4 +56,15 @@ Now, it's easy to tell f(k) = 0 if k has even number of 1s in binary representat
         if(flip)return 1;
         else return 0;
     }
+    //O(2^N)/O(2^N), simulation the environment
+    public int kthGrammar_Simulation(int N, int K) {
+        int[] lastrow = new int[1 << N];
+        for (int i = 1; i < N; ++i) {
+            for (int j = (1 << (i-1)) - 1; j >= 0; --j) {
+                lastrow[2*j] = lastrow[j];
+                lastrow[2*j+1] = 1 - lastrow[j];
+            }
+        }
+        return lastrow[K-1];
+    }
 }
