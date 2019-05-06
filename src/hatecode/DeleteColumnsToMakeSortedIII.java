a152410a4e1fd5c0abedcdda40cfd643aee7b4a3
Denny <qiangddu@gmail.com>
2019-05-06 03:28:20 -0700

    more problems and comments

---
+++ src/hatecode/DeleteColumnsToMakeSortedIII.java
@@ -0,0 +1,48 @@
+package hatecode;
+
+import java.util.*;
+public class DeleteColumnsToMakeSortedIII {
+/*
+960. Delete Columns to Make Sorted III
+We are given an array A of N lowercase letter strings, all of the same length.
+
+Now, we may choose any set of deletion indices, and for each string, we delete all the characters in those indices.
+
+For example, if we have an array A = ["babca","bbazb"] and deletion indices {0, 1, 4}, then the final array after deletions is ["bc","az"].
+
+Suppose we chose a set of deletion indices D such that after deletions, the final array has every element (row) in lexicographic order.
+
+For clarity, A[0] is in lexicographic order (ie. A[0][0] <= A[0][1] <= ... <= A[0][A[0].length - 1]), A[1] is in lexicographic order (ie. A[1][0] <= A[1][1] <= ... <= A[1][A[1].length - 1]), and so on.
+
+Return the minimum possible value of D.length.
+
+ 
+
+Example 1:
+
+Input: ["babca","bbazb"]
+Output: 3
+Explanation: After deleting columns 0, 1, and 4, the final array is A = ["bc", "az"].
+Both these rows are individually in lexicographic order (ie. A[0][0] <= A[0][1] and A[1][0] <= A[1][1]).
+Note that A[0] > A[1] - the array A isn't necessarily in lexicographic order.
+*/
+    //thinking process: 
+    //
+    public int minDeletionSize(String[] A) {
+        if (A == null || A.length < 1) return 0;
+        int m = A.length, n = A[0].length(), res = n - 1, k;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < j; ++i) {
+                for (k = 0; k < m; ++k) {
+                    if (A[k].charAt(i) > A[k].charAt(j)) break;
+                }
+                if (k == m && dp[i] + 1 > dp[j])
+                    dp[j] = dp[i] + 1;
+            }
+            res = Math.min(res, n - dp[j]);
+        }
+        return res;
+    }
+}
