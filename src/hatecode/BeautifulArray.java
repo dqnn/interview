c3d171e74652d2fe70356cd0775b9caf030864e7
Denny <qiangddu@gmail.com>
2019-05-05 17:49:56 -0700

    more comments

---
+++ src/hatecode/BeautifulArray.java
@@ -0,0 +1,66 @@
+package hatecode;
+
+import java.util.*;
+public class BeautifulArray {
+/*
+932. Beautiful Array
+For some fixed N, an array A is beautiful if it is a permutation of the integers 1, 2, ..., N, such that:
+
+For every i < j, there is no k with i < k < j such that A[k] * 2 = A[i] + A[j].
+
+Given N, return any beautiful array A.  (It is guaranteed that one exists.)
+
+ 
+
+Example 1:
+
+Input: 4
+Output: [2,1,4,3]
+*/
+    /*
+    1. Deletion
+Easy to prove.
+
+2. Addition
+If we have A[k] * 2 != A[i] + A[j],
+(A[k] + x) * 2 = A[k] * 2 + 2x != A[i] + A[j] + 2x = (A[i] + x) + (A[j] + x)
+
+E.g: [1,3,2] + 1 = [2,4,3].
+
+3. Multiplication
+If we have A[k] * 2 != A[i] + A[j],
+for any x != 0,
+(A[k] * x) * 2 = A[k] * 2 * x != (A[i] + A[j]) * x = (A[i] * x) + (A[j] * x)
+
+E.g: [1,3,2] * 2 = [2,6,4]
+
+
+Explanation
+With the observations above, we can easily construct any beautiful array.
+Assume we have a beautiful array A with length N
+
+A1 = A * 2 - 1 is beautiful with only odds from 1 to N * 2 -1
+A2 = A * 2 is beautiful with only even from 2 to N * 2
+B = A1 + A2 beautiful array with length N * 2
+
+E.g:
+
+A = [2, 1, 4, 5, 3]
+A1 = [3, 1, 7, 9, 5]
+A2 = [4, 2, 8, 10, 6]
+B = A1 + A2 = [3, 1, 7, 9, 5, 4, 2, 8, 10, 6]
+    */
+    public int[] beautifulArray(int N) {
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        while(res.size() < N) {
+            List<Integer> list = new ArrayList<>();
+            for(int i: res) if (i * 2 - 1 <= N) list.add(i * 2 -1);
+            for(int i: res) if (i * 2 <= N) list.add(i * 2);
+                
+            res = list;
+        }
+        
+        return res.stream().mapToInt(i->i).toArray();
+    }
+}
