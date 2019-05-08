3a619031e6331c9089d124be29bb263abb854b2c
Denny <qiangddu@gmail.com>
2019-05-08 04:42:21 -0700

    more comments and solutions

--- src/hatecode/ConstructBinaryTreefromInorderandPostorderTraversal.java
+++ src/hatecode/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -1,6 +1,6 @@
 package hatecode;
 
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * Project Name : Leetcode
@@ -44,10 +44,69 @@ public class ConstructBinaryTreefromInorderandPostorderTraversal {
      * @return
      */
 
+    //good solution,compare to pre+in, the whole pattern is the same
+    public TreeNode buildTree_Recursive(int[] in, int[] post) {
+        if (in == null || in.length == 0 || post == null || post.length == 0) { return null; }
+        return helper(post, post.length - 1, in, 0, in.length - 1);
+      }
+      private TreeNode helper(int[] post, int idx, int[] in, int start, int end) {
+        if (start > end || idx < 0) { return null; }
+        TreeNode root = new TreeNode(post[idx]);
+        int i;
+        for (i = start; i <= end; i++) {
+          if (in[i] == root.val) {
+            break;
+          }
+        }
+        root.right = helper(post, idx - 1, in, i + 1, end);
+        root.left = helper(post, idx - (end - i  + 1), in, start, i - 1);
+        return root;    
+      }
+/*
+The core idea is: Starting from the last element of the postorder and inorder array, 
+we put elements from postorder array to a stack and each one is the right child of the 
+last one until an element in postorder array is equal to the element on the inorder array. 
+Then, we pop as many as elements we can from the stack and decrease the mark in inorder 
+array until the peek() element is not equal to the mark value or the stack is empty. Then, 
+the new element that we are gonna scan from postorder array is the left child of the last 
+element we have popped out from the stack.
+ */
+      public TreeNode buildTree_Iterative(int[] inorder, int[] postorder) {
+          if (inorder.length == 0 || postorder.length == 0) return null;
+          int ip = inorder.length - 1;
+          int pp = postorder.length - 1;
+          
+          Stack<TreeNode> stack = new Stack<TreeNode>();
+          TreeNode prev = null;
+          TreeNode root = new TreeNode(postorder[pp]);
+          stack.push(root);
+          pp--;
+          
+          while (pp >= 0) {
+              while (!stack.isEmpty() && stack.peek().val == inorder[ip]) {
+                  prev = stack.pop();
+                  ip--;
+              }
+              TreeNode newNode = new TreeNode(postorder[pp]);
+              if (prev != null) {
+                  prev.left = newNode;
+              } else if (!stack.isEmpty()) {
+                  TreeNode currTop = stack.peek();
+                  currTop.right = newNode;
+              }
+              stack.push(newNode);
+              prev = null;
+              pp--;
+          }
+          
+          return root;
+      }
+    
+    
     int pInorder;
     int pPostorder;
 
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
+    public TreeNode buildTree_Reference(int[] inorder, int[] postorder) {
         // edge case
         if (null == inorder || postorder == null || inorder.length < 1 || postorder.length < 1
                 || postorder.length != inorder.length) {
