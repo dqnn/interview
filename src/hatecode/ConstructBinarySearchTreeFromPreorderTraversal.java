332a06c6e2eec93b57516de2ce2a74c09060167d
Denny <qiangddu@gmail.com>
2019-05-09 16:00:36 -0700

    more comments

--- src/hatecode/ConstructBinarySearchTreeFromPreorderTraversal.java
+++ src/hatecode/ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -1,13 +1,6 @@
 package hatecode;
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
+import java.util.*;
 public class ConstructBinarySearchTreeFromPreorderTraversal {
 /*
 1008. Construct Binary Search Tree from Preorder Traversal
@@ -27,7 +20,8 @@ Output: [8,5,10,1,7,null,12]
     //O(n)/O(n)
   //thinking process: given a array with distinct value--preorder visit, return binary search tree
     
-  //so 
+  //so BST only has 1 rule, but this problem seems to have only 1 answer, so every tree problem can be solved 
+  //by recursive or iterative, 
     int idx = 0;
     public TreeNode bstFromPreorder(int[] A) {
         return helper(A, Integer.MAX_VALUE);
@@ -39,6 +33,29 @@ Output: [8,5,10,1,7,null,12]
         TreeNode root = new TreeNode(A[idx++]);
         root.left = helper(A, root.val);
         root.right = helper(A, max);
+        return root;
+    }
+    
+    public TreeNode bstFromPreorder_iterative(int[] preorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        stack.push(root);
+        for (int i = 1; i < preorder.length; i++) {
+            TreeNode node = new TreeNode(preorder[i]);
+            if (preorder[i] < stack.peek().val) {                
+                stack.peek().left = node;                
+            } else {
+                TreeNode parent = stack.peek();
+                while (!stack.isEmpty() && preorder[i] > stack.peek().val) {
+                    parent = stack.pop();
+                }
+                parent.right = node;
+            }
+            stack.push(node);            
+        }
         return root;
     }
 }
