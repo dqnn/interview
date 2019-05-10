96197421b7cb67b7fe19e52c0502c4574f9eda7a
Denny <qiangddu@gmail.com>
2019-05-10 03:49:36 -0700

    more problems and actually not filly understood, need more time to work on it

--- src/hatecode/MovingStonesUntilConsecutive.java
+++ src/hatecode/MovingStonesUntilConsecutive.java
@@ -21,13 +21,27 @@ Input: a = 1, b = 2, c = 5
 Output: [1,2]
 */
     //thinking process: 
-    //
+/*
+不妨设a < b < c，首先分析最小情况：
+对于任意三个数，我们总可以在两步之内将其变成连续的三个数，如a,b,c
+将a变成b-1，将c变成b+1。
+如果a,b,c已经有序，那么最小移动次数是0次
+如果a,b,c满足已经有两个数临近或间隔为2，也就是 1,2,x 或 1,3,x的情况，那么最小移动次数是1次
+其余情况全为2次
+
+分析最大情况：
+因为每次移动之后三个数总要靠近，那么每一次只靠近一步就是最大次数
+对于a,b,c，最大移动次数为 c-b-1 + b-a-1
+ */
     public int[] numMovesStones(int a, int b, int c) {
         int[] s = {a, b, c };
         Arrays.sort(s);
-        if (s[2] - s[0] == 2) return new int[] {0, 0 };
-        return new int[] {
-                Math.min(s[1] - s[0], s[2] - s[1]) <= 2 ? 1 : 2, s[2] - s[0] - 2 };
+        int minV = 0, maxV= s[2] - s[1] - 1 + s[1] - s[0] - 1;
+        if (s[0] == s[1] - 1 && s[1]==s[2]-1) minV = 0;
+        else if (s[1] - s[0] <= 2 || s[2] - s[1] <= 2) minV = 1;
+        else minV = 2;
+        
+        return new int[]{minV, maxV};
     }
 
     public int[] numMovesStones2(int a, int b, int c) {
