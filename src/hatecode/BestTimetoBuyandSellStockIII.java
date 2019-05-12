8b34996609df69d314c2874e10085f1a36536a5d
Denny <qiangddu@gmail.com>
2019-05-12 03:00:16 -0700

    more comments and solutions

--- src/hatecode/BestTimetoBuyandSellStockIII.java
+++ src/hatecode/BestTimetoBuyandSellStockIII.java
@@ -52,21 +52,4 @@ public class BestTimetoBuyandSellStockIII {
         }
         return sell2;
     }
-    
- // T[i][k][0] = max{T[i - 1][k][0], T[i - 1][k][1] + nums[i] - fee};
-    // T[i][k][1] = max{T[i - 1][k][1], T[i - 1][k][0] - nums [i]} 
-    public int maxProfit2(int[] prices) {
-        //edge case
-        int T_i10 = 0, T_i11 = Integer.MIN_VALUE, T_i20 = 0, T_i21 = Integer.MIN_VALUE;
-        
-    for (int price : prices) {
-        T_i20 = Math.max(T_i20, T_i21 + price);
-        T_i21 = Math.max(T_i21, T_i10 - price);// this is because  no stock left we want to buy for second time, connect two transactions
-        T_i10 = Math.max(T_i10, T_i11 + price); // sell the stock for first time
-        T_i11 = Math.max(T_i11, -price); // first buysing
-    }
-        
-    return T_i20;
-        
-    }
 }
