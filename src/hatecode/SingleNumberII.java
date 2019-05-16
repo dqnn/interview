dcd014eafe4ee0f71b5e433a4ea2206e2a01a313
Denny <qiangddu@gmail.com>
2019-05-16 04:47:34 -0700

    more comments and solutions

--- src/hatecode/SingleNumberII.java
+++ src/hatecode/SingleNumberII.java
@@ -67,4 +67,27 @@ b更新为1，a更新为0，就是01的状态；再次遇到1的时候，b更新
         }
         return ones;
     }
+    
+    /** There are only 32 bits for a number in Java. We can traverse all numbers and count number of 1's in every 
+     * bit index (totally 32). If number of 1's in current bit index can be divided by 3, then current bit index 
+     * must not be the effective index of that single number. So we only need to find those bit index that have 
+     * countBit % 3 != 0, it will be the result single number. */
+
+    public int singleNumber_COOL(int[] nums) {
+        int result = 0;
+        int occurTime = 3;
+        for (int i = 0; i < 32; i++) {
+            int countBit = 0;
+            int curIndex = 1 << i;
+            for (int num : nums) {
+                if ((num & curIndex) != 0) {
+                    countBit++;
+                }
+            }
+            if (countBit % occurTime != 0) {
+                result |= curIndex;
+            }
+        }
+        return result;
+    }
 }
