7b76a43bbe4dbb48c63b22804497c1a7f871cc1b
Denny <qiangddu@gmail.com>
2019-06-01 17:52:16 -0700

    more comments on 2nd solutions

--- src/hatecode/ShortestWayToFormString.java
+++ src/hatecode/ShortestWayToFormString.java
@@ -2,19 +2,20 @@ package hatecode;
 
 import java.util.*;
 public class ShortestWayToFormString {
-/*
-1055. Shortest Way to Form String
-From any string, we can form a subsequence of that string by deleting some number of characters (possibly no deletions).
-
-Given two strings source and target, return the minimum number of subsequences of source such that their concatenation equals target. If the task is impossible, return -1.
-
- 
-
-Example 1:
-
-Input: source = "abc", target = "abcbc"
-Output: 2
-*/
+    /*
+     * 1055. Shortest Way to Form String From any string, we can form a subsequence
+     * of that string by deleting some number of characters (possibly no deletions).
+     * 
+     * Given two strings source and target, return the minimum number of
+     * subsequences of source such that their concatenation equals target. If the
+     * task is impossible, return -1.
+     * 
+     * 
+     * 
+     * Example 1:
+     * 
+     * Input: source = "abc", target = "abcbc" Output: 2
+     */
     //binary search O(s + t + tlgs)/O(s)
     public int shortestWay_BS(String s, String t) {
         if (s == null && t== null || t ==null) return 0;
@@ -53,23 +54,28 @@ Output: 2
         return at >= list.size() ? -1 : list.get(at);
     }
     
-    //still not fully understood this solution, needs a little bit more time
-    public int shortestWay(String source, String target) {
-        int output = 0;
+    //s=abc, t=abcbc
+    //greedy thinking, so we have lastIndex in s, each time, we would compare
+    //t.charAt(i) from lastIndex in s, if we can find the the char in s(index), then we move
+    //lastIndex to index + 1, 
+    //if we cannot find the index, then we reset lastIndex = 0, search from starting, and this means 
+    //we have to arrange the sub sequence one more time
+    public int shortestWay(String s, String t) {
+        int res = 0;
         int lastIndex = 0;
-        for(int i = 0; i < target.length(); i++){
-            int index = source.indexOf(target.charAt(i), lastIndex);
+        for(int i = 0; i < t.length(); i++){
+            int index = s.indexOf(t.charAt(i), lastIndex);
             if(index < 0){
                 if(lastIndex == 0) return -1;
                 lastIndex = 0;
-                output++;
+                res++;
                 i--;
             } else {
                 lastIndex = index + 1;
                 //output++;
             }
         }
-        return output+1;
+        return res+1;
     }
     
     
