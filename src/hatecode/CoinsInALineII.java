fee69d6f54104785fb443ca7be47923ccba51d3d
Denny <qiangddu@gmail.com>
2019-06-06 05:06:11 -0700

    more comments and solutions

---
+++ src/hatecode/CoinsInALineII.java
@@ -0,0 +1,37 @@
+package hatecode;
+
+public class CoinsInALineII {
+    /*
+     * Goole interview doc, not from LC
+     * Similar problem, tags: two-parts-game
+     * 464. Can I Win
+     * 348. Design Tic-Tac-Toe
+     * 486. Predict the Winner
+     * 294. Flip Game II
+     * 293. Flip Game
+     * 292. Nim Game
+     * 877. Stone Game
+     * 1025. Divisor Game
+     * 913. Cat and Mouse --if i have time
+     * 723. Candy Crush
+     * 837. New 21 Game
+     * 488  Zuma Game
+     * 
+     * There are n coins with different value in a line. Two players take turns to
+     * take one or two coins from left side until there are no more coins left. The
+     * player who take the coins with the most value wins.
+     * 
+     * Could you please decide the first player will win or lose?
+     * 
+     * If the first player wins, return true, otherwise return false.
+     * Input: [1, 2, 2]
+Output: true
+Explanation: The first player takes 2 coins
+     */
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
