bba28c2a3e91e1405d979c6111e472106112017c
Denny <qiangddu@gmail.com>
2019-06-07 09:54:35 -0700

    more comments and solutions

--- src/hatecode/TextJustification.java
+++ src/hatecode/TextJustification.java
@@ -43,7 +43,45 @@ public class TextJustification {
      * @param L
      * @return
      */
-  //thinking process: 
+  
+    //   0   1  2    3
+    // "This is an example..."
+    //  i=0, j=3, width=8, space=(16-8)/(3-0-1)=4, extra=0
+    // ------------------------------------------------------
+    //   3      4   5        6
+    // "example of text justification."
+    //  i=3, j=6, width=13, space=(16-13)/(3-0-1)=1, extra=1
+    // ------------------------------------------------------
+    // "justification."
+    //  i=6, j=7, space=1, extra=0
+    
+    //this is better solution, less code
+    public List<String> fullJustify_Better(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0, j; i < words.length; ) {
+            int width = 0;                                  // width of words without space
+            for (j = i; j < words.length && width + words[j].length() + (j - i) <= maxWidth; j++)
+                width += words[j].length();                 /* j is the next word */
+            
+            int space = 1, extra = 0;                       // for last line, space=1
+            if (j - i != 1 && j != words.length) {          // not 1 word (div-by-zero) and not last line
+                space = (maxWidth - width) / (j - i - 1);   // minus 1 to exclude skip last word
+                extra = (maxWidth - width) % (j - i - 1);
+            }
+            
+            StringBuilder line = new StringBuilder(words[i]);
+            for (i = i + 1; i < j; i++) {                   // move i to j finally
+                for (int s = space; s > 0; s--) line.append(" ");
+                if (extra-- > 0) line.append(" ");
+                line.append(words[i]);
+            }
+            for (int s = maxWidth - line.length(); s > 0; s--) line.append(" ");
+            result.add(line.toString());
+        }
+        return result;
+    }
+    
+    //thinking process: 
     // the problem is given string array and length of each line, so we want to form a list of strings, 
     //each string has max width, and 
     //there is spaces between each word, and maybe more spaces than any other strings in on line
@@ -66,11 +104,12 @@ public class TextJustification {
         if (w == null || w.length < 1 || maxWidth < 1) {
             return res;
         }
-        
-        int index = 0;
-        while(index < w.length) {
-            int count = w[index].length();
-            int last = index + 1;
+        //global pointer, word index
+        int idx = 0;
+        while(idx < w.length) {
+            int count = w[idx].length();
+            //we suppose each line will be smaller than maxWidth, so last starts from next, 
+            int last = idx + 1;
             while(last < w.length) {
                 // we want to detect the count first so 
                 //last is in the correct place
@@ -82,34 +121,34 @@ public class TextJustification {
             }
             StringBuilder sb = new StringBuilder();
             //each line, we always append first word
-            sb.append(w[index]);
+            sb.append(w[idx]);
             //how many cut we can use for spaces, 
             //last - index means how many words
             //diff = 0 means 1 word
-            int diff = last - index - 1;
+            int diff = last - idx - 1;
             if (last == w.length || diff == 0) {
                 //since we already append index, so we start from index + 1
-                for (int i = index + 1; i < last; i++) {
+                for (int i = idx + 1; i < last; i++) {
                     sb.append(" " + w[i]);
                 }
                 //append rest of all spaces
                 for(int i = sb.length(); i < maxWidth; i++) {
                     sb.append(" ");
                 }
+            //not last line or first line,
             } else {
-                // if not last line, 
+                // except first word, spaces means how many space does each word have after 
                 int spaces = (maxWidth - count) / diff;
-                //how many extra space
+                //how many extra space each word has
                 int r = (maxWidth - count) % diff;
-                for (int i = index + 1; i < last; i++) {
+                for (int i = idx + 1; i < last; i++) {
                     //append avg space
                     for(int k = spaces; k > 0; k--) {
                         sb.append(" ");
                     }
                     //we append extra space
-                    if (r-- > 0) {
-                        sb.append(" ");
-                    }
+                    if (r-- > 0)  sb.append(" ");
+                    
                     //append one more space because previous count already plus 1 space
                     sb.append(" ");
                     //append the word
@@ -117,7 +156,7 @@ public class TextJustification {
                 }
             }
             res.add(sb.toString());
-            index = last;
+            idx = last;
         }
         return res;
     }
