25a733b2740a411e06fb8238433333a8392bb151
Denny <qiangddu@gmail.com>
2019-06-07 10:44:37 -0700

    better solutions

--- src/hatecode/ImplementMagicDictionary.java
+++ src/hatecode/ImplementMagicDictionary.java
@@ -72,6 +72,41 @@ Input: search("leetcoded"), Output: False
         }
         return node.isWord;
     }
+    
+    
+    
+    //this is solution without 26*
+    Map<String, List<int[]>> map = new HashMap<>();
+   
+    //will build a map like following
+    //"hello" -> {"ello":[[0, 'h']], "hllo":[[1, 'e']], "helo":[[2, 'l'],[3, 'l']], "hell":[[4, 'o']]}
+    /** Build a dictionary through a list of words */
+    public void buildDict_Best(String[] dict) {
+        for (String s : dict) {
+            for (int i = 0; i < s.length(); i++) {
+                String key = s.substring(0, i) + s.substring(i + 1);
+                int[] pair = new int[] {i, s.charAt(i)};
+                
+                List<int[]> val = map.getOrDefault(key, new ArrayList<int[]>());
+                val.add(pair);
+                
+                map.put(key, val);
+            }
+        }
+    }
+    
+    /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
+    public boolean search_Best(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            String key = word.substring(0, i) + word.substring(i + 1);
+            if (map.containsKey(key)) {
+                for (int[] pair : map.get(key)) {
+                    if (pair[0] == i && pair[1] != word.charAt(i)) return true;
+                }
+            }
+        }
+        return false;
+    }
 }
 
 /**
