b995c3fada5cce28707576d4a2c1d0784f704ab0
Denny <qiangddu@gmail.com>
2019-06-08 20:15:03 -0700

    add more problem's concise comments

--- src/hatecode/TheMazeII.java
+++ src/hatecode/TheMazeII.java
@@ -2,7 +2,9 @@ package hatecode;
 import java.util.*;
 public class TheMazeII {
     /*
-     * 505. The Maze II There is a ball in a maze with empty spaces and walls. The
+     * 505. The Maze II 
+     * 
+     * There is a ball in a maze with empty spaces and walls. The
      * ball can go through empty spaces by rolling up, down, left or right, but it
      * won't stop rolling until hitting a wall. When the ball stops, it could choose
      * the next direction.
