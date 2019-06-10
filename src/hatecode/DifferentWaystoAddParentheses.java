a44878f1b3f757a3db675516bfdc9ae8cdec4f46
Denny <qiangddu@gmail.com>
2019-06-10 02:23:51 -0700

    add new questions into old ones

--- src/hatecode/DifferentWaystoAddParentheses.java
+++ src/hatecode/DifferentWaystoAddParentheses.java
@@ -80,4 +80,49 @@ public class DifferentWaystoAddParentheses {
         }
         return res;
     }
+    
+    /*
+     * similiar question in Googe interview 一个只有正整数的list， 其中插入+， * 或者（），求得到式子最大的值。
+     * e.g. [1，2，1，2 ]-> (1+2)*(1+2)=9. dp解， follow up， 如果有负数该怎么办， 如果想要拿到最大的式子该怎么办。
+     * 思路：类似burst balloon dp[i][j] = max of for (k : i ~ j max(dp[i][k - 1] *
+     * dp[k][j], dp[i][k - 1] + dp[k][j]))
+     * 
+     * follow up: 1. 如果有负数该怎么办， 2. 如果想要拿到最大的式子该怎么办
+     */
+    //O(n^3)/O(n^2)
+    public static int maxExprResult(int[] A) {  
+        int n = A.length;  
+        int[][] f = new int[n][n];  
+        for(int i=0; i<n; i++) {  
+            f[i][i] = A[i];  
+        }  
+        for(int w=2; w<=n; w++) { // window size of the sliding window  
+            for(int i=0, j=w-1; j<n; i++, j++) {  
+                for(int k=i; k<j; k++) {  
+                    f[i][j] = Math.max(f[i][j], Math.max(f[i][k]+f[k+1][j], f[i][k]*f[k+1][j]));  
+                }  
+            }  
+        }  
+        return f[0][n-1];  
+    }
+    //O(n)/O(n)
+    public static int maxExprResult_Better(int[] A) {  
+        int n = A.length;  
+        int[] dp = new int[n];  
+        dp[0] = A[0];  
+          
+        for (int i = 1; i < n; i++) {  
+            dp[i] = Math.max(dp[i - 1] * A[i], (i >= 2 ? dp[i - 2] : 1) * (A[i - 1] + A[i]));  
+      
+            if (i >= 2) {  
+                dp[i] = Math.max(dp[i], (i >= 3 ? dp[i - 3] : 1) * (A[i - 2] + A[i - 1] + A[i]));  
+            }  
+        }  
+        return dp[n-1];  
+    }  
+    public static void main(String[] args) {
+        int[] in = {2,2,2};
+        System.out.println(maxExprResult_Better(in));
+        System.out.println(maxExprResult(in));
+    }
 }
