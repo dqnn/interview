d84a9818cbb7bedc64ff851e70929ed3ec3d55d8
Denny <qiangddu@gmail.com>
2019-06-10 03:53:16 -0700

    add more comments and solutions

--- src/hatecode/MaxChunksToMakeSorted.java
+++ src/hatecode/MaxChunksToMakeSorted.java
@@ -2,7 +2,9 @@ package hatecode;
 class MaxChunksToMakeSorted {
     
     /*
-    769. Max Chunks To Make Sorted and Max Chunks To Make Sorted II
+    769. Max Chunks To Make Sorted
+    768. Max Chunks To Make Sorted II  not distinct value
+    42. Trapping Rain Water
     
     Given an array arr that is a permutation of [0, 1, ..., arr.length - 1], we split the array into some number of "chunks" (partitions), and individually sort each chunk.  
     After concatenating them, the result equals the sorted array.
