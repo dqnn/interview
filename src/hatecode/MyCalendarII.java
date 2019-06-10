62e14eb95162905e18a28300320f62620cd014dd
Denny <qiangddu@gmail.com>
2019-06-10 03:10:04 -0700

    refactor comments

--- src/hatecode/MyCalendarII.java
+++ src/hatecode/MyCalendarII.java
@@ -1,26 +1,32 @@
 package hatecode;
 import java.util.*;
 public class MyCalendarII {
-/*
-731. My Calendar II
-Implement a MyCalendarTwo class to store your events. A new event can be added if adding the event will not cause a triple booking.
-
-Your class will have one method, book(int start, int end). Formally, this represents a booking on the half open interval [start, end), the range of real numbers x such that start <= x < end.
-
-A triple booking happens when three events have some non-empty intersection (ie., there is some time that is common to all 3 events.)
-
-For each call to the method MyCalendar.book, return true if the event can be added to the calendar successfully without causing a triple booking. Otherwise, return false and do not add the event to the calendar.
-
-Your class will be called like this: MyCalendar cal = new MyCalendar(); MyCalendar.book(start, end)
-Example 1:
-MyCalendar();
-MyCalendar.book(10, 20); // returns true
-MyCalendar.book(50, 60); // returns true
-MyCalendar.book(10, 40); // returns true
-MyCalendar.book(5, 15); // returns false
-MyCalendar.book(5, 10); // returns true
-MyCalendar.book(25, 55); // returns true
-*/
+    /*
+     * 731. My Calendar II Implement a MyCalendarTwo class to store your events. A
+     * new event can be added if adding the event will not cause a triple booking.
+     * 
+     * Your class will have one method, book(int start, int end). Formally, this
+     * represents a booking on the half open interval [start, end), the range of
+     * real numbers x such that start <= x < end.
+     * 
+     * A triple booking happens when three events have some non-empty intersection
+     * (ie., there is some time that is common to all 3 events.)
+     * 
+     * For each call to the method MyCalendar.book, return true if the event can be
+     * added to the calendar successfully without causing a triple booking.
+     * Otherwise, return false and do not add the event to the calendar.
+     * 
+     * Your class will be called like this:
+     *  MyCalendar cal = new MyCalendar();
+     * MyCalendar.book(start, end) 
+     * Example 1: MyCalendar(); 
+     * MyCalendar.book(10, 20); // returns true 
+     * MyCalendar.book(50, 60); // returns true 
+     * MyCalendar.book(10,40); // returns true 
+     * MyCalendar.book(5, 15); // returns false
+     * MyCalendar.book(5, 10); // returns true 
+     * MyCalendar.book(25, 55); // returns true
+     */
     
     private List<int[]> cals;
     private List<int[]> overlaps;
