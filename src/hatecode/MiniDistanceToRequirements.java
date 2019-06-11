5c53fc362fb00271edb8314c015f0884f98df9e6
Denny <qiangddu@gmail.com>
2019-06-11 03:41:00 -0700

    add more comments and solutions

---
+++ src/hatecode/MiniDistanceToRequirements.java
@@ -0,0 +1,64 @@
+package hatecode;
+
+import java.util.*;
+public class MiniDistanceToRequirements {
+/*
+ *  From goole interiview, not from LC
+    *Input:1. 给一条路，路上的不同位置有不同的设施，有多个设施在不同位置的情况,List<Set<String>>
+    *2. 给一个需求设施的set
+    *Output：希望给出一个位置，距离所有设施的距离最近的和
+    *
+    *Example:
+    *Road:
+    {
+    *           [bookstore, school],
+    *           [grocery] ,
+    *           [],
+    *           [],
+    *           [bookstore, library],
+    *           []
+    *           [grocery]
+    *        }
+    *Requires:[bookstore,library,grocery]
+
+    *Output:
+    the best place is 4, to bookstore and   lib is 0, and grocery is 2, so in sum is 2.
+*/
+
+    //mini of mini distance to requirement
+    //thinking process:
+    
+    //given a list of roads, find the min path sum of mini distance
+    //
+    public int findBestLocationn(List<Set<String>> road, List<String> requires) {
+        
+        Map<String, List<Integer>> roadMap = new HashMap<>();
+        for (int i = 0; i < road.size(); i++) {
+            for (String facility : road.get(i)) {
+                roadMap.computeIfAbsent(facility, v->new ArrayList<>()).add(i);
+            }
+        }
+        
+        int minSum = Integer.MAX_VALUE, index = 0;
+        for (int i = 0; i < road.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < requires.size(); j++) {
+                sum += getMinLen(roadMap, requires.get(j), i);
+            }
+            if (sum < minSum) {
+                minSum = sum;
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private int getMinLen(Map<String, List<Integer>> roadMap, String require, int index) {
+        List<Integer> list = roadMap.get(require);
+        int minLen = Integer.MAX_VALUE;
+        for (int pos : list) {
+            minLen = Math.min(minLen, Math.abs(pos - index));
+        }
+        return minLen;
+    }
+}
