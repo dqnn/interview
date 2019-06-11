42d606a7291bf615e0ab1372fa9136e9a4851840
Denny <qiangddu@gmail.com>
2019-06-11 06:23:30 -0700

    more comments and solutions

--- src/hatecode/FindPermutation.java
+++ src/hatecode/FindPermutation.java
@@ -36,7 +36,7 @@ Output: [2,1,3]
     
 /*
  *      IIIIIIII    IDDIDDDI
- *      12345678    14327658
+ *      123456789   143287659
  *  first is the templates, if we change any I ->D, we just reverse the adjacent
  *  
  */
@@ -56,4 +56,42 @@ Output: [2,1,3]
         }
         return res;
     }
+    
+    //most naive solution, but working and easy to write
+    //IDDIDDDI, first res=1,2,3,4,5,6,7,8,9
+    //then we search for D, first s=1,e=3, so swap(2,4)
+    public static int[] findPermutation_naive(String s) {
+        int n = s.length();
+        int[] res = new int[n + 1];
+        for(int i = 0 ; i < res.length ; i++) {
+            res[i] = i + 1;
+        }
+        //IDDIDDDI
+        for(int i = 0 ; i < n ; i++) {
+            if(s.charAt(i) == 'D') {
+                int j = i;
+                while(j < n && s.charAt(j) == 'D') j++;
+                //note j is not D, but I, that's why i = j - 1
+                reverse(res, i, j);
+                i = j - 1;
+            }
+        }
+        return res;
+    }
+    private static void reverse(int[] arr, int s, int e) {
+        int l = s, r = e;
+        while(l < r) {
+            swap(arr, l++, r--);
+        }
+    }
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(findPermutation_naive("IDDIDDDI")));
+    }
+
 }
