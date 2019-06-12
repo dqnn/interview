e8db8a329f786d90326950f319d6c6f1ad00120f
Denny <qiangddu@gmail.com>
2019-06-11 18:54:15 -0700

    add new problem sets

--- src/hatecode/GuessTheWord.java
+++ src/hatecode/GuessTheWord.java
@@ -187,23 +187,23 @@ public void findSecretWord2(String[] wordlist, Master master) {
         return dist1 - dist2;
     }
     public void guessGift(int rows, int cols) {
-        int left = 0, right = cols - 1;
-        while (left < right) {
-            int res = guess(0, left, 0, right);
-            int mid = left + (right - left) / 2;
+        int l = 0, r = cols - 1;
+        while (l <= r) {
+            int res = guess(0, l, 0, r);
+            int mid = l + (r - l) / 2;
             if(res == 0) {
-                left = mid;
+                l = mid;
                 break;
             } else if(res < 0) {
-                right = mid - 1;
+                r = mid - 1;
             } else {
-                left = mid + 1;
+                l = mid + 1;
             }
         }
         int top = 0, bot = rows - 1;
         while (top < bot) {
             int mid = top + (bot - top) / 2;
-            int res = guess(top, left, bot, left);
+            int res = guess(top, l, bot, l);
             if(res == 0) {
                 break;
             } else if(res < 0) {
@@ -212,7 +212,7 @@ public void findSecretWord2(String[] wordlist, Master master) {
                 top = mid + 1;
             }
         }
-        System.out.println(top + ", " + left);
+        System.out.println(top + ", " + l);
     }
 
 }
