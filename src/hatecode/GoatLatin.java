078d58f2256a0679dd34d287b2c9c9983a2ea9ac
Denny <qiangddu@gmail.com>
2019-06-15 22:59:06 -0700

    more comments and solutions

--- src/hatecode/GoatLatin.java
+++ src/hatecode/GoatLatin.java
@@ -1,30 +1,36 @@
 package hatecode;
 public class GoatLatin {
-/*
-824. Goat Latin 
-A sentence S is given, composed of words separated by spaces. Each word consists of lowercase and uppercase letters only.
-
-We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
-
-The rules of Goat Latin are as follows:
-
-If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
-For example, the word 'apple' becomes 'applema'.
- 
-If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
-For example, the word "goat" becomes "oatgma".
- 
-Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
-For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
-Return the final sentence representing the conversion from S to Goat Latin. 
-
- 
-
-Example 1:
-
-Input: "I speak Goat Latin"
-Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
-*/
+    /*
+     * 824. Goat Latin
+     * 
+     *  A sentence S is given, composed of words separated by spaces.
+     * Each word consists of lowercase and uppercase letters only.
+     * 
+     * We would like to convert the sentence to "Goat Latin" (a made-up language
+     * similar to Pig Latin.)
+     * 
+     * The rules of Goat Latin are as follows:
+     * 
+     * If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of
+     * the word. For example, the word 'apple' becomes 'applema'.
+     * 
+     * If a word begins with a consonant (i.e. not a vowel), remove the first letter
+     * and append it to the end, then add "ma". For example, the word "goat" becomes
+     * "oatgma".
+     * 
+     * Add one letter 'a' to the end of each word per its word index in the
+     * sentence, starting with 1. For example, the first word gets "a" added to the
+     * end, the second word gets "aa" added to the end and so on. Return the final
+     * sentence representing the conversion from S to Goat Latin.
+     * 
+     * 
+     * 
+     * Example 1:
+     * 
+     * Input: "I speak Goat Latin" Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+     */
+    //thinking process:
+    //
     public String toGoatLatin(String s) {
         if (s == null || s.length() < 1) return s;
         
