accb9745bbd1d2d500a01973ae60883e538d8863
Denny <qiangddu@gmail.com>
2019-06-16 06:10:29 -0700

    more comments and solutions

--- src/hatecode/NQueens.java
+++ src/hatecode/NQueens.java
@@ -1,6 +1,6 @@
 package hatecode;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 /**
@@ -100,5 +100,50 @@ Explanation: There exist two distinct solutions to the 4-queens puzzle as shown 
             list.add(sb.toString());
         }
         res.add(list);
+    }
+    
+    
+  //O(n * n!)
+    
+    //this is shorter solution, and with same complexity
+    
+    //so the question is to say to place n queues in a nxn matrix, so same row, column, dial and anti-dial
+    //cannot have two queues
+    
+    //
+    private Set<Integer> col = new HashSet<Integer>();
+    private Set<Integer> diag1 = new HashSet<Integer>();
+    private Set<Integer> diag2 = new HashSet<Integer>();
+    
+    public List<List<String>> solveNQueens_DFS(int n) {
+        List<List<String>> res = new ArrayList<List<String>>();
+        dfs(res,new ArrayList<String>(), 0, n);
+        return res;
+    }
+    private void dfs(List<List<String>> res, List<String> list, int row, int n){
+        if (row == n){
+            res.add(new ArrayList<String>(list));
+            return;
+        }
+        for (int i = 0; i < n; i++){
+            if (col.contains(i) || diag1.contains(row + i) || diag2.contains(row - i)) continue;
+            
+            char[] charArray = new char[n];
+            Arrays.fill(charArray, '.');
+            charArray[i] = 'Q';
+            String rowString = new String(charArray);
+            
+            list.add(rowString);
+            col.add(i);
+            diag1.add(row + i);
+            diag2.add(row - i);
+            
+            dfs(res, list, row + 1, n);
+            
+            list.remove(list.size() - 1);
+            col.remove(i);
+            diag1.remove(row + i);
+            diag2.remove(row - i);
+        }
     }
 }
