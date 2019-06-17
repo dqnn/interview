089abbd282139a41a76c61990e209b5c1c0b015f
Denny <qiangddu@gmail.com>
2019-06-17 03:18:35 -0700

    more comments and solutions

--- src/hatecode/BasicCalculator.java
+++ src/hatecode/BasicCalculator.java
@@ -45,30 +45,31 @@ public class BasicCalculator {
      * @return
      */
     public int calculate(String s) {
+        if (s == null) return 0;
+        
         Stack<Integer> stack = new Stack<>();
-        int sign = 1;
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                int num = s.charAt(i) - '0';
-                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
-                    num = num * 10 + s.charAt(i + 1) - '0';
-                    i++;
-                }
-                res += num * sign;
-            } else if (s.charAt(i) == '+') {
+        int sign = 1, res = 0;
+        for(int i = 0; i< s.length(); i++) {
+            char c = s.charAt(i);
+            if(Character.isDigit(c)) {
+                int start = i;
+                while(i+1< s.length() && Character.isDigit(s.charAt(i+1))) i++;
+                int num = Integer.valueOf(s.substring(start, i +1));
+                res += sign * num;
+            } else if(c == '+') {
                 sign = 1;
-            } else if (s.charAt(i) == '-') {
+            } else if (c =='-') {
                 sign = -1;
-            } else if (s.charAt(i) == '(') {
+            } else if(c == '(') {
                 stack.push(res);
                 stack.push(sign);
                 res = 0;
                 sign = 1;
-            } else if (s.charAt(i) == ')') {
-                res = res * stack.pop() + stack.pop();// first pop is sign, second pop is res
+            } else if(c == ')') {
+                res = res * stack.pop() + stack.pop();
             }
         }
         return res;
+        
     }
 }
