7f236b509fc85452fa3131bee6f0094721b6c2a8
Denny <qiangddu@gmail.com>
2019-06-17 04:17:46 -0700

    more comments and solutions

--- src/hatecode/LongestArithmeticSequence.java
+++ src/hatecode/LongestArithmeticSequence.java
@@ -2,19 +2,21 @@ package hatecode;
 
 import java.util.*;
 public class LongestArithmeticSequence {
-/*
-1027. Longest Arithmetic Sequence
-Given an array A of integers, return the length of the longest arithmetic subsequence in A.
-
-Recall that a subsequence of A is a list A[i_1], A[i_2], ..., A[i_k] with 0 <= i_1 < i_2 < ... < i_k <= A.length - 1, and that a sequence B is arithmetic if B[i+1] - B[i] are all the same value (for 0 <= i < B.length - 1).
-
-Example 1:
-
-Input: [3,6,9,12]
-Output: 4
-
-    
-    */
+    /*
+     * 1027. Longest Arithmetic Sequence 
+     * Given an array A of integers, return the
+     * length of the longest arithmetic subsequence in A.
+     * 
+     * Recall that a subsequence of A is a list A[i_1], A[i_2], ..., A[i_k] with 0
+     * <= i_1 < i_2 < ... < i_k <= A.length - 1, and that a sequence B is arithmetic
+     * if B[i+1] - B[i] are all the same value (for 0 <= i < B.length - 1).
+     * 
+     * Example 1:
+     * 
+     * Input: [3,6,9,12] Output: 4
+     * 
+     * 
+     */
     // O(n^2)/O(n), 
     //thinking process: given an array, find the longest Arithmetic sequence length
     //we used a map to store diff<->length of arithmetic sequence, so we use two pointers i and j, (j < i, i =1,2..n-1), 
