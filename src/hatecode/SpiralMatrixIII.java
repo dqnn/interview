6db7befa68fb3617dceca524e948b54dcb9e87f7
Denny <qiangddu@gmail.com>
2019-06-16 17:22:49 -0700

    more comments and solutions

--- src/hatecode/SpiralMatrixIII.java
+++ src/hatecode/SpiralMatrixIII.java
@@ -1,4 +1,6 @@
 package hatecode;
+
+import java.util.*;
 public class SpiralMatrixIII {
 /*
 885. Spiral Matrix III
@@ -21,6 +23,33 @@ Example 1:
 Input: R = 1, C = 4, r0 = 0, c0 = 0
 Output: [[0,0],[0,1],[0,2],[0,3]]
 */
+    
+    //interview friendly:
+    //thinking process:
+    //
+    public int[][] spiralMatrixIII_Best(int R, int C, int r0, int c0) {
+        int[][] dirt = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // east, south, west, north
+        List<int[]> res = new ArrayList<>();
+        int len = 0, d = 0; // move <len> steps in the <d> direction
+        res.add(new int[]{r0, c0});
+        while (res.size() < R * C) {
+            if (d == 0 || d == 2) len++; // when move east or west, the length of path need plus 1 
+            for (int i = 0; i < len; i++) {
+                r0 += dirt[d][0];
+                c0 += dirt[d][1];
+                if (r0 >= 0 && r0 < R && c0 >= 0 && c0 < C) // check valid
+                    res.add(new int[]{r0, c0});
+            }
+            d = (d + 1) % 4; // turn to next direction
+        }
+        return res.toArray(new int[R * C][2]);
+    }
+    
+    
+    
+    
+    
+    
     /*thinking process: 
      * The idea here is that once we start at (r=r0, c=c0), we walk along the east,
      * then south, then west, and then north.
@@ -40,7 +69,12 @@ Output: [[0,0],[0,1],[0,2],[0,3]]
      */
     //
     
-    //this is a low priority problem, just for reference
+    
+    
+    
+    
+    //1 every circle our len will, realize how we traverse the matrix, 
+    //every approach two steps, we will change our direction
     int idx;
     int[][] ret;
     
