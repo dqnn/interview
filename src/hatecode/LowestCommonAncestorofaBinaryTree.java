1b371169c149599525e7873d3a725f3c5f2380c8
Denny <qiangddu@gmail.com>
2019-06-17 07:09:36 -0700

    add more code

--- src/hatecode/LowestCommonAncestorofaBinaryTree.java
+++ src/hatecode/LowestCommonAncestorofaBinaryTree.java
@@ -1,5 +1,6 @@
 package hatecode;
 
+import java.util.*;
 /**
  * Created by duqiang on 28/07/2017.
  */
@@ -28,18 +29,46 @@ public class LowestCommonAncestorofaBinaryTree {
     
     //this needs to be remembered
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-       // please note here root ==p and root == q
+       // returns real root which is LCA since 
+        //it’s doesn’t matter where second (p or q) is – root is LCA anyway.
         if (root == null || root == p || root == q) {
             return root;
         }
         
-        // recursive to find the elements
+        // Let’s say root node is not p or q then it checks left branch first
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-
+        //
         if (left != null && right != null) {
             return root;
         }
         return left == null ? right : left;
+    }
+    //iterative solutions
+    public TreeNode lowestCommonAncestor_Iterative(TreeNode root, TreeNode p, TreeNode q) {
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        parent.put(root, null);
+        stack.push(root);
+
+        while (!parent.containsKey(p) || !parent.containsKey(q)) {
+            TreeNode node = stack.pop();
+            if (node.left != null) {
+                parent.put(node.left, node);
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                parent.put(node.right, node);
+                stack.push(node.right);
+            }
+        }
+        Set<TreeNode> ancestors = new HashSet<>();
+        while (p != null) {
+            ancestors.add(p);
+            p = parent.get(p);
+        }
+        while (!ancestors.contains(q))
+            q = parent.get(q);
+        return q;
     }
 }
