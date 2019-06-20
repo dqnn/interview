a78916bf3ce855b7a734d065b76ae98b0bc4b0e5
Denny <qiangddu@gmail.com>
2019-06-20 06:15:20 -0700

    remove some stupid comments

--- src/hatecode/LongestIncreasingSubsequence.java
+++ src/hatecode/LongestIncreasingSubsequence.java
@@ -47,46 +47,7 @@ public class LongestIncreasingSubsequence {
     // tail to insert, it is more like insert sort, 
     // the way to identify the element which should be longest increasing sequence is the position for 
     //res is the variabe to maintain the insert position for each element
-    
-    //another explanation:
-    /*
- I use an array dp[] to store the largest elements in the increasing subsequences and the array's 
- index is the length of the subsequence (since this, we can make sure that the array dp[] is 
-     * in-order). 
-     * The idea is keep checking if nums[i] is bigger than dp[len] or not: nums[i] > dp[len] means we can 
-     * retrieve a longer subsequence by adding the current element nums[i] into the result, so we just increase 
-     * the pointer 'len' by one and put the current element into the new index; otherwise we need to do a binary 
-     * search to find out the index of the largest element st. dp[index] < nums[i] && dp[index + 1] > nums[i] and 
-     * update the dp[index] with value nums[i].
 
-Let's see an example:
-intput: 2 4 9 3 7 8
-
-The array 'dp' looks like these in first two iterations:
-
-2 4 0 0 0 (len = 1)
-
-2 4 9 0 0 (len = 2)
-
-len =1  means if max LIS length is 1, 
-len =2  means if max LIS length is 2
-In the third iteration, we notice that dp[len] ('9' here) > nums[i] ('3' here), 
-so we update the dp[index] with value '3', then we got:
-
-2 3 9 0 0 (len = 2) <---- Attention here, this array doesn't mean we can retrieve subsequence [2,3,9], 
-it only means the largest element in a length 1 subsequence is '2' ([2]), the largest element in a 
-length 2 subsequence is '3' ([2,3]) and the largest element in a length 3 subsequence is '9' ([2,4,9])
- by far.
-
-In next iteration, we update the dp[index] with value '7' again and we got:
-2 3 7 0 0 (len = 2)
-
-In the final round, obviously dp[len] (7) < nums[i] (8) so we increase the 'len' by one and put '8' into dp[3]
- then we got:
-2 3 7 8 0 (len = 3)
-
-Clearly len + 1 = 4 is our result : )
-     */
     //the tail means, if we want to find nums[i] as the last elment in LIS, then how it would like
     //LIS(5) = LIS(4) + 1,
     
@@ -98,6 +59,8 @@ Clearly len + 1 = 4 is our result : )
         }
         //to store the LIS last elements in array
         //[1,3,5,4,7]->[1, 3, 4, 7, 0]
+       /**(1) if x is larger than all tails, append it, increase the size by 1
+        * (2) if tails[i-1] < x <= tails[i], update tails[i] */
         int[] tails = new int[nums.length];
         int res = 0;
         for (int num : nums) {
