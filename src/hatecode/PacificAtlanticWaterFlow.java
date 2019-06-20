72a563b4c5e5167d6a415bc16111eca2ba5703db
Denny <qiangddu@gmail.com>
2019-06-20 04:12:33 -0700

    more comments and solutions

--- src/hatecode/PacificAtlanticWaterFlow.java
+++ src/hatecode/PacificAtlanticWaterFlow.java
@@ -30,93 +30,52 @@ Return:
     //it detect all points while we should only detect border ones
     //so the problem is to focus on memorize the 
     List<int[]> res;
-    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0,1}};
+    int[][]     dirs = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}};
+
     public List<int[]> pacificAtlantic(int[][] m) {
         res = new ArrayList<>();
-        if (m == null || m.length < 1 || m[0].length < 1) return res;
-        
+        if (m == null || m.length < 1 || m[0].length < 1)
+            return res;
+
         int r = m.length, c = m[0].length;
         boolean[][] left = new boolean[r][c];
         boolean[][] right = new boolean[r][c];
-       
-        for(int i = 0; i < r;i++) {
-            for(int j = 0; j < c;j++) {
-                helper(m, i, j, left, right);
-            }
+
+        for (int i = 0; i < r; i++) {
+            helper(m, i, 0, left);
+            helper(m, i, c - 1, right);
         }
-        
-        for(int i = 0; i < r;i++) {
-            for(int j = 0; j < c;j++) {
+        for (int i = 0; i < c; i++) {
+            helper(m, 0, i, left);
+            helper(m, r - 1, i, right);
+        }
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 if (left[i][j] && right[i][j]) {
-                    res.add(new int[]{i, j});
+                    res.add(new int[] {
+                            i, j });
                 }
             }
         }
         return res;
     }
-    
-    private void helper(int[][] m, int i, int j, boolean[][] left, boolean[][] right) {
-        if (left[i][j] || right[i][j]) return;
 
-        if (i == 0 || j == 0) {
-            left[i][j] = true;
-        }
-        if (i == m.length - 1 || j == m[0].length - 1) {
-            right[i][j] = true;
-        }
-        
-        for(int[] dir : dirs) {
+    private void helper(int[][] m, int i, int j, boolean[][] visited) {
+        if (visited[i][j])
+            return;
+
+        visited[i][j] = true;
+
+        for (int[] dir : dirs) {
             int x = i + dir[0];
             int y = j + dir[1];
-            if (x >= 0 && y >= 0 && x < m.length && y < m[0].length && m[x][y] <= m[i][j]){
-                helper(m, x, y, left, right);
-                if (left[x][y]) left[i][j] = true;
-                if (right[x][y]) right[i][j] = true;
-            } 
-        }
-    }
-    //this is accepted, the reason is it reduced units which are needed to be checked
-    class AcceptedSolution {
-        List<int[]> res;
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0,1}};
-        public List<int[]> pacificAtlantic(int[][] m) {
-            res = new ArrayList<>();
-            if (m == null || m.length < 1 || m[0].length < 1) return res;
-            
-            int r = m.length, c = m[0].length;
-            boolean[][] left = new boolean[r][c];
-            boolean[][] right = new boolean[r][c];
-           
-            for(int i = 0; i< r;i++) {
-                helper(m, i, 0, left);
-                helper(m, i, c-1, right);
-            }
-            for(int i = 0; i< c;i++) {
-                helper(m, 0, i, left);
-                helper(m, r-1, i, right);
-            }
-            
-            for(int i = 0; i < r;i++) {
-                for(int j = 0; j < c;j++) {
-                    if (left[i][j] && right[i][j]) {
-                        res.add(new int[]{i, j});
-                    }
-                }
-            }
-            return res;
-        }
-        
-        private void helper(int[][] m, int i, int j, boolean[][] visited) {
-            if (visited[i][j]) return;
-            
-            visited[i][j] = true;
-            
-            for(int[] dir : dirs) {
-                int x = i + dir[0];
-                int y = j + dir[1];
-                if (x >= 0 && y >= 0 && x < m.length && y < m[0].length && m[x][y] >= m[i][j]){
-                    helper(m, x, y, visited);
-                } 
+            if (x >= 0 && y >= 0 && x < m.length && y < m[0].length && m[x][y] >= m[i][j]) {
+                helper(m, x, y, visited);
             }
         }
     }
