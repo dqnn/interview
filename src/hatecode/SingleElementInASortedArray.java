46874f954c737e98f498127ed1b558e15ee183ed
Denny <qiangddu@gmail.com>
2019-06-20 09:00:48 -0700

    more comments and solutions

--- src/hatecode/SingleElementInASortedArray.java
+++ src/hatecode/SingleElementInASortedArray.java
@@ -12,6 +12,7 @@ Input: [3,3,7,7,10,11,11]
 Output: 10
 */
     //thinking process: 
+    //if it is sorted, then BS is on the plate
     //
     public int singleNonDuplicate_Standard(int[] nums) {
         if (nums == null || nums.length < 1)
