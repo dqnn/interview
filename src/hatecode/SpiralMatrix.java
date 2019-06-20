5281d939ca1f13c94e2ef45d578fa4af1b9aa50c
Denny <qiangddu@gmail.com>
2019-06-20 08:17:49 -0700

    refactor

--- src/hatecode/SpiralMatrix.java
+++ src/hatecode/SpiralMatrix.java
@@ -26,7 +26,7 @@ public class SpiralMatrix {
      time : O(n * m)     n * m : 总元素个数
      space : O(n * m)
 
-     * @param matrix
+     * @param g
      * @return
      */
     
@@ -34,24 +34,24 @@ public class SpiralMatrix {
     //interview friendly, 
     //this is much simpler solution, up means top start point, down means bottom start point, 
     // this means direction, same to left and right
-    public List<Integer> spiralOrder(int[][] matrix) {
+    public List<Integer> spiralOrder(int[][] g) {
         List<Integer> res = new LinkedList<>(); 
-        if (matrix == null || matrix.length == 0) return res;
-        int n = matrix.length, m = matrix[0].length;
-        int up = 0,  down = n - 1;
-        int left = 0, right = m - 1;
-        while (res.size() < n * m) {
-            for (int j = left; j <= right && res.size() < n * m; j++)
-                res.add(matrix[up][j]);
+        if (g == null || g.length == 0) return res;
+        int r = g.length, c = g[0].length;
+        int up = 0,  down = r - 1;
+        int left = 0, right = c - 1;
+        while (res.size() < r * c) {
+            for (int j = left; j <= right && res.size() < r * c; j++)
+                res.add(g[up][j]);
             
-            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
-                res.add(matrix[i][right]);
+            for (int i = up + 1; i <= down - 1 && res.size() < r * c; i++)
+                res.add(g[i][right]);
                      
-            for (int j = right; j >= left && res.size() < n * m; j--)
-                res.add(matrix[down][j]);
+            for (int j = right; j >= left && res.size() < r * c; j--)
+                res.add(g[down][j]);
                         
-            for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--) 
-                res.add(matrix[i][left]);
+            for (int i = down - 1; i >= up + 1 && res.size() < r * c; i--) 
+                res.add(g[i][left]);
                 
             left++; right--; up++; down--; 
         }
