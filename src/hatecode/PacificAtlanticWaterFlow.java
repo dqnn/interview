fb787206292e6924ab694d4ffc252c588235a20d
Denny <qiangddu@gmail.com>
2019-06-20 04:09:35 -0700

    more comments and solutions

--- src/hatecode/PacificAtlanticWaterFlow.java
+++ src/hatecode/PacificAtlanticWaterFlow.java
@@ -3,9 +3,12 @@ import java.util.*;
 public class PacificAtlanticWaterFlow {
 /*
 417. Pacific Atlantic Water Flow
-Given an m x n matrix of non-negative integers representing the height of each unit cell in a continent, the "Pacific ocean" touches the left and top edges of the matrix and the "Atlantic ocean" touches the right and bottom edges.
+Given an m x n matrix of non-negative integers representing the height of each unit cell in 
+a continent, the "Pacific ocean" touches the left and top edges of the matrix and the 
+"Atlantic ocean" touches the right and bottom edges.
 
-Water can only flow in four directions (up, down, left, or right) from a cell to another one with height equal or lower.
+Water can only flow in four directions (up, down, left, or right) 
+from a cell to another one with height equal or lower.
 
 Find the list of grid coordinates where water can flow to both the Pacific and Atlantic ocean.
 
