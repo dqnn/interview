37e0fece4d00f86a9ac548e6bb9c6eadf805c273
Denny <qiangddu@gmail.com>
2019-06-21 00:44:05 -0700

    more comments and solutions

--- src/hatecode/IncreasingOrderSearchTree.java
+++ src/hatecode/IncreasingOrderSearchTree.java
@@ -61,8 +61,7 @@ Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
         increasingBST(root.right); 
         return head;
     }
-    
-    
+
     public TreeNode increasingBST_Iterative(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
         TreeNode dummy = new TreeNode(0), p = dummy; 
