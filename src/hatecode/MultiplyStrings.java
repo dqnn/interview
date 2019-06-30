5a1a881b89a9f9a1e7897fad62d4c1ba4e3815b7
Denny <qiangddu@gmail.com>
2019-06-30 08:03:08 -0700

    more comments and solutions

--- src/hatecode/MultiplyStrings.java
+++ src/hatecode/MultiplyStrings.java
@@ -10,6 +10,7 @@ package hatecode;
  */
 public class MultiplyStrings {
     /**
+     * tags: facebook
      * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
 
      Note:
