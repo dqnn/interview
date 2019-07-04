ac46e1e332ba30434b5721c83d2e6263ebfc462c
Denny <qiangddu@gmail.com>
2019-07-04 12:26:46 -0700

    more comments and solutions

--- src/hatecode/LetterTilePossibilities.java
+++ src/hatecode/LetterTilePossibilities.java
@@ -1,17 +1,17 @@
 package hatecode;
 public class LetterTilePossibilities {
-/*
-1079. Letter Tile Possibilities
-You have a set of tiles, where each tile has one letter tiles[i] printed on it.  Return the number of possible non-empty sequences of letters you can make.
-
- 
-
-Example 1:
-
-Input: "AAB"
-Output: 8
-Explanation: The possible sequences are "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA".
-*/
+    /*
+     * 1079. Letter Tile Possibilities You have a set of tiles, where each tile has
+     * one letter tiles[i] printed on it. Return the number of possible non-empty
+     * sequences of letters you can make.
+     * 
+     * 
+     * 
+     * Example 1:
+     * 
+     * Input: "AAB" Output: 8 Explanation: The possible sequences are "A", "B",
+     * "AA", "AB", "BA", "AAB", "ABA", "BAA".
+     */
     //interview friendly, really brilliant solution
     public int numTilePossibilities(String t) {
         if(t == null || t.length() < 1) return 0;
