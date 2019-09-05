4479c73a224127c2a981762bf58016754b2364d5
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-05 02:58:13 -0700

    more comments and solutions

--- src/hatecode/NumbersWithSameConsecutiveDifferences.java
+++ src/hatecode/NumbersWithSameConsecutiveDifferences.java
@@ -50,8 +50,45 @@ Output: [181,292,707,818,929]
         return cur.stream().mapToInt(e->e).toArray();
         
     }
-    
-    
+    //DFS solution, the idea is below:
+/*
+ *      1 2 3 4 5 6 7 8 9 
+ *      / // /    \ \ \ \ 
+ *     1  2
+ *    / \
+ *   1+N 1-N
+ *  ......
+ *  
+ *  so this is a DFS problem
+ */
+    public int[] numsSameConsecDiff_DFS(int N, int K) {
+        
+        if(N <= 0) return new int[0];
+        
+        if(N == 1) return new int[]{0,1,2,3,4,5,6,7,8,9}; 
+        List<Integer> list = new ArrayList<>();
+        helper(N, K, list, 0);
+        return list.stream().mapToInt(i->i).toArray();   //list.toArray(new int[list.size()]); doesn't work for primitives
+    }
+    public void helper(int N, int K, List<Integer> list, int number){
+        if(N == 0){   // base case, if you have added enough number of integers then append it to list; Here N is used as the total digits in temporary number 
+            list.add(number);
+            return ;
+        }
+        for(int i=0; i<10; ++i){
+             // no leading 0
+            if(i==0 && number == 0) continue;  
+            // base case, we add all the digits when we do not have any previous digit to check if difference = K 
+            else if(number == 0 && i!=0){     
+                helper(N-1, K, list, i);
+            } else{
+                if(Math.abs((number%10) - i )==K){
+                    // General dfs to add the digit at the units place and reducing the number of digits by 1.
+                    helper(N-1, K, list, number*10+i); 
+                }
+            }
+        }
+    }
     
     
 }
