43d3d9b418c10d09229593411331e8455fa8f416
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-12 01:55:42 -0700

    add more comments and solutions

--- src/hatecode/_991BrokenCalculator.java
+++ src/hatecode/_991BrokenCalculator.java
@@ -12,17 +12,24 @@ Return the minimum number of operations needed to display the number Y.
 Input: X = 2, Y = 3
 Output: 2
 */
-    //thinking process:
-    //the problem 
-    //greedy solution, so 
+    //thinking process: math problem
+    //the problem is to say: given two numbers x and y, we want to know 
+    //the fewest steps from x->y, we either y-1 or x*2
+  
+    //the regular thinking is BFS, to find the shortest path
+    //greedy solution, so if y > x, the fastest way is y = y /2, 
+    //if not we make y+1, then next step is /2
     //y > x, then we want to 
     public int brokenCalc(int x, int y) {
         int res = 0;
         while(y > x) {
+            //the tricky part is y+1, why?
+            //
             y = y % 2 > 0 ? y+1 : y /2;
             res++;
         }
-        
+        //y < x already, so we want how many steps y need to substract 1
+        //totally
         return res + x - y;
     }
     
