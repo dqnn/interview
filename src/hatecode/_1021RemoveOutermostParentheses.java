b7201345ec679af5f6ae440e7c9c1e2e69d7f454
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-18 03:25:38 -0700

    more comments and solutions

--- src/hatecode/_1021RemoveOutermostParentheses.java
+++ src/hatecode/_1021RemoveOutermostParentheses.java
@@ -1,5 +1,5 @@
 package hatecode;
-public class RemoveOutermostParentheses {
+public class _1021RemoveOutermostParentheses {
 /*
 1021. Remove Outermost Parentheses
 A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
