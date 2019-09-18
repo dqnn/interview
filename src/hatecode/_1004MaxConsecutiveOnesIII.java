c6eefce4b6c206ad1e199a100a996484ab865a94
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-18 02:09:53 -0700

    add number to class name

--- src/hatecode/_1004MaxConsecutiveOnesIII.java
+++ src/hatecode/_1004MaxConsecutiveOnesIII.java
@@ -1,5 +1,5 @@
 package hatecode;
-public class MaxConsecutiveOnesIII {
+public class _1004MaxConsecutiveOnesIII {
 /*
 1004. Max Consecutive Ones III
 Given an array A of 0s and 1s, we may change up to K values from 0 to 1.
