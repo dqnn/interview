a1f52429b092c9f4481dbb1b23435c30409d9b95
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-18 03:19:27 -0700

    add more comments

--- src/hatecode/_1017ConvertToBase2.java
+++ src/hatecode/_1017ConvertToBase2.java
@@ -2,9 +2,11 @@ package hatecode;
 public class _1017ConvertToBase2 {
 /*
 1017. Convert to Base -2
-Given a number N, return a string consisting of "0"s and "1"s that represents its value in base -2 (negative two).
+Given a number N, return a string consisting of "0"s and "1"s 
+that represents its value in base -2 (negative two).
 
-The returned string must have no leading zeroes, unless the string is "0".
+The returned string must have no leading zeroes, unless the 
+string is "0".
 
  
 
