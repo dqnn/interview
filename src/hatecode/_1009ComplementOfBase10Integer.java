1b79f6baa850a59bcb624673fc49955a7665d1dc
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-18 02:47:25 -0700

    refactor

--- src/hatecode/_1009ComplementOfBase10Integer.java
+++ src/hatecode/_1009ComplementOfBase10Integer.java
@@ -2,11 +2,18 @@ package hatecode;
 public class _1009ComplementOfBase10Integer {
 /*
 1009. Complement of Base 10 Integer
-Every non-negative integer N has a binary representation.  For example, 5 can be represented as "101" in binary, 11 as "1011" in binary, and so on.  Note that except for N = 0, there are no leading zeroes in any binary representation.
+Every non-negative integer N has a binary representation.  
+For example, 5 can be represented as "101" in binary, 
+11 as "1011" in binary, and so on.  
+Note that except for N = 0, there are no leading zeroes in 
+any binary representation.
 
-The complement of a binary representation is the number in binary you get when changing every 1 to a 0 and 0 to a 1.  For example, the complement of "101" in binary is "010" in binary.
+The complement of a binary representation is the number in 
+binary you get when changing every 1 to a 0 and 0 to a 1.  
+For example, the complement of "101" in binary is "010" in binary.
 
-For a given number N in base-10, return the complement of it's binary representation as a base-10 integer.
+For a given number N in base-10, return the complement of 
+it's binary representation as a base-10 integer.
 Example 1:
 
 Input: 5
