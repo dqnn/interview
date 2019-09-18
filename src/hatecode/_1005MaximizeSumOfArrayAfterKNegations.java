18b22b9b2364e6ed2a971afa5fe5a0e16983bb9c
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-18 02:12:47 -0700

    more comments and solutions

--- src/hatecode/_1005MaximizeSumOfArrayAfterKNegations.java
+++ src/hatecode/_1005MaximizeSumOfArrayAfterKNegations.java
@@ -1,11 +1,15 @@
 package hatecode;
 import java.util.*;
-public class MaximizeSumOfArrayAfterKNegations {
+public class _1005MaximizeSumOfArrayAfterKNegations {
 /*
 1005. Maximize Sum Of Array After K Negations
-Given an array A of integers, we must modify the array in the following way: we choose an i and replace A[i] with -A[i], and we repeat this process K times in total.  (We may choose the same index i multiple times.)
+Given an array A of integers, we must modify the array in the 
+following way: we choose an i and replace A[i] with -A[i], 
+and we repeat this process K times in total.  
+(We may choose the same index i multiple times.)
 
-Return the largest possible sum of the array after modifying it in this way.
+Return the largest possible sum of the array after modifying 
+it in this way.
 
  
 
@@ -15,6 +19,8 @@ Input: A = [4,2,3], K = 1
 Output: 5
 Explanation: Choose indices (1,) and A becomes [4,-2,3].
 */
+    //thinking process:
+    //
     public int largestSumAfterKNegations(int[] A, int K) {
         if (A == null ||A.length < 1) return 0;
         
