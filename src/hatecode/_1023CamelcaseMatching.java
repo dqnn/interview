9f8be388e03b208a96cacac9bf38efdd6dc77283
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-20 03:03:23 -0700

    change the class name

--- src/hatecode/_1023CamelcaseMatching.java
+++ src/hatecode/_1023CamelcaseMatching.java
@@ -4,14 +4,9 @@ import java.util.*;
 public class _1023CamelcaseMatching {
 /*
 1023. Camelcase Matching
-A query word matches a given pattern if we can insert lowercase 
-letters to the pattern word so that it equals the query. 
-(We may insert each character at any position, and may insert 
-0 characters.)
+A query word matches a given pattern if we can insert lowercase letters to the pattern word so that it equals the query. (We may insert each character at any position, and may insert 0 characters.)
 
-Given a list of queries, and a pattern, return an answer list 
-of booleans, where answer[i] is true if and only if queries[i] 
-matches the pattern.
+Given a list of queries, and a pattern, return an answer list of booleans, where answer[i] is true if and only if queries[i] matches the pattern.
 
  
 
@@ -19,7 +14,16 @@ Example 1:
 
 Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FB"
 Output: [true,false,true,true,false]
+Explanation: 
+"FooBar" can be generated like this "F" + "oo" + "B" + "ar".
+"FootBall" can be generated like this "F" + "oot" + "B" + "all".
+"FrameBuffer" can be generated like this "F" + "rame" + "B" + "uffer".
 */
+    //thinking process:
+    //given a pattern and list of words, if words[i] can be achieved by insert
+    //lower cases to pattern, then it is true
+    
+    //
     public List<Boolean> camelMatch(String[] queries, String pattern) {
         List<Boolean> res = new ArrayList<>();
         
@@ -37,6 +41,7 @@ Output: [true,false,true,true,false]
         for (int i = 0; i < queryArr.length; i++) {
             if (j < patternArr.length && queryArr[i] == patternArr[j]) {
                 j++;
+            //if upper case and not equals then it is wrong
             } else if (queryArr[i] >= 'A' && queryArr[i] <= 'Z') {
                 return false;
             }
