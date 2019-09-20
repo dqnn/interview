dab8cb627edb1f76dc117e070d6d51eb0c39db79
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-20 02:48:39 -0700

    more comments

--- src/hatecode/_1022SumOfRootToLeafBinaryNumbers.java
+++ src/hatecode/_1022SumOfRootToLeafBinaryNumbers.java
@@ -3,9 +3,14 @@ package hatecode;
 public class _1022SumOfRootToLeafBinaryNumbers {
 /*
 1022. Sum of Root To Leaf Binary Numbers
-Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+Given a binary tree, each node has value 0 or 1.  
+Each root-to-leaf path represents a binary number starting 
+with the most significant bit.  For example, if the path 
+is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in 
+binary, which is 13.
 
-For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+For all leaves in the tree, consider the numbers 
+represented by the path from the root to that leaf.
 
 Return the sum of these numbers.
 
