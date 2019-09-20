0d1e3125dee111687f800e37f49b3010470b65cc
dy/dx <qiangddu@gmail.com>
2019-09-19 18:19:07 -0700

    Update _1021RemoveOutermostParentheses.java

--- src/hatecode/_1021RemoveOutermostParentheses.java
+++ src/hatecode/_1021RemoveOutermostParentheses.java
@@ -18,6 +18,10 @@ Input: "(()())(())"
 Output: "()()()"
 */
     //thinking process: the input S is valid, we want to remove 
+    //the problem is to say: given a valid parenthese string s, for each substring valid parenthese, if we removed the outest parenthese
+ // the rest still valid, just return it. the primitive string of parenthese is to say it is the cleanest, no way to remove any parenthese, 
+ //return result is all primitive valid parethese strings
+ // 
     //outest parentness, like ()()->""
     //this is used opened as indicator we can remove outest parentness
     public String removeOuterParentheses(String S) {
