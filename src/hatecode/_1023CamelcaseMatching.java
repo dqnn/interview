9db8617ba9b32c6b6f219672fdea9ebdc7e8044e
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-20 03:01:21 -0700

    more comments and solutions

--- src/hatecode/_1023CamelcaseMatching.java
+++ src/hatecode/_1023CamelcaseMatching.java
@@ -4,9 +4,14 @@ import java.util.*;
 public class _1023CamelcaseMatching {
 /*
 1023. Camelcase Matching
-A query word matches a given pattern if we can insert lowercase letters to the pattern word so that it equals the query. (We may insert each character at any position, and may insert 0 characters.)
+A query word matches a given pattern if we can insert lowercase 
+letters to the pattern word so that it equals the query. 
+(We may insert each character at any position, and may insert 
+0 characters.)
 
-Given a list of queries, and a pattern, return an answer list of booleans, where answer[i] is true if and only if queries[i] matches the pattern.
+Given a list of queries, and a pattern, return an answer list 
+of booleans, where answer[i] is true if and only if queries[i] 
+matches the pattern.
 
  
 
