8ee594090b4bea5125cbf32fa50dbffe7c6118bd
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-22 02:11:01 -0700

    add more comments

--- src/hatecode/_1025DivisorGame.java
+++ src/hatecode/_1025DivisorGame.java
@@ -24,13 +24,14 @@ Output: true
     //return true if and only if first player wins
     
     //dp[i] means if alice move, true is win, false is lose
+    //
     public boolean divisorGame(int n) {
         boolean[] dp = new boolean[n +1];
         dp[0] = false;
         dp[1] = false;
         for(int i = 2; i<=n; i++) {
             for(int j = 1; j<i;j++) {
-                if (i %j == 0 && !dp[i -j]) {
+                if (i % j == 0 && !dp[i -j]) {
                     dp[i] = true;
                     break;
                 }
