00aff65d69d41b24ea920c2db0c9b91307fd8763
qiangddu@gmail.com <qiangddu@gmail.com>
2019-09-29 01:28:48 -0700

    add more problems

---
+++ src/hatecode/Solution.java
@@ -0,0 +1,29 @@
+package hatecode;
+class Solution {
+/*
+1035. Uncrossed Lines
+We write the integers of A and B (in the order they are given) on two separate horizontal lines.
+
+Now, we may draw connecting lines: a straight line connecting two numbers A[i] and B[j] such that:
+
+A[i] == B[j];
+The line we draw does not intersect any other connecting (non-horizontal) line.
+Note that a connecting lines cannot intersect even at the endpoints: each number can only belong to one connecting line.
+
+Return the maximum number of connecting lines we can draw in this way.
+*/
+    //thinking process:O(rc)/O(rc)
+    public int maxUncrossedLines(int[] A, int[] B) {
+        if(A == null || A.length < 1 || B == null || B.length < 1) return 0;
+        
+        int r = A.length, c = B.length;
+        int[][] dp = new int[r+1][c+1];
+        for(int i = 1; i<= r; i++) {
+            for(int j =1; j<=c; j++) {
+                if(A[i-1] == B[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+        return dp[r][c];
+    }
+}
