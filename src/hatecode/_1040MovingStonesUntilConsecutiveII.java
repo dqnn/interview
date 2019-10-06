42144204872af336b064a537cc91f5a4b3479a56
qiangddu@gmail.com <qiangddu@gmail.com>
2019-10-05 19:26:50 -0700

    add more comments and solutions

---
+++ src/hatecode/_1040MovingStonesUntilConsecutiveII.java
@@ -0,0 +1,43 @@
+package hatecode;
+
+import java.util.*;
+public class _1040MovingStonesUntilConsecutiveII {
+/*
+1040. Moving Stones Until Consecutive II
+On an infinite number line, the position of the i-th stone is given by stones[i].  Call a stone an endpoint stone if it has the smallest or largest position.
+
+Each turn, you pick up an endpoint stone and move it to an unoccupied position so that it is no longer an endpoint stone.
+
+In particular, if the stones are at say, stones = [1,2,5], you cannot move the endpoint stone at position 5, since moving it to any position (such as 0, or 3) will still keep that stone as an endpoint stone.
+
+The game ends when you cannot make any more moves, ie. the stones are in consecutive positions.
+
+When the game ends, what is the minimum and maximum number of moves that you could have made?  Return the answer as an length 2 array: answer = [minimum_moves, maximum_moves]
+
+ 
+
+Example 1:
+
+Input: [7,4,9]
+Output: [1,2]
+Explanation: 
+We can move 4 -> 8 for one move to finish the game.
+Or, we can move 9 -> 5, 4 -> 6 for two moves to finish the game.
+*/
+    //thinking process: 
+    
+    //
+   public int[] numMovesStonesII(int[] A) {
+        Arrays.sort(A);
+        int i = 0, n = A.length, low = n;
+        int high = Math.max(A[n - 1] - n + 2 - A[1], A[n - 2] - A[0] - n + 2);
+        for (int j = 0; j < n; ++j) {
+            while (A[j] - A[i] >= n) ++i;
+            if (j - i + 1 == n - 1 && A[j] - A[i] == n - 2)
+                low = Math.min(low, 2);
+            else
+                low = Math.min(low, n - (j - i + 1));
+        }
+        return new int[] {low, high};
+    }
+}
