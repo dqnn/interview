c234f31cf3ea263ace23639b65826be2aff9c284
qiangddu@gmail.com <qiangddu@gmail.com>
2019-10-20 04:26:39 -0700

    add new problems

---
+++ src/hatecode/_1049LastStoneWeightII.java
@@ -0,0 +1,37 @@
+package hatecode;
+public class _1049LastStoneWeightII {
+/*
+1049. Last Stone Weight II
+We have a collection of rocks, each rock has a positive integer weight.
+
+Each turn, we choose any two rocks and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the smallest possible weight of this stone (the weight is 0 if there are no stones left.)
+
+ 
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+*/
+    
+    //thinking process: 
+    //
+    public int lastStoneWeightII(int[] A) {
+        if(A == null || A.length < 1) return 0;
+        boolean[] dp = new boolean[1501];
+        dp[0] = true;
+        int sumA = 0;
+        for (int a : A) {
+            sumA += a;
+            for (int i = Math.min(1500, sumA); i >= a; --i)
+                dp[i] |= dp[i - a];
+        }
+        for (int i = sumA / 2; i > 0; --i)
+            if (dp[i]) return sumA - i - i;
+        return 0;
+    }
+}
