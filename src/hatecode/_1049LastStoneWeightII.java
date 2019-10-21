4e88a09056e6cd2faa8c56783e44cf4029cd59ff
qiangddu@gmail.com <qiangddu@gmail.com>
2019-10-20 17:54:16 -0700

    more comments and solutions

--- src/hatecode/_1049LastStoneWeightII.java
+++ src/hatecode/_1049LastStoneWeightII.java
@@ -39,7 +39,26 @@ Output: 1
 
     same as 494. Target Sum
     */
-    public int lastStoneWeightII(int[] A) {
+    
+    public int lastStoneWeightII(int[] stones) {
+        int S = 0, S2 = 0;
+        for (int s : stones) S += s;
+        int n = stones.length;
+        boolean[][] dp = new boolean[S + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[0][i] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= S / 2; s++) {
+                if (dp[s][i - 1] || (s >= stones[i - 1] && dp[s - stones[i - 1]][i - 1])) {
+                    dp[s][i] = true;
+                    S2 = Math.max(S2, s);
+                }
+            }
+        }
+        return S - 2 * S2;
+    }
+    public int lastStoneWeightII_DP(int[] A) {
         if(A == null || A.length < 1) return 0;
         boolean[] dp = new boolean[1501];
         dp[0] = true;
