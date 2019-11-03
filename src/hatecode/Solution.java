361d192e427d273563e2412461262926ebc5824d
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-03 01:41:03 -0800

    more comments

--- src/hatecode/Solution.java
+++ src/hatecode/Solution.java
@@ -4,9 +4,13 @@ import java.util.*;
 class Solution {
 /*
 1072. Flip Columns For Maximum Number of Equal Rows
-Given a matrix consisting of 0s and 1s, we may choose any number of columns in the matrix and flip every cell in that column.  Flipping a cell changes the value of that cell from 0 to 1 or from 1 to 0.
+Given a matrix consisting of 0s and 1s, we may choose any number 
+of columns in the matrix and flip every cell in that column.  
+Flipping a cell changes the value of that cell from 0 to 1 or 
+from 1 to 0.
 
-Return the maximum number of rows that have all values equal after some number of flips.
+Return the maximum number of rows that have all values equal 
+after some number of flips.
 
  
 
