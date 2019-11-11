b9271af8b7a3d513e57d9305da0f5fe45ae28aa6
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-10 19:59:37 -0800

    more comments

--- src/hatecode/_1081SmallestSubsequenceOfDistinctCharacters.java
+++ src/hatecode/_1081SmallestSubsequenceOfDistinctCharacters.java
@@ -11,18 +11,22 @@ Example 1:
 Input: "cdadabcc"
 Output: "adbc"
 */
-    //thinking process:O(n)/O(1),1 is because we have limited character, max 26
+    //thinking process:O(n)/O(1),1 is because we have limited character, max 26,
+    //O(n) if we think res used n space
     //given string s, return the smallest lexi order subsequence, which each character
     //in s will only appear once
     
-    //for example, we process[0, n] substring, 
-    //we pick 1 character from string, then we compare to current result set substring, 
+    //for example, cdadabcc
+    //c,d will be in res, then come a, we found d>a and cnt['d'-'a']> 0, so we remove d,
+    //we compare c vs a, still remove c, and put a there, res is used like a stack. see 
+    //below stack solution
     
     //so we use used[] to indicate whether we used this character or not, everytime, we would
     //increase 1,
     //we get char c, and comapre to last character in res, if it is bigger than current char
     //and last character count bigger than 0, then we can remove last character and mark
-    //used[] as 0, 
+    //used[] as 0, this step is to remove non-qualified sequence
+    
     public String smallestSubsequence(String s) {
         List<Character> res = new ArrayList<>();
         int[] used = new int[26];
