ef7f5d70cb67668c3a83ef2b1b5cf38541dc2517
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-10 17:14:28 -0800

    add more comments

--- src/hatecode/_1080InsufficientNodesInRootToLeafPaths.java
+++ src/hatecode/_1080InsufficientNodesInRootToLeafPaths.java
@@ -13,13 +13,21 @@ Example 1:
 Input: root = [1,2,3,4,-99,-99,7,8,9,-99,-99,12,13,-99,14], limit = 1
 Output: [1,2,3,4,null,null,7,8,9,null,14]
 */
+    //thinking process: O(n)/O(lgn),lgn is mem recursion management
+    //given a binary tree, from each path from root to leaf, if one node, with every
+    //path sum less than limit, then remove this node and return the new root
+    
+    //this solution is BEST, i really was surprised by root.left == root.right, this is 
+    //COOL code i have never seen
     public TreeNode sufficientSubset(TreeNode root, int limit) {
         if (root == null) return null;
+        //if this is the leaf and we found the it is less than limit,then ew return the result
         if (root.left == null && root.right == null)
             return root.val < limit ? null : root;
         
         root.left = sufficientSubset(root.left, limit - root.val);
         root.right = sufficientSubset(root.right, limit - root.val);
+        //this is the key, 
         return root.left == root.right ? null : root;
     }
 }
