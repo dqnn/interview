f7c96face7fcbbc1bea200bd84778a85eca8764d
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-10 20:50:30 -0800

    more commments

--- src/hatecode/_1086HighFive.java
+++ src/hatecode/_1086HighFive.java
@@ -15,6 +15,13 @@ Example 1:
 Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
 Output: [[1,87],[2,88]]
 */
+    //thinking process:O(n)/O(n), so map use n people, n here is generail mean linear 
+    
+    //given 2D array items, item[0][0] means id, item[0][1] means score, so 
+    //return another 2D array with each student top 5 average
+    
+    //we use List to store all courses score and map to each student id,
+    //then just compute on first 5, i used sublist here, so i don;t use priorityQUeue
     public int[][] highFive(int[][] items) {
         if(items == null || items.length <1 || items[0].length < 1) return new int[0][0];
         
