da48d295d3d0c014fb725008ffca9caf3628e36f
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-10 17:39:24 -0800

    add more comments

--- src/hatecode/_316RemoveDuplicateLetters.java
+++ src/hatecode/_316RemoveDuplicateLetters.java
@@ -10,11 +10,12 @@ import java.util.HashMap;
  * Date : Sep, 2018
  * Description : 316. Remove Duplicate Letters
  */
-public class RemoveDuplicateLetters {
+public class _316RemoveDuplicateLetters {
     /**
 Given a string which contains only lowercase letters, remove duplicate letters 
 so that every letter appear once and only once. You must make sure your result
  is the smallest in lexicographical order among all possible results.
+ keep the sequence
 
 Example 1:
 
