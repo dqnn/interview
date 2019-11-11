aa08776f7d0ca0c6c4e9e630b173b0f1aec33c1f
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-10 17:20:36 -0800

    refactor a little bit

--- src/hatecode/_1080InsufficientNodesInRootToLeafPaths.java
+++ src/hatecode/_1080InsufficientNodesInRootToLeafPaths.java
@@ -50,6 +50,7 @@ else we return root.
         root.left = sufficientSubset(root.left, limit - root.val);
         root.right = sufficientSubset(root.right, limit - root.val);
         //this is the key, means no path to leaf, need to remove 
-        return root.left == root.right ? null : root;
+        //root.left == root.right ? null : root;
+        return root.left == null && root.right == null ? null : root;
     }
 }
