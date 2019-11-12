73120a5bc1e0f9c3c1c2fd23fc8d08d2d499b402
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-12 01:56:30 -0800

    more comments

--- src/hatecode/_1091ShortestPathInBinaryMatrix.java
+++ src/hatecode/_1091ShortestPathInBinaryMatrix.java
@@ -23,7 +23,7 @@ Output: 4
     //given 2D matrix, 0 is empty, 1 is blocked, find the shortest path from top left
     //to right bottom
     
-    //
+    //we use BFS to find shortest, but make sure we should get all elements in queue in one bfs
     public int shortestPathBinaryMatrix(int[][] m) {
         if(m == null || m.length < 1 || m[0].length < 1 || m[0][0] == 1) return -1;
         
