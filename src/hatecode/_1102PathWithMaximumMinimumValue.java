ff79f1a67fe67cc1d618ba6c814bfdd08e91e75a
qiangddu@gmail.com <qiangddu@gmail.com>
2019-11-22 03:04:12 -0800

    add more comments

--- src/hatecode/_1102PathWithMaximumMinimumValue.java
+++ src/hatecode/_1102PathWithMaximumMinimumValue.java
@@ -6,9 +6,12 @@ public class _1102PathWithMaximumMinimumValue {
 1102. Path With Maximum Minimum Value
 Given a matrix of integers A with R rows and C columns, find the maximum score of a path starting at [0,0] and ending at [R-1,C-1].
 
-The score of a path is the minimum value in that path.  For example, the value of the path 8 →  4 →  5 →  9 is 4.
+The score of a path is the minimum value in that path.  
+For example, the value of the path 8 →  4 →  5 →  9 is 4.
 
-A path moves some number of times from one visited cell to any neighbouring unvisited cell in one of the 4 cardinal directions (north, east, west, south).
+A path moves some number of times from one visited cell to any 
+neighbouring unvisited cell in one of the 4 cardinal directions 
+(north, east, west, south).
 Example 1:
 
 Input: [[5,4,5],[1,2,6],[7,4,6]]
@@ -16,7 +19,10 @@ Output: 4
 */
     //thinking process： 
     
-    //
+    //given a matrix, each cell has an integer value, so find one path from 
+    //left top to bottom right, then return the min value during this path,
+    
+    //so bfs, 
     public int maximumMinimumPath(int[][] A) {
         final int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
         Queue<int[]> pq = new PriorityQueue<>((a, b) -> (b[0] - a[0]));
