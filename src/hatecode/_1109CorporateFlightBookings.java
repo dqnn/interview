2e5caf107c9ca53301784bb5971054e28d513937
qiangddu@gmail.com <qiangddu@gmail.com>
2019-12-15 23:09:07 -0800

    more comments and solutions

--- src/hatecode/_1109CorporateFlightBookings.java
+++ src/hatecode/_1109CorporateFlightBookings.java
@@ -20,8 +20,8 @@ Output: [10,55,45,25,25]
     
     //thinking process: O(n)/O(n)
     
-    //given a flight 2D array as bookings,[i,j,k]->frm i to j reserved k seats,
-    //return an array which contains seats for all stops, all stops is another paramter n
+    //given a flight 2D array as bookings,[i,j,k]->from i to j reserved k seats,
+    //return an array which contains seats for all stops, all stops is another parameter n
     
     //
     public int[] corpFlightBookings(int[][] bookings, int n) {
@@ -33,7 +33,7 @@ Output: [10,55,45,25,25]
         for (int i = 1; i < n; ++i) res[i] += res[i - 1];
         return res;
     }
-    
+
     //TLE solution
     public int[] corpFlightBookings_TLE(int[][] bookings, int n) {
         if(bookings == null || bookings.length < 1) return new int[0];
