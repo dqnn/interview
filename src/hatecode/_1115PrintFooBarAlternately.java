dee259c3cdc7ca1506c39595457326e52422d20a
qiangddu@gmail.com <qiangddu@gmail.com>
2020-01-11 23:34:34 -0800

    more comments

--- src/hatecode/_1115PrintFooBarAlternately.java
+++ src/hatecode/_1115PrintFooBarAlternately.java
@@ -64,7 +64,7 @@ Output: "foobar"
         for (int i = 0; i < n; i++) {
             mu2.acquire();
             // printBar.run() outputs "bar". Do not change or remove this line.
-        	printBar.run();
+            printBar.run();
             mu1.release();
         }
     }
