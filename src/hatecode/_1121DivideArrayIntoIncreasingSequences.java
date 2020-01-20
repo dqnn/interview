250292035d25db7adbf0590ca2150c9805dd8d2b
qiangddu@gmail.com <qiangddu@gmail.com>
2020-01-19 19:25:46 -0800

    more comments

---
+++ src/hatecode/_1121DivideArrayIntoIncreasingSequences.java
@@ -0,0 +1,43 @@
+package hatecode;
+public class _1121DivideArrayIntoIncreasingSequences {
+/*
+1121. Divide Array Into Increasing Sequences
+Given a non-decreasing array of positive integers nums and an integer K, find out if this array can be divided into one or more disjoint increasing subsequences of length at least K.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3,3,4,4], K = 3
+Output: true
+*/
+  
+    public boolean canDivideIntoSubsequences(int[] A, int K) {
+        int cur = 1, groups = 1, n = A.length;
+        for (int i = 1; i < n; i++) {
+            cur = A[i - 1] < A[i] ?  1 : cur + 1;
+            groups = Math.max(groups, cur);
+        }
+        return n >= K * groups;
+    }
+    
+    public boolean canDivideIntoSubsequences_BestPerformance(int[] nums, int K) {
+        int n = nums.length;
+        int max = 1;
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+                if (count > max) {
+                    max = count;
+                    if (max * K > n) {
+                        return false;
+                    }
+                }
+            } else {
+                count = 1;
+            }
+        }
+        return max * K <= n;
+    }
+}
