0005c10c621c70ce3291e98c2828c07c1695cf07
qiangddu@gmail.com <qiangddu@gmail.com>
2020-01-19 19:00:36 -0800

    more comments

--- src/hatecode/_1120MaximumAverageSubtree.java
+++ src/hatecode/_1120MaximumAverageSubtree.java
@@ -9,11 +9,7 @@ Given the root of a binary tree, find the maximum average value of any subtree o
 
 (A subtree of a tree is any node of that tree plus all its descendants. The average value of a tree is the sum of its values, divided by the number of nodes.)
 
- 
-
 Example 1:
-
-
 
 Input: [5,6,1]
 Output: 6.00000
