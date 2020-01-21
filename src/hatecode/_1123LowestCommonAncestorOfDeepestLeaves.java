9f0b41c659b11168634eb82d5428a1b1bae6a02c
qiangddu@gmail.com <qiangddu@gmail.com>
2020-01-20 17:45:38 -0800

    more comments

--- src/hatecode/_1123LowestCommonAncestorOfDeepestLeaves.java
+++ src/hatecode/_1123LowestCommonAncestorOfDeepestLeaves.java
@@ -8,8 +8,8 @@ Given a rooted binary tree, return the lowest common ancestor of its deepest lea
 Recall that:
 
 The node of a binary tree is a leaf if and only if it has no children
-The depth of the root of the tree is 0, a
-nd if the depth of a node is d, 
+The depth of the root of the tree is 0, 
+and if the depth of a node is d, 
 the depth of each of its children is d+1.
 The lowest common ancestor of a set S of nodes is the node 
 A with the largest depth such that every node in S is in 
