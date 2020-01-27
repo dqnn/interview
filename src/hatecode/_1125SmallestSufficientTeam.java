9c5aff6a966c108f394ec7470cb5e4b968699ec0
qiangddu@gmail.com <qiangddu@gmail.com>
2020-01-27 03:17:44 -0800

    new comments

--- src/hatecode/_1125SmallestSufficientTeam.java
+++ src/hatecode/_1125SmallestSufficientTeam.java
@@ -24,7 +24,7 @@ Example 2:
 Input: req_skills = ["algorithms","math","java","reactjs","csharp","aws"], people = [["algorithms","math","java"],["algorithms","math","reactjs"],["java","csharp","aws"],["reactjs","csharp"],["csharp","math"],["aws","java"]]
 Output: [1,2]
  */
-
+    // thinking process: O()
     public int[] smallestSufficientTeam(String[] skills, List<List<String>> people) {
         int sLen = skills.length, pLen = people.size();
         
