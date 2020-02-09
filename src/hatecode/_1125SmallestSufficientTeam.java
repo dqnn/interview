3af3bf23f4f36161e325cd1243c2b5f786c17ba4
qiangddu@gmail.com <qiangddu@gmail.com>
2020-02-09 04:24:37 -0800

    add tags

--- src/hatecode/_1125SmallestSufficientTeam.java
+++ src/hatecode/_1125SmallestSufficientTeam.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class _1125SmallestSufficientTeam {
 /*
  * 1125. Smallest Sufficient Team
+ * tags: set cover
  * In a project, you have a list of required skills req_skills, and a list of people.  The i-th person people[i] contains a list of skills that person has.
 
 Consider a sufficient team: a set of people such that for every required skill in req_skills, there is at least one person in the team who has that skill.  We can represent these teams by the index of each person: for example, team = [0, 1, 3] represents the people with skills people[0], people[1], and people[3].
