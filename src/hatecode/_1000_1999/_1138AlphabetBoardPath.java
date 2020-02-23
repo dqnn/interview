6d8107c9a123b52862d9241ce2ad0b81c5fcc3c8
qiangddu@gmail.com <qiangddu@gmail.com>
2020-02-23 01:49:38 -0800

    more comments

--- src/hatecode/_1000_1999/_1138AlphabetBoardPath.java
+++ src/hatecode/_1000_1999/_1138AlphabetBoardPath.java
@@ -49,28 +49,30 @@ Output: "DDR!UURRR!!DDD!"
           return sb.toString();
     }
     
-         public String alphabetBoardPath_BestPerf(String target) {
-            StringBuilder sb = new StringBuilder();
-            for (int n = 0, i = 0, j = 0; n < target.length(); ++n) {
-                int pos = target.charAt(n) - 'a', row = pos / 5, col = pos % 5;
-                while (j > col) { // while loop of left (L) move before that of downward (D) move.
-                    sb.append('L');
-                    --j;
-                }
-                while (i > row) { // while loop of upward (U) move before that of right (R) move.
-                    sb.append('U');
-                    --i;
-                }
-                while (i < row) { // while loop of downward (D) move.
-                    sb.append('D');
-                    ++i;
-                }
-                while (j < col) { // while loop of right (R) move.
-                    sb.append('R');
-                    ++j;
-                }
-                sb.append('!');   
+    //thinking process: 
+    //
+    public String alphabetBoardPath_BestPerf(String target) {
+        StringBuilder sb = new StringBuilder();
+        for (int n = 0, i = 0, j = 0; n < target.length(); n++) {
+            int pos = target.charAt(n) - 'a', row = pos / 5, col = pos % 5;
+            while (j > col) { // while loop of left (L) move before that of downward (D) move.
+                sb.append('L');
+                --j;
             }
-            return sb.toString();   
+            while (i > row) { // while loop of upward (U) move before that of right (R) move.
+                sb.append('U');
+                --i;
+            }
+            while (i < row) { // while loop of downward (D) move.
+                sb.append('D');
+                ++i;
+            }
+            while (j < col) { // while loop of right (R) move.
+                sb.append('R');
+                ++j;
+            }
+            sb.append('!');
+        }
+        return sb.toString();
     }
 }
