40a30350d21262842b00ee1926c27976d1c4df5c
qiangddu@gmail.com <qiangddu@gmail.com>
2020-03-15 09:09:48 -0700

    add ne wproblems

---
+++ src/hatecode/_1000_1999/_1144DecreaseElementsToMakeArrayZigzag.java
@@ -0,0 +1,46 @@
+package hatecode._1000_1999;
+public class _1144DecreaseElementsToMakeArrayZigzag {
+/*
+1144. Decrease Elements To Make Array Zigzag
+Given an array nums of integers, a move consists of choosing any element and decreasing it by 1.
+
+An array A is a zigzag array if either:
+
+Every even-indexed element is greater than adjacent elements, ie. A[0] > A[1] < A[2] > A[3] < A[4] > ...
+OR, every odd-indexed element is greater than adjacent elements, ie. A[0] < A[1] > A[2] < A[3] > A[4] < ...
+Return the minimum number of moves to transform the given array nums into a zigzag array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: 2
+*/
+    public int movesToMakeZigzag(int[] A) {
+        if(A == null || A.length < 1) return 0;
+        
+        int res[] = new int[2],  n = A.length, left, right;
+        for (int i = 0; i < n; ++i) {
+            left = i > 0 ? A[i - 1] : 1001;
+            right = i + 1 < n ? A[i + 1] : 1001;
+            res[i % 2] += Math.max(0, A[i] - Math.min(left, right) + 1);
+        }
+        return Math.min(res[0], res[1]);
+    }
+    
+    public int movesToMakeZigzag_BF(int[] nums) {
+        int e = 0, o = 0, n = nums.length;
+        for(int i=1; i < n; i+= 2){
+            int min = Math.min(nums[i-1], i+1 < n ? nums[i+1] : 1000);
+            if(min <= nums[i])
+                e += (nums[i]-min+1);
+        }
+        for(int i=0; i < n; i+= 2){
+            int min = Math.min(i > 0 ? nums[i-1] : 1000, i+1 < n ? nums[i+1] : 1000);
+            if(min <= nums[i])
+                o += (nums[i]-min+1);
+        }
+        return Math.min(e,o);
+    }
+}
