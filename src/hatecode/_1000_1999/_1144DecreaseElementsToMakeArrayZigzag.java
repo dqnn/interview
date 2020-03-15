fd2c4dd45a23e33c0239d56e2b222fcea7520a1f
qiangddu@gmail.com <qiangddu@gmail.com>
2020-03-15 09:17:36 -0700

    nore ocmments

--- src/hatecode/_1000_1999/_1144DecreaseElementsToMakeArrayZigzag.java
+++ src/hatecode/_1000_1999/_1144DecreaseElementsToMakeArrayZigzag.java
@@ -16,19 +16,26 @@ Example 1:
 
 Input: nums = [1,2,3]
 Output: 2
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 1000
 */
+    //thinking process:O(n)/O(1)
+    
+    //we look for the diff
     public int movesToMakeZigzag(int[] A) {
         if(A == null || A.length < 1) return 0;
         
-        int res[] = new int[2],  n = A.length, left, right;
+        int res[] = new int[2],  n = A.length, l, r;
         for (int i = 0; i < n; ++i) {
-            left = i > 0 ? A[i - 1] : 1001;
-            right = i + 1 < n ? A[i + 1] : 1001;
-            res[i % 2] += Math.max(0, A[i] - Math.min(left, right) + 1);
+            l = i > 0 ? A[i - 1] : 1001;
+            r = i + 1 < n ? A[i + 1] : 1001;
+            res[i % 2] += Math.max(0, A[i] - Math.min(l, r) + 1);
         }
         return Math.min(res[0], res[1]);
     }
     
+    //
     public int movesToMakeZigzag_BF(int[] nums) {
         int e = 0, o = 0, n = nums.length;
         for(int i=1; i < n; i+= 2){
