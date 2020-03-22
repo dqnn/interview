268a16209ef02763ea2841324ab70a599922ecfd
qiangddu@gmail.com <qiangddu@gmail.com>
2020-03-22 01:20:02 -0700

    more comments

--- src/hatecode/_1000_1999/_1151MinimumSwapsToGroupAll1sTogether.java
+++ src/hatecode/_1000_1999/_1151MinimumSwapsToGroupAll1sTogether.java
@@ -14,6 +14,31 @@ Output: 1
 
     //thinking process: O(n)/O(1)
     
+    /*
+     * Pattern 1: When the size of the sliding window is fixed
+Some leetcode problems fall in this pattern whereby the size of the sliding window is 
+fixed and the window moves along the length of the array. The problems are :
+
+https://leetcode.com/problems/diet-plan-performance/
+https://leetcode.com/problems/find-k-length-substrings-with-no-repeated-characters
+
+template:
+
+windowSize - a variable that defines the size of the window
+arr - array consisting the list of numbers
+Element - represent the element of the array - it might be an integer, string, character (based on the array type)
+for(int i = 0; i < arr.length; i++) {
+  Element element = arr[i];
+  if( i > windowSize - 1) { 
+    // perform a series of actions as required by the problem.
+    // remove the first element of the window which has been visited 
+    // so that we could move ahead in the array without affecting      
+    // the window size
+  }
+}
+     *
+     */
+    
     //given one array, which only contains 0 or 1, then return the min swaps so all 1
     //are together, [1,0,1,0,1]->1, swap [0] and [4]
     
