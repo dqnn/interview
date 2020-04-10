8b6b6ea3e2148af7cd22e8a0780d2e86cd13f626
qiangddu@gmail.com <qiangddu@gmail.com>
2020-04-10 12:05:42 -0700

    more comments

--- src/hatecode/_1000_1999/_1154DayOfTheYear.java
+++ src/hatecode/_1000_1999/_1154DayOfTheYear.java
@@ -1,6 +1,7 @@
 package hatecode._1000_1999;
 public class _1154DayOfTheYear {
 /*
+ * This is not good coding question
 1154. Day of the Year
 Given a string date representing a Gregorian calendar date formatted as YYYY-MM-DD, return the day number of the year.
 
