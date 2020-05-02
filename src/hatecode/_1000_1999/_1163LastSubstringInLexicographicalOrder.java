f187f9cd9ae67d278bf8b59379d6a3fd19e9bf91
qiangddu@gmail.com <qiangddu@gmail.com>
2020-05-02 12:02:17 -0700

    more comments

--- src/hatecode/_1000_1999/_1163LastSubstringInLexicographicalOrder.java
+++ src/hatecode/_1000_1999/_1163LastSubstringInLexicographicalOrder.java
@@ -41,66 +41,45 @@ Case 3 (s[i+k]>s[j+k]):
 the second string to j+k+1. Because s[j]~s[j+k] must be less than s[i], otherwise "i" 
 will be updated by "j". So the next possible candidate is "j+k+1".
      */
+    
     public String lastSubstring(String s) {
         int l = s.length();
-        int i = 0, j = 1, k = 0;
-        while(j + k < l) {
-            if (s.charAt(i+k) == s.charAt(j+k)){
-                k++;
+        
+        //i is first string starting index
+        //j is 2nd string starting index
+        //w is the width of both strings
+        int i = 0, j = 1, w = 0;
+        while(j + w < l) {
+            if (s.charAt(i+w) == s.charAt(j+w)){
+                w++;
                 continue;
             }
-            if (s.charAt(i+k) > s.charAt(j+k)){
+            if (s.charAt(i+w) > s.charAt(j+w)){
                 j++;
             } else {
                 i = j;
                 j = i + 1;
             }
-            k = 0;
+            w = 0;
         }
         
         return s.substring(i);
     }
     
-    public static String lastSubstring_More(String s) {
-        if (s == null || s.length() < 2) {
-            return s;
-        }
-
-        int stringLen = s.length();
-        List<Integer> indexes = new ArrayList<>(stringLen);
-        char[] chars = s.toCharArray();
-        char max = 0;
-        char c = 0, prevChar = 0;
-        for (int i = 0; i < stringLen; i++) {
-            prevChar = c;
-            c = chars[i];
-            if (c != prevChar && c > max) {
-                max = c;
-                indexes.clear();
-                indexes.add(i);
-            } else if (c != prevChar && c == max) {
-                indexes.add(i);
+    //encoding solution
+    public String lastSubstring_encoding(String s) {
+        TreeSet<Character> ts = new TreeSet<>();
+        for (int i = 0; i < s.length(); ++i)
+            ts.add(s.charAt(i));
+        int radix = ts.size(), lo = 0;
+        double max = 0d, cur = 0d;
+        for (int i = s.length() - 1; i >= 0; --i) {
+            cur = ts.headSet(s.charAt(i)).size() + cur / radix;
+            if (max <= cur) {
+                max = cur;
+                lo = i;
             }
         }
-
-        int one = 0;
-        int two = 0;
-        int maxIdxStart = 0;
-        for (int i = 1; i < indexes.size(); i++) {
-            one = indexes.get(maxIdxStart) + 1;
-            two = indexes.get(i) + 1;
-            max = 0;
-            while (max == 0 && two < stringLen) {
-                if (chars[one] > chars[two]) {
-                    max = chars[one];
-                } else if (chars[two] > chars[one]) {
-                    max = chars[two];
-                    maxIdxStart = i;
-                }
-                one++;
-                two++;
-            }
-        }
-        return s.substring(indexes.get(maxIdxStart));
+        return s.substring(lo);
     }
 }
