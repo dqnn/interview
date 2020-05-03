0f7bf6058e6c4cf56f6e1d120e747326fd3b68c5
qiangddu@gmail.com <qiangddu@gmail.com>
2020-05-03 11:05:06 -0700

    more ocmments

--- src/hatecode/_1000_1999/_1166DesignFileSystem.java
+++ src/hatecode/_1000_1999/_1166DesignFileSystem.java
@@ -96,21 +96,23 @@ fileSystem.get("/a"); // return 1
     }
     
     class SimpleSolution {
-        Map<String, Integer> file = new HashMap<>(); 
-    
-    public SimpleSolution() {
-        file.put("", -1);
-    }
-    
-    public boolean create(String path, int value) {
-        int idx = path.lastIndexOf("/");
-        String parent = path.substring(0, idx);
-        if (!file.containsKey(parent)) { return false; }
-        return file.putIfAbsent(path, value) == null;   
-    }
-    
-    public int get(String path) {
-        return file.getOrDefault(path, -1);
-    }
+        Map<String, Integer> file = new HashMap<>();
+
+        public SimpleSolution() {
+            file.put("", -1);
+        }
+
+        public boolean create(String path, int value) {
+            int idx = path.lastIndexOf("/");
+            String parent = path.substring(0, idx);
+            if (!file.containsKey(parent)) {
+                return false;
+            }
+            return file.putIfAbsent(path, value) == null;
+        }
+
+        public int get(String path) {
+            return file.getOrDefault(path, -1);
+        }
     }
 }
