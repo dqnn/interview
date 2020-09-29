a808b3d7596232c233b3faf0d594ab4c48701b08
qiangddu@gmail.com <qiangddu@gmail.com>
2020-09-29 01:00:28 -0700

    more comments

--- src/hatecode/_1000_1999/_1181BeforeAndAfterPuzzle.java
+++ src/hatecode/_1000_1999/_1181BeforeAndAfterPuzzle.java
@@ -6,9 +6,11 @@ public class _1181BeforeAndAfterPuzzle {
 1181. Before and After Puzzle
 Given a list of phrases, generate a list of Before and After puzzles.
 
-A phrase is a string that consists of lowercase English letters and spaces only. No space appears in the start or the end of a phrase. There are no consecutive spaces in a phrase.
+A phrase is a string that consists of lowercase English letters and spaces only. 
+No space appears in the start or the end of a phrase. There are no consecutive spaces in a phrase.
 
-Before and After puzzles are phrases that are formed by merging two phrases where the last word of the first phrase is the same as the first word of the second phrase.
+Before and After puzzles are phrases that are formed by merging two phrases where 
+the last word of the first phrase is the same as the first word of the second phrase.
 
 Return the Before and After puzzles that can be formed by every two phrases phrases[i] and phrases[j] where i != j. Note that the order of matching two phrases matters, we want to consider both orders.
 
