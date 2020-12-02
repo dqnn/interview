ed18e6ee0047d7d81e736bb794bc7b25bdc7c3fa
qiangddu@gmail.com <qiangddu@gmail.com>
2020-12-02 02:10:36 -0800

    bew problem

---
+++ src/hatecode/_1000_1999/_1185DayOfTheWeek.java
@@ -0,0 +1,24 @@
+package hatecode._1000_1999;
+/*
+1185. Day of the Week
+Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+*/
+public class _1185DayOfTheWeek {
+
+    
+    String[] days = new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    public String dayOfTheWeek(int d, int m, int y) {
+        if (m < 3) {
+            m += 12;
+            y -= 1;
+        }
+        int c = y / 100;
+        y = y % 100;
+        int w = (c / 4 - 2 * c + y + y / 4 + 13 * (m + 1) / 5 + d - 1) % 7;
+        return days[(w + 7) % 7];
+    }
+}
