36acc5584b371810dd00a0d38173dc5c5eb63b68
qiangddu@gmail.com <qiangddu@gmail.com>
2020-12-26 02:14:51 -0800

    more solutions

--- src/hatecode/_1000_1999/_1186MaximumSubarraySumWithOneDeletion.java
+++ src/hatecode/_1000_1999/_1186MaximumSubarraySumWithOneDeletion.java
@@ -23,15 +23,19 @@ Output: 4
      * l[i] means from 0->i, at position i, the max sum for previous 0->i.
      * r[i] means from n-1-> i, scan from right to i, the max sum 
      * 
-     * so we can try all cases, 
+     * so we can try all cases by l[i-1] + r[i+1]
      */
     public int maximumSum(int[] A) {
         int n = A.length;
         int[] l = new int[n], r = new int[n];
         int max = A[0];
+        
         l[0] = A[0];
         for (int i = 1; i < n; i++) {
             l[i] = Math.max(A[i], l[i - 1] + A[i]);
+            //Key1: we need to know max element in this array, because later
+            //when l[i-1] + r[i+1], we are trying to say at least 2 elements, but one element 
+            //could be the case
             max = Math.max(max, l[i]);
         }
         r[n - 1] = A[n - 1];
@@ -57,6 +61,34 @@ Output: 4
             res = Math.max(Math.max(dpExclude, dp), res);
         }
 
+        return res;
+    }
+    
+    //here is suppose to solve the problem that we removed K elements, return the max
+    //sum of the sub array
+    
+    //dp[i][j] means 
+    public int maximumSum_K_Deletion(int[] A, int K) {
+        if (A == null || A.length < 1) return 0;
+
+        int n = A.length;
+
+        int[][] dp = new int[n + 1][K + 1];
+        for (int i = 0; i <= K; i++) {
+            dp[0][i] = Integer.MIN_VALUE;
+        }
+
+        int res = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int k = 0; k <= K; k++) {
+                if (dp[i - 1][k] >= 0) {
+                    dp[i][k] = Math.max((k - 1 >= 0 ? dp[i - 1][k - 1] : Integer.MIN_VALUE), dp[i - 1][k] + A[i - 1]);
+                } else {
+                    dp[i][k] = A[i - 1];
+                }
+                res = Math.max(res, dp[i][k]);
+            }
+        }
         return res;
     }
 }
