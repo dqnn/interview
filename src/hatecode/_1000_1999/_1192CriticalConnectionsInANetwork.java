7a017db04b235e5eaa0ee221907eea9140f69011
qiangddu@gmail.com <qiangddu@gmail.com>
2020-12-28 12:52:47 -0800

    more comments

--- src/hatecode/_1000_1999/_1192CriticalConnectionsInANetwork.java
+++ src/hatecode/_1000_1999/_1192CriticalConnectionsInANetwork.java
@@ -28,27 +28,29 @@ Return all critical connections in the network in any order.
         List<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
         
-        for(List<Integer> oneConnection :connections) {
-            graph[oneConnection.get(0)].add(oneConnection.get(1));
-            graph[oneConnection.get(1)].add(oneConnection.get(0));
+        for(List<Integer> conn :connections) {
+            graph[conn.get(0)].add(conn.get(1));
+            graph[conn.get(1)].add(conn.get(0));
         }
         int timer[] = new int[1];
         List<List<Integer>> results = new ArrayList<>();
         boolean[] visited = new boolean[n];
         int []timeStampAtThatNode = new int[n]; 
-        criticalConnectionsUtil(graph, -1, 0, timer, visited, results, timeStampAtThatNode);
+        helper(graph, -1, 0, timer, visited, results, timeStampAtThatNode);
         return results;
     }
     
     
-    public void criticalConnectionsUtil(List<Integer>[] graph, int parent, int node, int timer[], boolean[] visited, List<List<Integer>> results, int []timeStampAtThatNode) {
+    public void helper(List<Integer>[] graph, int parent, int node, 
+            int timer[], boolean[] visited, List<List<Integer>> results, 
+            int []timeStampAtThatNode) {
         visited[node] = true;
         timeStampAtThatNode[node] = timer[0]++;
         int currentTimeStamp = timeStampAtThatNode[node];
         
         for(int oneNeighbour : graph[node]) {
             if(oneNeighbour == parent) continue;
-            if(!visited[oneNeighbour]) criticalConnectionsUtil(graph, node, oneNeighbour, timer, visited, results, timeStampAtThatNode);
+            if(!visited[oneNeighbour]) helper(graph, node, oneNeighbour, timer, visited, results, timeStampAtThatNode);
             timeStampAtThatNode[node] = Math.min(timeStampAtThatNode[node], timeStampAtThatNode[oneNeighbour]);
             if(currentTimeStamp < timeStampAtThatNode[oneNeighbour]) results.add(Arrays.asList(node, oneNeighbour));
         }
