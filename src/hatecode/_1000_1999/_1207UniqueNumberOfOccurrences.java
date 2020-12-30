70b59f8b4be0ed85efc5848efcbc80395d9974b0
qiangddu@gmail.com <qiangddu@gmail.com>
2020-12-29 21:06:04 -0800

    new problems

---
+++ src/hatecode/_1000_1999/_1207UniqueNumberOfOccurrences.java
@@ -0,0 +1,32 @@
+package hatecode._1000_1999;
+
+import java.util.*;
+public class _1207UniqueNumberOfOccurrences {
+/*
+1207. Unique Number of Occurrences
+Given an array of integers arr, write a function that returns true if and 
+only if the number of occurrences of each value in the array is unique.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+*/
+    //thinking process: O(n)/O(1)
+    
+    //the problem is to say, given array A, return each element's occurrence is unique.
+    
+    //we use set to record each element's occurence, and use map to record them, their 
+    //length should be same
+    public boolean uniqueOccurrences(int[] A) {
+        if (A == null || A.length < 1) return true;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        Arrays.stream(A).forEach(e->map.put(e, map.getOrDefault(e, 0) + 1));
+        Set<Integer> set = new HashSet<>(map.values());
+        
+        return map.size() == set.size();
+    }
+}
