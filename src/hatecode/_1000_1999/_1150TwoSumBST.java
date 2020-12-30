c8210216e86c4b059700028448766b464a8e9dc2
qiangddu@gmail.com <qiangddu@gmail.com>
2020-12-29 23:53:11 -0800

    new problems

---
+++ src/hatecode/_1000_1999/_1150TwoSumBST.java
@@ -0,0 +1,74 @@
+package hatecode._1000_1999;
+
+import hatecode._0001_0999.TreeNode;
+import java.util.*;
+public class _1150TwoSumBST {
+    /*
+     * 1150. Two Sum BST
+    Given two binary search trees, return True if and only if there is a node 
+    in the first tree and a node in the second tree whose values sum up 
+    to a given integer target.
+    
+    Input: root1 = [2,1,4], root2 = [1,0,3], target = 5
+    Output: true
+    Explanation: 2 and 3 sum up to 5.
+       2              1
+      /  \           / \
+    1     4         0   3
+   
+   Constraints:
+Each tree has at most 5000 nodes.
+-10^9 <= target, node.val <= 10^9
+*/
+    
+    //thinking process: O(n+m)/O(lg(max(h1, h2))
+    
+    //
+    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+        // 如果root2为空，返回false
+        if(root2==null) return false;
+        // 利用root2的当前节点去root1中找到和为target的值。
+        // 若没找到，继续用root2的左右子节点去找
+        return help(root1, root2.val, target)
+            || twoSumBSTs(root1, root2.left, target)
+            || twoSumBSTs(root1, root2.right, target);
+    }
+    boolean help(TreeNode root1, int value, int target){
+        // 如果root1为空返回false
+        if(root1==null) return false;
+        // 计算两棵树当前节点和
+        int sum=root1.val+value;
+        // 如果sum等于target，返回true
+        if(sum==target) return true;
+        // 如果sum大于target，减小root1的值
+        if(sum>target){
+            // 使用root1左子节点的值继续递归
+            return help(root1.left,value,target);
+        }else{ // 反之
+            // 使用root1右子节点的值继续递归
+            return help(root1.right,value,target);
+        }
+    }
+    
+    public boolean twoSumBSTs_BruteForce(TreeNode root1, TreeNode root2, int target) {
+        if (root1 == null || root2 == null) return false;
+        Set<Integer> set1 = new HashSet<>(), set2 = new HashSet<>();
+        helper(root1, set1);
+        helper(root2, set2);
+        
+        for(int ele: set1) {
+            if (set2.contains(target - ele)) return true;
+        }
+        return false;
+    }
+    
+    private void helper(TreeNode root, Set<Integer> set) {
+        if (root == null) return;
+        set.add(root.val);
+        helper(root.left, set);
+        helper(root.right, set);
+    }
+    
+    
+}
+
