a2aaa0bb5c983d58e92dd606d876a294beb85f9c
qiangddu@gmail.com <qiangddu@gmail.com>
2021-01-02 03:28:11 -0800

    new problems

--- src/hatecode/_1000_1999/_1222_QueensThatCanAttackTheKing.java
+++ src/hatecode/_1000_1999/_1222_QueensThatCanAttackTheKing.java
@@ -6,7 +6,10 @@ public class _1222_QueensThatCanAttackTheKing {
 1222. Queens That Can Attack the King
 On an 8x8 chessboard, there can be multiple Black Queens and one White King.
 
-Given an array of integer coordinates queens that represents the positions of the Black Queens, and a pair of coordinates king that represent the position of the White King, return the coordinates of all the queens (in any order) that can attack the King.
+Given an array of integer coordinates queens that represents the positions of 
+the Black Queens, and a pair of coordinates king that represent the position 
+of the White King, return the coordinates of all the queens (in any order) 
+that can attack the King.
 
 Input: queens = [[0,1],[1,0],[4,0],[0,4],[3,3],[2,4]], king = [0,0]
 Output: [[0,1],[1,0],[3,3]]
@@ -14,7 +17,10 @@ Output: [[0,1],[1,0],[3,3]]
     
     //thinking process:
     
-    //
+    //the problem is to say: given a 8x8 board, we have multiple queens and one King,
+    //queen can attack King if they are same row, column or diag or anti-diag.
+    
+    //better way is we look for queen from King，
     public static String COLON = ":";
     public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
         List<List<Integer>> rslt = new ArrayList<>();
@@ -23,6 +29,8 @@ Output: [[0,1],[1,0],[3,3]]
         for(int[] q:queens){
             qset.add(q[0] + COLON + q[1]);
         }
+        
+        // - 1, 0, 1 means 3x3 cells, suppose King is in the middle
         for(int i = -1; i <= 1; i++){
             for(int j = -1; j <= 1; j++){
                 for(int k = 1; k <= 8; k++){
---
+++ src/hatecode/_1000_1999/_1223_DiceRollSimulation.java
@@ -0,0 +1,50 @@
+package hatecode._1000_1999;
+public class _1223_DiceRollSimulation {
+/*
+1223. Dice Roll Simulation
+A die simulator generates a random number from 1 to 6 for each roll. You introduced a constraint to the generator such that it cannot roll the number i more than rollMax[i] (1-indexed) consecutive times. 
+
+Given an array of integers rollMax and an integer n, return the number of distinct sequences that can be obtained with exact n rolls.
+
+Two sequences are considered different if at least one element differs from each other. Since the answer may be too large, return it modulo 10^9 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 2, rollMax = [1,1,2,2,2,3]
+Output: 34
+Explanation: There will be 2 rolls of die, if there are no constraints on the 
+die, there are 6 * 6 = 36 possible combinations. In this case, 
+looking at rollMax array, the numbers 1 and 2 appear at most once consecutively, 
+therefore sequences (1,1) and (2,2) cannot occur, so the final answer is 36-2 = 34.
+*/
+    
+    //thinking process: 
+    
+    public int dieSimulator(int n, int[] rollMax) {
+        long divisor = (long)Math.pow(10, 9) + 7;
+        long[][] dp = new long[n][7];
+        for(int i = 0; i < 6; i++) {
+            dp[0][i] = 1;
+        }
+        dp[0][6] = 6;
+        for(int i = 1; i < n; i++) {
+            long sum = 0;
+            for(int j = 0; j < 6; j++) {
+                dp[i][j] = dp[i - 1][6];
+                if(i - rollMax[j] < 0) {
+                    sum = (sum + dp[i][j]) % divisor;
+                }
+                else {
+                    if(i - rollMax[j] - 1 >= 0) dp[i][j] = (dp[i][j] - (dp[i - rollMax[j] - 1][6] - dp[i - rollMax[j] - 1][j])) % divisor + divisor;
+                    else dp[i][j] = (dp[i][j] - 1) % divisor;
+                    sum = (sum + dp[i][j]) % divisor;
+                }
+
+            }
+            dp[i][6] = sum;
+        }
+        return (int)(dp[n - 1][6]);
+    }
+}
