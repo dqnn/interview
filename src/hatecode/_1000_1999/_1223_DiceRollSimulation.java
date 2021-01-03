d54af623e732c8092e5f68b6d67e63877ca935c7
qiangddu@gmail.com <qiangddu@gmail.com>
2021-01-03 02:57:15 -0800

    more comments

--- src/hatecode/_1000_1999/_1223_DiceRollSimulation.java
+++ src/hatecode/_1000_1999/_1223_DiceRollSimulation.java
@@ -2,13 +2,15 @@ package hatecode._1000_1999;
 public class _1223_DiceRollSimulation {
 /*
 1223. Dice Roll Simulation
-A die simulator generates a random number from 1 to 6 for each roll. You introduced a constraint to the generator such that it cannot roll the number i more than rollMax[i] (1-indexed) consecutive times. 
+A dice simulator generates a random number from 1 to 6 for each roll. 
+You introduced a constraint to the generator such that it cannot roll the number
+ i more than rollMax[i] (1-indexed) consecutive times. 
 
-Given an array of integers rollMax and an integer n, return the number of distinct sequences that can be obtained with exact n rolls.
+Given an array of integers rollMax and an integer n, 
+return the number of distinct sequences that can be obtained with exact n rolls.
 
-Two sequences are considered different if at least one element differs from each other. Since the answer may be too large, return it modulo 10^9 + 7.
-
- 
+Two sequences are considered different if at least one element differs from each other. 
+Since the answer may be too large, return it modulo 10^9 + 7.
 
 Example 1:
 
@@ -21,7 +23,13 @@ therefore sequences (1,1) and (2,2) cannot occur, so the final answer is 36-2 = 
 */
     
     //thinking process: 
+    //the problem is to say: given one array rollMax and one integer n, rollMax means 
+    //for each number i, it cannot give rollMax[i] times consecutively. n means how many times 
+    //you can play(roll) the die. n = 2, rollMax = [1,1,2,2,2,3], means you can play 2 times, 
+    //but 1 and 2 can not occur 2 times consecutively, so 6 x 6 =36, but (1,1) and (2, 2)
+    //need to be excluded.
     
+    //
     public int dieSimulator(int n, int[] rollMax) {
         long divisor = (long)Math.pow(10, 9) + 7;
         long[][] dp = new long[n][7];
