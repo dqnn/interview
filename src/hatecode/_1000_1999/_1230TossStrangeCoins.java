e0af67eb6195aa6f90af8f7df9f2efe8a27e387f
qiangddu@gmail.com <qiangddu@gmail.com>
2021-01-18 02:25:43 -0800

    new problems

---
+++ src/hatecode/_1000_1999/_1230TossStrangeCoins.java
@@ -0,0 +1,30 @@
+package hatecode._1000_1999;
+
+import java.util.*;
+
+public class _1230TossStrangeCoins {
+/*
+ * 1230. Toss Strange Coins
+    You have some coins.  The i-th coin has a probability 
+    prob[i] of facing heads when tossed.
+    
+    Return the probability that the number of coins facing 
+    heads equals target if you toss every coin exactly once.
+    
+    Example 1:
+    
+    Input: prob = [0.4], target = 1
+    Output: 0.40000
+    
+    Input: prob = [0.5,0.5,0.5,0.5,0.5], target = 0
+    Output: 0.03125
+  */
+    //thinking process: 
+    //
+    double probabilityOfHeads(List<Double> prob, int target) { 
+        
+        return 0.0;
+    }
+           
+}
+
