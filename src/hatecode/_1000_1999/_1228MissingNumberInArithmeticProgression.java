b5eb0ca4c99df7974c23df77d97cdf97fe4573cf
qiangddu@gmail.com <qiangddu@gmail.com>
2021-01-18 01:42:50 -0800

    more comments

---
+++ src/hatecode/_1000_1999/_1228MissingNumberInArithmeticProgression.java
@@ -0,0 +1,19 @@
+package hatecode._1000_1999;
+
+public class _1228MissingNumberInArithmeticProgression {
+/*
+ * 1228 - Missing Number In Arithmetic Progression
+ * Given an array that represents elements of arithmetic progression in order. 
+ * One element is missing in the progression, 
+ * find the missing number.
+ * 
+ * examples：
+ * Input: arr[]  = {2, 4, 8, 10, 12, 14}
+        Output: 6
+        
+  Input: arr[]  = {1, 6, 11, 16, 21, 31};
+        Output: 26
+ */
+   
+}
+
