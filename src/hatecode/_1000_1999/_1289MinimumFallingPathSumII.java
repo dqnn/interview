0c626b0cca356d1b60979a307520b8bb01f99983
qiangddu@gmail.com <qiangddu@gmail.com>
2021-09-02 22:37:01 -0700

    add new

---
+++ src/hatecode/_1000_1999/_1289MinimumFallingPathSumII.java
@@ -0,0 +1,21 @@
+package hatecode._1000_1999;
+public class _1289MinimumFallingPathSumII {
+    /*
+    1289. Minimum Falling Path Sum II
+    Given an n x n integer matrix grid, return the minimum sum of a falling path with non-zero shifts.
+
+A falling path with non-zero shifts is a choice of exactly one element from each row of grid such that no two elements chosen in adjacent rows are in the same column.
+
+Input: arr = [[1,2,3],[4,5,6],[7,8,9]]
+Output: 13
+Explanation: 
+The possible falling paths are:
+[1,5,9], [1,5,7], [1,6,7], [1,6,8],
+[2,4,8], [2,4,9], [2,6,7], [2,6,8],
+[3,4,8], [3,4,9], [3,5,7], [3,5,9]
+The falling path with the smallest sum is [1,5,7], so the answer is 13.
+    */
+    public int minFallingPathSum(int[][] grid) {
+        
+    }
+}
