76f69c9332543ba9ecaafe9c088142179a3a951a
qiangddu@gmail.com <qiangddu@gmail.com>
2021-09-26 20:15:19 -0700

    more

--- src/hatecode/_1000_1999/_1155NumberOfDiceRollsWithTargetSum.java
+++ src/hatecode/_1000_1999/_1155NumberOfDiceRollsWithTargetSum.java
@@ -49,9 +49,11 @@ Output: 1
     public int numRollsToTarget_DP2(int d, int f, int target) {
         int MOD = (int)Math.pow(10, 9) + 7;
         long[][] dp = new long[d + 1][target + 1];
+        
+        //TODO: why only initialize here can get correct results;
         dp[0][0] = 1;
         for (int i = 1; i <= d; i++) {
-            for (int j = 0; j <= target; j++) {
+            for (int j = 1; j <= target; j++) {
                 for (int k = 1; k <= f; k++) {
                     if (j >= k) {
                         dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
