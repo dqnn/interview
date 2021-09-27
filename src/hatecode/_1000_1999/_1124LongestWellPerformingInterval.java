7f393b4de91f8196483d69202b9c0dbb1c8ef05a
qiangddu@gmail.com <qiangddu@gmail.com>
2021-09-26 20:25:51 -0700

    more

--- src/hatecode/_1000_1999/_1124LongestWellPerformingInterval.java
+++ src/hatecode/_1000_1999/_1124LongestWellPerformingInterval.java
@@ -10,9 +10,11 @@ public class _1124LongestWellPerformingInterval {
 962. Maximum Width Ramp
 We are given hours, a list of the number of hours worked per day for a given employee.
 
-A day is considered to be a tiring day if and only if the number of hours worked is (strictly) greater than 8.
+A day is considered to be a tiring day if and only if the number of hours 
+worked is (strictly) greater than 8.
 
-A well-performing interval is an interval of days for which the number of tiring days is strictly larger than the number of non-tiring days.
+A well-performing interval is an interval of days for which the number of 
+tiring days is strictly larger than the number of non-tiring days.
 
 Return the length of the longest well-performing interval.
 
