bbcdf14523e1370dc6678147be3b5f0131427d20
qiangddu@gmail.com <qiangddu@gmail.com>
2021-10-09 16:03:11 -0700

    more comments

--- src/hatecode/_1000_1999/_1239MaximumLengthOfAConcatenatedStringWithUniqueCharacters.java
+++ src/hatecode/_1000_1999/_1239MaximumLengthOfAConcatenatedStringWithUniqueCharacters.java
@@ -110,5 +110,26 @@ Maximum length is 4.
         }
         return res;
     }
+    
+    //most optimal solution, but not recommend in interview, just for self practice
+    public int maxLength3(List<String> A) {
+        List<Integer> dp = new ArrayList<>();
+        dp.add(0);
+        int res = 0;
+        for (String s : A) {
+            int a = 0, dup = 0;
+            for (char c : s.toCharArray()) {
+                dup |= a & (1 << (c - 'a'));
+                a |= 1 << (c - 'a');
+            }
+            if (dup > 0) continue;
+            for (int i = dp.size() - 1; i >= 0; --i) {
+                if ((dp.get(i) & a) > 0) continue;
+                dp.add(dp.get(i) | a);
+                res = Math.max(res, Integer.bitCount(dp.get(i) | a));
+            }
+        }
+        return res;
+    }
 
 }
