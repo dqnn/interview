2501f546ca10116160804ab8dbd1582f7939baf9
qiangddu@gmail.com <qiangddu@gmail.com>
2021-11-11 21:38:50 -0800

    more comments

--- src/hatecode/_1000_1999/_1353MaximumNumberOfEventsThatCanBeAttended.java
+++ src/hatecode/_1000_1999/_1353MaximumNumberOfEventsThatCanBeAttended.java
@@ -16,17 +16,20 @@ Return the maximum number of events you can attend.
 
 Example 1:
 */
-    public int maxEvents(int[][] events) {
-        Arrays.sort(events, (a,b)->a[0]==b[0]? a[1]-b[1] : a[0]-b[0]);
+    //thinking process: O(nlgn + max(A) - min(A))
+    
+    //
+    public int maxEvents(int[][] A) {
+        Arrays.sort(A, (a,b)->a[0]==b[0]? a[1]-b[1] : a[0]-b[0]);
         PriorityQueue<Integer> q = new PriorityQueue<>();//hold attandable events at each time t; 
         //meaning the ones that haven't ended yet
         int maxT = Integer.MIN_VALUE;
-        for (int[] event : events) maxT = Math.max(maxT, event[1]);
-        int minT = events[0][0];
+        for (int[] event : A) maxT = Math.max(maxT, event[1]);
+        int minT = A[0][0];
         int eventId=0, res =0;
         for(int t=minT; t<=maxT; t++){
-            while(eventId<events.length && events[eventId][0]<=t)//at each time, we add the new events that are now attendable
-                q.add(events[eventId++][1]);
+            while(eventId<A.length && A[eventId][0]<=t)//at each time, we add the new events that are now attendable
+                q.add(A[eventId++][1]);
             while(!q.isEmpty() && q.peek()<t) q.poll();//we remove the ones that are not attendable anymore
             if(!q.isEmpty()){
                 q.poll();
