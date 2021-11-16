fd80b6078233fd7f0cd6bc9ce0f90f2c0047957f
qiangddu@gmail.com <qiangddu@gmail.com>
2021-11-16 13:50:39 -0800

    more comments

---
+++ src/hatecode/_1000_1999/_1362ClosestDivisors.java
@@ -0,0 +1,33 @@
+package hatecode._1000_1999;
+public class _1362ClosestDivisors {
+/*
+1362. Closest Divisors
+Given an integer num, find the closest two integers in absolute difference whose product equals num + 1 or num + 2.
+
+Return the two integers in any order.
+
+ 
+
+Example 1:
+
+Input: num = 8
+Output: [3,3]
+*/
+    
+    //thinking process: O(sqrt(A)/O(1)); math problem
+    
+    //the problem is to say: given one 
+    public int[] closestDivisors(int A) {
+        int sqrt = (int)Math.sqrt(A+2);
+        for(int i = sqrt; i > 0; i--) {
+            if( (A+1) % i == 0) {
+                return new int[]{i, (A+1)/i};
+            }
+            if ((A+2)%i == 0) {
+                return new int[]{i, (A+2)/i};
+            }
+        }
+        
+        return new int[]{};
+    }
+}
