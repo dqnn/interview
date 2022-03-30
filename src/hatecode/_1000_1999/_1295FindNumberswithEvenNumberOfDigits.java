a16b0a2ed7ff3a671c69b6b7d9fa82fde2b06248
qiangddu@gmail.com <qiangddu@gmail.com>
2022-03-29 23:59:45 -0700

    more comments

---
+++ src/hatecode/_1000_1999/_1295FindNumberswithEvenNumberOfDigits.java
@@ -0,0 +1,33 @@
+package hatecode._1000_1999;
+public class _1295FindNumberswithEvenNumberOfDigits {
+/*
+1295. Find Numbers with Even Number of Digits
+    Given an array nums of integers, return how many 
+    of them contain an even number of digits.
+
+ 
+
+Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+*/
+    //thinking process: 
+    //given an integer array, return how many contain an even number of 
+    //digits
+    public int findNumbers(int[] A) {
+        
+        int count=0;
+        for(int i =0 ; i< A.length; i++){
+            
+            if((A[i]>9 && A[i]<100) 
+               || (A[i]>999 && A[i]<10000) 
+               || A[i]==100000){
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
