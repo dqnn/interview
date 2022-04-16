5e15d56c6f06ef46cc04e4011ee40263fb4572eb
qiangddu@gmail.com <qiangddu@gmail.com>
2022-04-16 11:54:23 -0700

    new problems

---
+++ src/hatecode/_1000_1999/_1249MinimumRemoveToMakeValidParentheses.java
@@ -0,0 +1,51 @@
+package hatecode._1000_1999;
+
+public class _1249MinimumRemoveToMakeValidParentheses {
+    
+/*
+1249. Minimum Remove to Make Valid Parentheses
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+*/
+    public String minRemoveToMakeValid(String s) {
+        int count = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i <s.length(); i++) {
+            char c = s.charAt(i);
+           
+            if (c == '(') {
+                count++;
+                stack.push(i);
+            } else if (c==')') {
+                if (count>0) {
+                    stack.pop();
+                     count--;
+                } else stack.push(i);
+            }
+            //System.out.println(stack);
+        }
+        
+        
+        Set<Integer> set = new HashSet<>(stack);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i <s.length(); i++) {
+            if (set.contains(i)) continue;
+            sb.append(s.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
