2f00e84b2a916d16cc85884057bb838453cdd5b9
qiangddu@gmail.com <qiangddu@gmail.com>
2022-05-01 21:52:49 -0700

    more

--- src/hatecode/_1000_1999/_1291SequentialDigits.java
+++ src/hatecode/_1000_1999/_1291SequentialDigits.java
@@ -8,7 +8,6 @@ An integer has sequential digits if and only if each digit in the number is one 
 
 Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
 
- 
 
 Example 1:
 
