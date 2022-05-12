21ec1305ba22e26a8e460f32261f62dcfac03ec1
qiangddu@gmail.com <qiangddu@gmail.com>
2022-05-11 18:09:23 -0700

    more comments

--- src/hatecode/_0001_0999/_010RegularExpressionMatching.java
+++ src/hatecode/_0001_0999/_010RegularExpressionMatching.java
@@ -4,7 +4,6 @@ package hatecode._0001_0999;
  * Project Name : Leetcode
  * Package Name : leetcode
  * File Name : RegularExpressionMatching
- * Creator : duqiang
  * Date : Sep, 2018
  * Description : 10. Regular Expression Matching
  */
