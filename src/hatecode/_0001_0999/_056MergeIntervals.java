f8d063be6cc5cc4d489942156e1d755a6a90c969
qiangddu@gmail.com <qiangddu@gmail.com>
2022-05-15 23:33:49 -0700

    more ocmments

--- src/hatecode/_0001_0999/_056MergeIntervals.java
+++ src/hatecode/_0001_0999/_056MergeIntervals.java
@@ -20,7 +20,8 @@ Example 1:
 
 Input: [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Explanation: Since intervals [1,3] and [2,6] overlaps, 
+merge them into [1,6].
 Example 2:
 
 Input: [[1,4],[4,5]]
