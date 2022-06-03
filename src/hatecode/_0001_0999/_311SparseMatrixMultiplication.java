0d6474b92897243c24e93a2f05f7f61b0193b9f9
qiangddu@gmail.com <qiangddu@gmail.com>
2022-06-03 12:18:44 -0700

    morecomments

--- src/hatecode/_0001_0999/_311SparseMatrixMultiplication.java
+++ src/hatecode/_0001_0999/_311SparseMatrixMultiplication.java
@@ -1,5 +1,7 @@
 package hatecode._0001_0999;
 
+import java.util.*;
+
 /**
  * Project Name : Leetcode
  * Package Name : leetcode
@@ -41,7 +43,7 @@ row number.
     // this is matrix calculations, so just know how to use
     // 3 loops to calculate the sum
     //one small trick is res[i][j] +=...
-    public int[][] multiply(int[][] A, int[][] B) {
+    public int[][] multiply_BF(int[][] A, int[][] B) {
         int m = A.length, n = A[0].length;
         int nB = B[0].length;
         int[][] res = new int[m][nB];
@@ -56,6 +58,51 @@ row number.
                     }
                 }
             }
+        }
+        return res;
+    }
+    
+    //interview friendly::
+    
+    public int[][] multiply(int[][] m1, int[][] m2) {
+        Map<Integer, Map<Integer, Integer>> map1 = new HashMap<>();
+        
+        for(int i = 0; i<m1.length; i++) {
+            for(int j = 0; j< m1[0].length; j++) {
+                if (m1[i][j] != 0) {
+                    map1.computeIfAbsent(i, v->new HashMap<>()).put(j, m1[i][j]);
+                }
+            }
+        }
+        
+        Map<Integer, Map<Integer, Integer>> map2 = new HashMap<>();
+        
+        for(int j = 0; j<m2[0].length; j++) {
+            for(int i = 0; i< m2.length; i++) {
+                if (m2[i][j] != 0) {
+                    map2.computeIfAbsent(j, v->new HashMap<>()).put(i, m2[i][j]);
+                }
+            }
+        }
+        
+        int[][] res = new int[m1.length][m2[0].length];
+        for(int i = 0; i<m1.length; i++) {
+            for(int j =0; j<m2[0].length; j++) {
+                res[i][j] = ops(map1.get(i), map2.get(j));
+            }
+        }
+        
+        return res;
+    }
+    
+    
+    private int ops(Map<Integer, Integer> m1, Map<Integer, Integer> m2) {
+        if (m1 == null || m2 == null) return 0;
+        int res = 0;
+        for(var entry: m1.entrySet()) {
+            int k = entry.getKey(), v = entry.getValue();
+            if (!m2.containsKey(k)) continue;
+            res += v * m2.get(k);
         }
         return res;
     }
