687b34aa1d23cd4e26122c3601531943e2162044
qiangddu@gmail.com <qiangddu@gmail.com>
2022-06-28 09:27:42 -0700

    more comments

--- src/hatecode/_0001_0999/_958CheckCompletenessOfABinaryTree.java
+++ src/hatecode/_0001_0999/_958CheckCompletenessOfABinaryTree.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class _958CheckCompletenessOfABinaryTree {
 /*
 958. Check Completeness of a Binary Tree
-Given a binary tree, determine if it is a complete binary tree.
+Given a binary tree, determine if it is a 
+complete binary tree.
 
 In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
 Input: [1,2,3,4,5,null,7]
