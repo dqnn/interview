a77c363a7ad7c7eac38ec68ec8e92b9da3db7067
qiangddu@gmail.com <qiangddu@gmail.com>
2022-06-28 09:19:51 -0700

    more comments

---
+++ src/hatecode/_0001_0999/_498DiagonalTraverse.java
@@ -0,0 +1,63 @@
+package hatecode._0001_0999;
+public class _498DiagonalTraverse {
+    
+    
+    /*
+    498. Diagonal Traverse
+Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,4,7,5,3,6,8,9]
+      1. 2
+      3. 4
+      
+      
+    */
+    
+    //thinking process: O(mn)/O(1)
+    
+    //diag line has some laws like even or odd number, here we leverage this
+    //to decide which direction, up right or left down. 
+    
+    //note that in if else we process edge case
+    public int[] findDiagonalOrder(int[][] m) {
+        if (m == null || m.length < 1 || m[0].length < 1) return new int[0];
+        
+        
+        int i = 0, j= 0;
+        int r = m.length, c=m[0].length;
+        int[] res = new int[r * c];
+        
+        for(int idx = 0; idx< r * c; idx++) {
+            res[idx] = m[i][j];
+            //up right
+            if ( (i + j) % 2 == 0) {
+                if (j == c - 1) {
+                    i++;
+                } else if (i == 0) {
+                    j++;
+                } else {
+                    i--;
+                    j++;
+                }
+            //left down
+            } else {
+                if (i == r - 1) {
+                    j++;
+                } else if (j == 0) {
+                    i++;
+                } else {
+                    i++;
+                    j--;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
