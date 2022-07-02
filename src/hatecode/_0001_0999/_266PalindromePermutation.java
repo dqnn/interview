4dcd755628e326651bc277f2c330ffe4fbfcbdf6
qiangddu@gmail.com <qiangddu@gmail.com>
2022-07-02 11:11:54 -0700

    more ocmments

--- src/hatecode/_0001_0999/_266PalindromePermutation.java
+++ src/hatecode/_0001_0999/_266PalindromePermutation.java
@@ -12,7 +12,8 @@ import java.util.HashSet;
 public class _266PalindromePermutation {
     /**
      * 266. Palindrome Permutation
-     * Given a string, determine if a permutation of the string could form a palindrome.
+     * Given a string, determine if a permutation of the 
+     * string could form a palindrome.
 
      For example,
      "code" -> False, "aab" -> True, "carerac" -> True.
