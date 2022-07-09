210bd2f09bcdf821274ec8e6f350768247b41336
qiangddu@gmail.com <qiangddu@gmail.com>
2022-07-08 23:03:38 -0700

    morecomments

--- src/hatecode/_0001_0999/_489RobotRoomCleaner.java
+++ src/hatecode/_0001_0999/_489RobotRoomCleaner.java
@@ -4,18 +4,19 @@ import java.util.*;
 
 public class _489RobotRoomCleaner {
     /*
-     * 489. Robot Room Cleaner Given a robot cleaner in a room modeled as a grid.
+     * 489. Robot Room Cleaner Given a robot cleaner 
+     * in a room modeled as a grid.
      * 
      * Each cell in the grid can be empty or blocked.
      * 
-     * The robot cleaner with 4 given APIs can move forward, turn left or turn
-     * right. Each turn it made is 90 degrees.
+     * The robot cleaner with 4 given APIs can move forward, 
+     * turn left or turn right. Each turn it made is 90 degrees.
      * 
-     * When it tries to move into a blocked cell, its bumper sensor detects the
-     * obstacle and it stays on the current cell.
+     * When it tries to move into a blocked cell, its bumper 
+     * sensor detects the obstacle and it stays on the current cell.
      * 
-     * Design an algorithm to clean the entire room using only the 4 given APIs
-     * shown below.
+     * Design an algorithm to clean the entire room using 
+     * only the 4 given APIs shown below.
      */
     interface Robot {
         public boolean move();
