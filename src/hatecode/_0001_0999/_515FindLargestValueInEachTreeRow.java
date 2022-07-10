177d6666e4f90946266f7ba7b6aa7837543fe22c
qiangddu@gmail.com <qiangddu@gmail.com>
2022-07-09 18:18:31 -0700

    more comments

--- src/hatecode/_0001_0999/_515FindLargestValueInEachTreeRow.java
+++ src/hatecode/_0001_0999/_515FindLargestValueInEachTreeRow.java
@@ -37,4 +37,27 @@ Output: [1, 3, 9]
         }
         return res;
     }
+    
+    //DFS version, it is tricky to use an arraylist to store the 
+    //level d max value
+        public List<Integer> largestValues_DFS(TreeNode root) {
+            List<Integer> res = new ArrayList<Integer>();
+            helper(root, res, 0);
+            return res;
+        }
+        private void helper(TreeNode root, List<Integer> res, int d){
+            if(root == null){
+                return;
+            }
+           //expand list size
+            if(d == res.size()){
+                res.add(root.val);
+            }
+            else{
+            //or set value
+                res.set(d, Math.max(res.get(d), root.val));
+            }
+            helper(root.left, res, d+1);
+            helper(root.right, res, d+1);
+        }
 }
