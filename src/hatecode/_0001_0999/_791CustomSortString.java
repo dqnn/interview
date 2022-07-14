08c24adaa140eedb536461c2e2dd371f17f91d54
qiangddu@gmail.com <qiangddu@gmail.com>
2022-07-14 14:53:27 -0700

    more comments

--- src/hatecode/_0001_0999/_791CustomSortString.java
+++ src/hatecode/_0001_0999/_791CustomSortString.java
@@ -3,13 +3,18 @@ import java.util.*;
 public class _791CustomSortString {
 /*
 791. Custom Sort String
-S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+S and T are strings composed of lowercase letters. 
+In S, no letter occurs more than once.
 
-S was sorted in some custom order previously. We want to permute the characters of 
-T so that they match the order that S was sorted. More specifically, if x occurs 
-before y in S, then x should occur before y in the returned string.
+S was sorted in some custom order previously. 
+We want to permute the characters of 
+T so that they match the order that S was sorted. 
+More specifically, if x occurs 
+before y in S, then x should occur before y in the 
+returned string.
 
-Return any permutation of T (as a string) that satisfies this property.
+Return any permutation of T (as a string) that satisfies 
+this property.
 
 Example :
 Input: 
