cbed8f3b5542db96ce8d6995d604ff3376c622a7
qiangddu@gmail.com <qiangddu@gmail.com>
2022-07-17 10:45:14 -0700

    more comments

--- src/hatecode/_0001_0999/_410SplitArrayLargestSum.java
+++ src/hatecode/_0001_0999/_410SplitArrayLargestSum.java
@@ -17,6 +17,14 @@ Input: nums = [7,2,5,10,8], m = 2
 Output: 18
 */
     //thinking process: O(nlgn)/O(1)
+    
+    /*
+     * the problem is to say: give one integer array A, and integer k,
+     * you can cut the array to k subarray, 
+     * return the min of max sum of subarrays,
+     * A = [7,2,5,10,8], m = 2, you can cut A to 2 subarrays, 
+     * sum = 32, so it must be bigger >= 16, 
+     */
     public int splitArray(int[] A, int k) {
        int l = 0, r = 0;
        for(int a : A) {
