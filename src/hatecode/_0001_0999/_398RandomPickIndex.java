faf0dd84e4c55b481e97eadd45f13bc3cfb07e28
qiangddu@gmail.com <qiangddu@gmail.com>
2022-07-30 22:45:04 -0700

    more comments

--- src/hatecode/_0001_0999/_398RandomPickIndex.java
+++ src/hatecode/_0001_0999/_398RandomPickIndex.java
@@ -80,8 +80,19 @@ to get the random number 0 among {0,1,2,3} in the loop where i = 4, so the proba
             // exclusive.
             
             // this is the key, this ways:
-            // suppose there are 3 5 in the array, and when we meet first 5, the probobality is 100% 
-            // but that's ok because later we are able to override, next 5, we have 50%, third we have 33%, 
+            /*
+             * The first time we saw target 3 is at index 2. The count is 0. 
+             * Our reservoir only have 0 and we need to pick rnd.nextInt(++count) == 0. 
+             * The probability is 1. Result = 2.
+
+            Then we went to index 3. The count is 1. Our reservoir is has [0,1].
+             We say if we get 0, we'll change the the result, otherwise we keep it. 
+             Then chance that we keep the result=2 is 1/2 which means we got 1 from the reservoir.
+           Then we went to index 4. count =2. Our reservoir has [0,1,2]. 
+           Same as before, if we get 0, then we'll change the result. 
+           The chance we get 0 is 1/3, while the chance we didn't get is 2/3. 
+           i.e The chance we keep the result ==2 is 2/3.The chance we get index=2 is 1*1/2*2/3=1/3
+             */
             if (rmd.nextInt(++count) == 0) {
                 res = i;
             }
