bf6d32341b9940cba56399e300d8ac8843b2bdb3
qiangddu@gmail.com <qiangddu@gmail.com>
2022-08-17 22:49:02 -0700

    mroe comments

--- src/hatecode/_0001_0999/_325MaximumSizeSubarraySumEqualsk.java
+++ src/hatecode/_0001_0999/_325MaximumSizeSubarraySumEqualsk.java
@@ -67,11 +67,11 @@ Can you do it in O(n) time?
         return res;
     }
     //another short solution
-    public int maxSubArrayLen2(int[] nums, int k) {
+    public int maxSubArrayLen2(int[] A, int k) {
         int sum = 0, max = 0;
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            sum = sum + nums[i];
+        for (int i = 0; i < A.length; i++) {
+            sum = sum + A[i];
             if (sum == k) max = i + 1;
             else if (map.containsKey(sum - k)) max = Math.max(max, i - map.get(sum - k));
             if (!map.containsKey(sum)) map.put(sum, i);
