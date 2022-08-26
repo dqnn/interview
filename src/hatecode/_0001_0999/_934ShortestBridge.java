22a10aaa368c9d64f3bb603b7336e7482aed431c
qiangddu@gmail.com <qiangddu@gmail.com>
2022-08-26 01:15:47 -0700

    more comments

--- src/hatecode/_0001_0999/_934ShortestBridge.java
+++ src/hatecode/_0001_0999/_934ShortestBridge.java
@@ -27,51 +27,63 @@ Output: 1
     /*
      * 
      */
+    int[][] dirs = {{-1,0}, {1,0}, {0,1}, {0,-1}};
     public int shortestBridge(int[][] A) {
         int r = A.length, c = A[0].length;
         boolean[][] visited = new boolean[r][c];
-        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
         Queue<int[]> q = new LinkedList<>();
+        
         boolean found = false;
-        // 1. dfs to find an island, mark it in `visited`
-        for (int i = 0; i < r; i++) {
+        for(int i = 0; i< r; i++) {
             if (found) break;
-            for (int j = 0; j < c; j++) {
+            for(int j = 0; j<c;j++) {
                 if (A[i][j] == 1) {
-                    dfs(A, visited, q, i, j, dirs);
+                    helper(A, i, j, visited, q);
                     found = true;
                     break;
                 }
             }
         }
-        // 2. bfs to expand this island
+        
         int step = 0;
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int size = q.size();
-           for(int k = 0; k< size; k++) {
-                int[] cur = q.poll();
-                for (int[] dir : dirs) {
-                    int i = cur[0] + dir[0];
-                    int j = cur[1] + dir[1];
-                    if (i >= 0 && j >= 0 && i < r && j < c && !visited[i][j]) {
-                        if (A[i][j] == 1) return step;
-                        q.offer(new int[]{i, j});
-                        visited[i][j] = true;
+            
+            
+            while(size-- > 0) {
+                int[] e = q.poll();
+                int x = e[0], y=e[1];
+                if (A[x][y] == 1) return step;
+                for(int[] d: dirs) {
+                    int i = x + d[0];
+                    int j = y + d[1];
+                    if (i >0 && i < r && j >=0 && j < c && !visited[i][j]) {
+                         visited[i][j] = true;
+                         q.offer(new int[]{i, j});
                     }
                 }
             }
             step++;
         }
+        
         return -1;
     }
-    private void dfs(int[][] A, boolean[][] visited, Queue<int[]> q, int i, int j, int[][] dirs) {
-        if (i < 0 || j < 0 || i >= A.length || j >= A[0].length || visited[i][j] || A[i][j] == 0) {
-            return;
-        }
-        visited[i][j] = true;
-        q.offer(new int[]{i, j});
-        for (int[] dir : dirs) {
-            dfs(A, visited, q, i + dir[0], j + dir[1], dirs);
+    
+    //we only add these most out round of 0 into queue, not all of them
+    private void helper(int[][] A, int i, int j, boolean[][] visited, Queue<int[]> q) {
+        int r = A.length, c = A[0].length;
+        if (i >=0 && i < r && j >=0 && j < c && !visited[i][j]) {
+            visited[i][j] = true;
+            if (A[i][j] == 1) {
+                for(int[] d: dirs) {
+                    int x = i + d[0];
+                    int y = j + d[1];
+                    helper(A, x, y, visited, q);
+                }
+            } else {
+                q.offer(new int[]{i,j});
+            }
         }
     }
 }
