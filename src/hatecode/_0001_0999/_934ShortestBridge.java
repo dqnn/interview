863231cabb0a6317bcab65e953c9746ef2fb71f6
qiangddu@gmail.com <qiangddu@gmail.com>
2022-08-25 21:43:14 -0700

    more comments

--- src/hatecode/_0001_0999/_934ShortestBridge.java
+++ src/hatecode/_0001_0999/_934ShortestBridge.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class _934ShortestBridge {
 /*
 934. Shortest Bridge
-In a given 2D binary array A, there are two islands.  (An island is a 4-directionally connected group of 1s not connected to any other 1s.)
+In a given 2D binary array A, there are two islands.  
+(An island is a 4-directionally connected group of 1s not 
+connected to any other 1s.)
 
-Now, we may change 0s to 1s so as to connect the two islands together to form 1 island.
+Now, we may change 0s to 1s so as to connect the two islands 
+together to form 1 island.
 
-Return the smallest number of 0s that must be flipped.  (It is guaranteed that the answer is at least 1.)
+Return the smallest number of 0s that must be flipped.  
+(It is guaranteed that the answer is at least 1.)
 
  
 
