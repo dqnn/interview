ed17f648e98d3904a1bf318fac3ca8e0810036a2
qiangddu@gmail.com <qiangddu@gmail.com>
2022-08-29 14:56:44 -0700

    more comments

--- src/hatecode/_0001_0999/_247StrobogrammaticNumberII.java
+++ src/hatecode/_0001_0999/_247StrobogrammaticNumberII.java
@@ -42,17 +42,26 @@ public class _247StrobogrammaticNumberII {
     //here we use different way to solve this problem, we use f(n) = 4 * f(n - 2), 
     //the anonying part is to handle 
     // "0" case, so if it is not the outest loop, then we need to add "0" as valid case, if it is, then we do not need 
+    /*
+     * thinking about n = 0 and n = 1, 
+     * if n = 1, then we can get 0, 1 and 8
+     * if n = 2, suppose we have a virtual position there, we need to add numbers 
+     * on its both sides, left and right, 
+     * so for n =3, it is like add numbers to boths sides of 0, 1, 8
+     * 0->0 0 0,  1 0 1, 6 0 9, 8 0 8, 9 0 6
+     * same to to others
+     */
     public List<String> findStrobogrammatic(int n) {
         return helper(n, n);
     }
     //m is just to make sure whether we are at outtest loop or not. 
-    //if n == m, then yes, if not, no
-    public List<String> helper(int n, int m) {
+    //n means the current string length, m means maxLength
+    public List<String> helper(int n, int maxLength) {
         if (n == 0) return new ArrayList<>(Arrays.asList(""));
         if (n == 1) return new ArrayList<>(Arrays.asList("0", "1", "8"));
 
         // n - 2 because we will add two digits(before and after) for each string
-        List<String> list = helper(n - 2, m);
+        List<String> list = helper(n - 2, maxLength);
         List<String> res = new ArrayList<>();
         //note: list at least has 1 size, "" 
         // 
@@ -61,7 +70,7 @@ public class _247StrobogrammaticNumberII {
             //It is to disallow zeros as first and last digit. 
             //We want numbers of length 'n'.
             //because in next loop, it maybe add 1 outside 0 s 0
-            if (n != m) {
+            if (n != maxLength) {
                 res.add("0" + s + "0");
             }
             res.add("1" + s + "1");
