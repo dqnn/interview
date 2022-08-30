f3a4f16f0363478569553a2b694baca6bf5eb2ce
qiangddu@gmail.com <qiangddu@gmail.com>
2022-08-30 01:57:15 -0700

    more comments

--- src/hatecode/_2000_2999/_2033MinimumOperationsToMakeAUniValueGrid.java
+++ src/hatecode/_2000_2999/_2033MinimumOperationsToMakeAUniValueGrid.java
@@ -31,6 +31,7 @@ A total of 4 operations were used.
      * 
      * [2,4,6,8] x = 2--> 2(1), 4(0), 6(1), 8(2) = 1+0+1+2 = 4 
      * 
+     * 1. how to prove the median element will be the correct answer.
      * 
      * 
      */
