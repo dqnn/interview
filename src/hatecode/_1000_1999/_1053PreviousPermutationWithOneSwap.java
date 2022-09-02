77af72364e6a8d9b2e06adbff6067b84cd8da5b7
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-01 17:36:02 -0700

    more

--- src/hatecode/_1000_1999/_1053PreviousPermutationWithOneSwap.java
+++ src/hatecode/_1000_1999/_1053PreviousPermutationWithOneSwap.java
@@ -2,7 +2,10 @@ package hatecode._1000_1999;
 public class _1053PreviousPermutationWithOneSwap {
 /*
 1053. Previous Permutation With One Swap
-Given an array A of positive integers (not necessarily distinct), return the lexicographically largest permutation that is smaller than A, that can be made with one swap (A swap exchanges the positions of two numbers A[i] and A[j]).  If it cannot be done, then return the same array.
+Given an array A of positive integers (not necessarily distinct), return the 
+lexicographically largest permutation that is smaller than A, that can be made
+ with one swap (A swap exchanges the positions of two numbers A[i] and A[j]). 
+  If it cannot be done, then return the same array.
 
  
 
