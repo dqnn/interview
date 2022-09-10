b10f816b30be85c48b758d2f0bf96ae4c6b84712
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-10 13:47:16 -0700

    more comments

--- src/hatecode/_0001_0999/_452MinimumNumberOfArrowsToBurstBalloons.java
+++ src/hatecode/_0001_0999/_452MinimumNumberOfArrowsToBurstBalloons.java
@@ -3,9 +3,17 @@ import java.util.*;
 public class _452MinimumNumberOfArrowsToBurstBalloons {
 /*
 452. Minimum Number of Arrows to Burst Balloons
-There are a number of spherical balloons spread in two-dimensional space. For each balloon, provided input is the start and end coordinates of the horizontal diameter. Since it's horizontal, y-coordinates don't matter and hence the x-coordinates of start and end of the diameter suffice. Start is always smaller than end. There will be at most 104 balloons.
+There are a number of spherical balloons spread in two-dimensional space. 
+For each balloon, provided input is the start and end coordinates of the 
+horizontal diameter. Since it's horizontal, y-coordinates don't matter and 
+hence the x-coordinates of start and end of the diameter suffice. 
+Start is always smaller than end. There will be at most 104 balloons.
 
-An arrow can be shot up exactly vertically from different points along the x-axis. A balloon with xstart and xend bursts by an arrow shot at x if xstart ≤ x ≤ xend. There is no limit to the number of arrows that can be shot. An arrow once shot keeps travelling up infinitely. The problem is to find the minimum number of arrows that must be shot to burst all balloons.
+An arrow can be shot up exactly vertically from different points along the 
+x-axis. A balloon with xstart and xend bursts by an arrow shot at 
+x if xstart ≤ x ≤ xend. There is no limit to the number of arrows that can be 
+shot. An arrow once shot keeps travelling up infinitely. The problem is to 
+find the minimum number of arrows that must be shot to burst all balloons.
 
 Example:
 
