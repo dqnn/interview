27f8e6496f69e534fe8d14af080e123b8a985dc6
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-11 22:22:53 -0700

    more comments

--- src/hatecode/_0001_0999/_865SmallestSubtreeWithAllTheDeepestNodes.java
+++ src/hatecode/_0001_0999/_865SmallestSubtreeWithAllTheDeepestNodes.java
@@ -3,13 +3,17 @@ public class _865SmallestSubtreeWithAllTheDeepestNodes {
 /*
  * tag: tree, dfs, lowest ancestor
 865. Smallest Subtree with all the Deepest Nodes
-Given a binary tree rooted at root, the depth of each node is the shortest distance to the root.
+Given a binary tree rooted at root, the depth of each node is the 
+shortest distance to the root.
 
-A node is deepest if it has the largest depth possible among any node in the entire tree.
+A node is deepest if it has the largest depth possible among any node 
+in the entire tree.
 
-The subtree of a node is that node, plus the set of all descendants of that node.
+The subtree of a node is that node, plus the set of all descendants of 
+that node.
 
-Return the node with the largest depth such that it contains all the deepest nodes in its subtree.
+Return the node with the largest depth such that it contains all the 
+deepest nodes in its subtree.
 
 
 */
