067eb3c38590982f145e8a4f412e16bf6600dc86
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-12 00:24:16 -0700

    more comments

---
+++ src/hatecode/_0001_0999/_669TrimABinarySearchTree.java
@@ -0,0 +1,38 @@
+package hatecode._0001_0999;
+
+public class _669TrimABinarySearchTree {
+/*
+669. Trim a Binary Search Tree
+Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
+
+Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+Output: [3,2,null,1]
+*/
+    /*
+     * 
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        return helper(root, low, high);
+    }
+    
+    
+    private TreeNode helper(TreeNode root, int l, int r) {
+        if (root == null) return null;
+        
+        if (root.val < l) return helper(root.right, l, r);
+        else if (root.val > r) return helper(root.left, l, r);
+        
+        
+        root.left = helper(root.left, l, r);
+        root.right = helper(root.right, l ,r);
+        
+        return root;
+    }
+}
