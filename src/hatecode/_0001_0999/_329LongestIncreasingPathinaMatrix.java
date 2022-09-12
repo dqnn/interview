37ba79e6674cb835a5644bdc86d1b6847ce68862
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-12 00:35:31 -0700

    more comments

--- src/hatecode/_0001_0999/_329LongestIncreasingPathinaMatrix.java
+++ src/hatecode/_0001_0999/_329LongestIncreasingPathinaMatrix.java
@@ -1,11 +1,6 @@
 package hatecode._0001_0999;
 
 /**
- * Project Name : Leetcode
- * Package Name : leetcode
- * File Name : LongestIncreasingPathinaMatrix
- * Creator : professorX
- * Date : Aug, 2018
  * Description : 329. Longest Increasing Path in a Matrix
  */
 public class _329LongestIncreasingPathinaMatrix {
