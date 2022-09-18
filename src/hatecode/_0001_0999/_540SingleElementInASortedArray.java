f9daa374b092771903223ba7b9ca5d1925a39cf7
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-18 14:59:07 -0700

    more

--- src/hatecode/_0001_0999/_540SingleElementInASortedArray.java
+++ src/hatecode/_0001_0999/_540SingleElementInASortedArray.java
@@ -14,7 +14,8 @@ Input: [3,3,7,7,10,11,11]
 Output: 10
 */
     //thinking process: O(logN)/O(1)
-    //if it is sorted, then BS is on the plate, whole length must be odd number
+    //if it is sorted, then BS is on the plate, 
+    //whole length must be odd number
     //so if mid is even, 
     public int singleNonDuplicate_Standard(int[] A) {
         if (A == null || A.length < 1)
