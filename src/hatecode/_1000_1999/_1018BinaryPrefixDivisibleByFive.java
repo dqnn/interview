8fb5acb13154798676508bd8e035d979e1640e98
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-17 19:37:54 -0700

    more

--- src/hatecode/_1000_1999/_1018BinaryPrefixDivisibleByFive.java
+++ src/hatecode/_1000_1999/_1018BinaryPrefixDivisibleByFive.java
@@ -1,8 +1,8 @@
 package hatecode._1000_1999;
 import java.util.*;
-public class _1029BinaryPrefixDivisibleByFive {
+public class _1018BinaryPrefixDivisibleByFive {
 /*
-1029. Binary Prefix Divisible By 5
+1018. Binary Prefix Divisible By 5
 Given an array A of 0s and 1s, consider N_i: the i-th subarray from A[0] to A[i] interpreted as a binary number (from most-significant-bit to least-significant-bit.)
 
 Return a list of booleans answer, where answer[i] is true if and only if N_i is divisible by 5.
