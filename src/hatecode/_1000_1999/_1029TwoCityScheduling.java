45e6f1983c97cf8bf27b2bf18dcccac145fc046e
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-17 21:00:33 -0700

    more

--- src/hatecode/_1000_1999/_1029TwoCityScheduling.java
+++ src/hatecode/_1000_1999/_1029TwoCityScheduling.java
@@ -18,10 +18,14 @@ Example 1:
 Input: [[10,20],[30,200],[400,50],[30,20]]
 Output: 110
 */
-    //thinking process:
+    //thinking process: O(nlgn)/O(1) 
+    
     //given a 2d array A, each i-th person cost to city a A[i][0], cost to 
     /*
      * city b is A[i][1], return the minimal cost for all persons 
+     * 
+     * Greedy, 
+     * 
      */
     
     //we want to sort by how much saved if we go to b instead of a
