d9b4b2a12aa848d34a1f10829f2b607cf415ba25
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-21 18:32:50 -0700

    more comments

--- src/hatecode/_0001_0999/_443StringCompression.java
+++ src/hatecode/_0001_0999/_443StringCompression.java
@@ -8,11 +8,13 @@ public class _443StringCompression {
     /**
      * Given an array of characters, compress it in-place.
 
-     The length after compression must always be smaller than or equal to the original array.
+     The length after compression must always be smaller than or equal 
+     to the original array.
 
      Every element of the array should be a character (not int) of length 1.
 
-     After you are done modifying the input array in-place, return the new length of the array.
+     After you are done modifying the input array in-place, return the 
+     new length of the array.
 
 
      Follow up:
