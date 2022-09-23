060fb0073c1f3c6ecfc78809413e12a735a7270b
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-22 20:20:39 -0700

    more comments

--- src/hatecode/_0001_0999/_057InsertInterval.java
+++ src/hatecode/_0001_0999/_057InsertInterval.java
@@ -14,7 +14,8 @@ public class _057InsertInterval {
      * 57. Insert Interval
      * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
 
-You may assume that the intervals were initially sorted according to their start times.
+You may assume that the intervals were initially 
+sorted according to their start times.
 
 Example 1:
 
