2c243f4ef4591f742332e969e5cc55abdba46627
qiangddu@gmail.com <qiangddu@gmail.com>
2022-09-23 01:26:35 -0700

    more

--- src/hatecode/_0001_0999/_859BuddyStrings.java
+++ src/hatecode/_0001_0999/_859BuddyStrings.java
@@ -86,7 +86,11 @@ public class _859BuddyStrings {
             return s.size() < A.length();
         }
         List<Integer> dif = new ArrayList<>();
-        for (int i = 0; i < A.length(); ++i) if (A.charAt(i) != B.charAt(i)) dif.add(i);
-        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
+        for (int i = 0; i < A.length(); ++i) 
+            if (A.charAt(i) != B.charAt(i)) 
+                dif.add(i);
+        
+        return dif.size() == 2 && A.charAt(dif.get(0)) == B.charAt(dif.get(1)) 
+                               && A.charAt(dif.get(1)) == B.charAt(dif.get(0));
     }
 }
