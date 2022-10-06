993bf762ac2cc09ef31a24cfd7bf8f441399570c
qiangddu@gmail.com <qiangddu@gmail.com>
2022-10-06 09:30:00 -0700

    more comments

--- src/hatecode/_0001_0999/_442FindAllDuplicatesinArray.java
+++ src/hatecode/_0001_0999/_442FindAllDuplicatesinArray.java
@@ -29,8 +29,21 @@ Output:
  */
 
 /*
+ *  thinking process: O(n)/O(1)
+ *  
+ *  the problem is to say: given one positive array, value ranges [1, n], each 
+ *  element will appear 1 or 2 times, return the elements which appear 2 times
+ *  
  *  use index as indicator to store whether visited before or not
  *  O(n), O(1)
+ *  [4,3,2,7,8,2,3,1]
+ *  -->
+ *  i = 0, [4,3,2,-7,8,2,3,1]
+ *  i = 1, [4,3,-2,-7,8,2,3,1]
+ *  i = 2, [4,-3,-2,-7,8,2,3,1]
+ *  i = 3, [4,-3,-2,-7,8,2,-3,1]
+ *  i = 4, [4,-3,-2,-7,8,2,-3,-1]
+ *  i = 5, [4,-3,-2,-7,8,2,-3,-1]
  */
 public class _442FindAllDuplicatesinArray {
     // so should be sensitive about the index,if it is within context of n, then we should consider 
