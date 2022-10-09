13de15674105401366b7075641d5174c85047f71
qiangddu@gmail.com <qiangddu@gmail.com>
2022-10-08 20:21:58 -0700

    more comments

--- src/hatecode/_1000_1999/_1588SumOfAllOddLengthSubarrays.java
+++ src/hatecode/_1000_1999/_1588SumOfAllOddLengthSubarrays.java
@@ -32,12 +32,21 @@ If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
      * times=  3   
      * 
      * for each A[i], we have two cases:
-     * 1. start with A[i], we have (n-1-i + 1) subarrays
+     * 1. start with A[i], we have (n-1-i + 1) subarrays, means from i->n-1, we have n-1-i+1 numbers 
+     * 
      * 2. not start with A[i],  we have 
      *    left   A[i] * (right)
-     * (i-1-0+1) * (n-1 - i + 1) subarrays
+     * (i-1-0+1) * (n-1 - (i +1) + 1 + 1) subarrays
      * 
-     * left at least 1, right can be 0 length
+     * left at least 1, right can be 0 length, so it means count from i+1 ->n-1, we add 1 last because we have to 
+     * include A[i]
+     * 
+     * add them together will be n - i + i * (n-i) = (n-i)(i+1) subarrays contains A[i],
+     * 
+     * how many are odds length, then will be (K +1)/2, if K is even, then K/2, if K
+     * is odd, then K/2+1, so we just use K + 1 to avoud write if else clause
+     * 
+     * 
      */
     public int sumOddLengthSubarrays(int[] A) {
         int res = 0;
