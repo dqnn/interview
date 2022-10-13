381f556a02f6e91435d6b34d643ef232d49135e4
qiangddu@gmail.com <qiangddu@gmail.com>
2022-10-13 10:45:51 -0700

    more comments

--- src/hatecode/_0001_0999/_698PartitionToKEqualSumSubsets.java
+++ src/hatecode/_0001_0999/_698PartitionToKEqualSumSubsets.java
@@ -3,7 +3,9 @@ import java.util.*;
 public class _698PartitionToKEqualSumSubsets {
 /*
 698. Partition to K Equal Sum Subsets
-Given an array of integers nums and a positive integer k, find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
+Given an array of integers nums and a positive integer k, 
+find whether it's possible to divide this array into k non-empty 
+subsets whose sums are all equal.
 
  
 
