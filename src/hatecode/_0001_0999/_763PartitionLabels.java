a6e9f19096227ea0453ed5f0b68e8e126c4114ac
qiangddu@gmail.com <qiangddu@gmail.com>
2022-10-12 23:59:57 -0700

    more comments

--- src/hatecode/_0001_0999/_763PartitionLabels.java
+++ src/hatecode/_0001_0999/_763PartitionLabels.java
@@ -3,17 +3,20 @@ import java.util.*;
 public class _763PartitionLabels {
 /*
 763. Partition Labels
-A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+A string S of lowercase letters is given. We want to partition this string into 
+as many parts as possible so that each letter appears in at most one part, 
+and return a list of integers representing the size of these parts.
 
 Example 1:
 Input: S = "ababcbacadefegdehijhklij"
 Output: [9,7,8]
 */
-    //so the problem is to say given a string s, we want to parition the string int several 
+    // thinking process: O()
+    //so the problem is to say given a string s, we want to partition the string into several 
     //substring so each substring can have all chars which would not disappear in other partitions
     //and as many as possible
     
-    //so we use sliding window, and we rememebr each char last character, everytime for each char
+    //so we use sliding window, and we remember each char last character, everytime for each char
     //we can get the char last position, but thats not ideal, 
     //so we need to find the char disappear last in the string and as end char. 
     public List<Integer> partitionLabels(String s) {
