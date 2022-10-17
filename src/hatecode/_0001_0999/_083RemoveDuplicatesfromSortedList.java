dbcb65006f6f75a8b4cbc8a9aab6ed1557829a8c
qiangddu@gmail.com <qiangddu@gmail.com>
2022-10-17 15:02:33 -0700

    more comments

--- src/hatecode/_0001_0999/_083RemoveDuplicatesfromSortedList.java
+++ src/hatecode/_0001_0999/_083RemoveDuplicatesfromSortedList.java
@@ -20,7 +20,7 @@ public class _083RemoveDuplicatesfromSortedList {
     public static ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null) return head;
         ListNode cur = head;
-        while (cur.next != null) {
+        while (cur != null && cur.next != null) {
             if (cur.next.val == cur.val) {
                 cur.next = cur.next.next;
             } else {
