8c218c4393af8815085bb0e6040ab77f659cb2bf
qiangddu@gmail.com <qiangddu@gmail.com>
2022-10-19 15:01:17 -0700

    more comments

--- src/hatecode/_0001_0999/_268MissingNumber.java
+++ src/hatecode/_0001_0999/_268MissingNumber.java
@@ -3,11 +3,6 @@ package hatecode._0001_0999;
 
 
 /**
- * Project Name : Leetcode
- * Package Name : leetcode
- * File Name : MissingNumber
- * Creator : professorX
- * Date : Aug, 2018
  * Description : 268. Missing Number
  */
 public class _268MissingNumber {
