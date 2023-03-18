39ab44fd197ef0f5f8360117606b7e61f4db071b
qiangddu@gmail.com <qiangddu@gmail.com>
2023-03-18 16:03:38 -0700

    more comments

--- src/hatecode/_0001_0999/_818RaceCar.java
+++ src/hatecode/_0001_0999/_818RaceCar.java
@@ -59,40 +59,46 @@ dp[i] = min(dp[i], forward + 1 + dp[2^forward - 1 - i])
 dp[i] = min(dp[i], forward + 1 + back + 1 + dp[i - ((2^forward-1)-(2^back-1))])
 
      */
-    public int racecar_DP(int target) {
+    public int racecar_DP(int e) {
         //处理边界
-        if (target <= 0) {
+        if (e <= 0) {
             return 0;
         }
 
-        int[] dp = new int[target + 1];
+        int[] dp = new int[e + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
 
-        for (int i = 1; i <= target; i++) {
+        for (int i = 1; i <= e; i++) {
             //先向前走 forward 步
-            for (int forward = 1; (1 << forward) - 1 < 2 * i; forward++) {
-                //向前走了forwardDistance
-                int forwardDistance = (1 << forward) - 1;
+            for (int fwdStep = 1; (1 << fwdStep) - 1 < 2 * i; fwdStep++) {
+                //fwdDis
+                int fwdDis = (1 << fwdStep) - 1;
                 //对应第一种情况，走了forward步直接到达i
-                if (forwardDistance == i) {
-                    dp[i] = forward;
-                } else if (forwardDistance > i) { //对应第二种情况，越过了i
+                if (fwdDis == i) {
+                    dp[i] = fwdStep;
+                } else if (fwdDis > i) { //对应第二种情况，越过了i
                     // +1 是因为回头需要一个R指令
                     dp[i] = Math.min(dp[i], 
-                            forward + 1 + dp[forwardDistance - i]);
+                            fwdStep + 1 + dp[fwdDis - i]);
                 } else { //对应第三种情况，没有越过i
                     //先回头走backward步
-                    for (int backward = 0; backward < forward; backward++) {
-                        int backwardDistance = (1 << backward) - 1;
+                    for (int backStep = 0; backStep < fwdStep; backStep++) {
+                        int backwardDis = (1 << backStep) - 1;
                         //第一个+1是还没到达i，先回头，使用一个R
                         //第二个+1是回头走了backwardDistance，再使用R回头走向i
+                        /*                           i
+                                                     |
+                         * ---------------------------------------
+                         *                  |
+                         *                 fwd
+                         */
                         dp[i] = Math.min(dp[i], 
-                                forward + 1 + backward + 1 + dp[i - forwardDistance + backwardDistance]);
+                                fwdStep + 1 + backStep + 1 + dp[i - fwdDis + backwardDis]);
                     }
                 }
             }
         }
-        return dp[target];
+        return dp[e];
     }
      
      public int racecar_DP_UpDown(int e) {
