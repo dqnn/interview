013c03fe0ec57974644f24677f25aa2f0119bf1a
qiangddu@gmail.com <qiangddu@gmail.com>
2023-05-07 23:13:00 -0700

    add more

--- src/hatecode/_2000_2999/_2672NumberOfAdjacentElementsWithTheSameColor.java
+++ src/hatecode/_2000_2999/_2672NumberOfAdjacentElementsWithTheSameColor.java
@@ -23,6 +23,18 @@ public class _2672NumberOfAdjacentElementsWithTheSameColor {
     Input: n = 4, queries = [[0,2],[1,2],[3,1],[1,1],[2,1]]
     Output: [0,1,1,0,2]
     */
+
+    /*
+     * thinking process: O(n)/O(n)
+     * 
+     * the problem is to say: given one integer array, intialized as all 0,  for each query
+     * [i,j], just A[i] = j, then count how many addjancet elements are the same as a pair.
+     * 
+     * [1,1,1] should have 2 
+     * 
+     *  just count each time if we change the value,  whether we increase or decrease, 
+     * just need to take care index = 0 and index = n - 1, because no one is precedent or after.
+     */
         public int[] colorTheArray(int n, int[][] queries) {
             if (n < 1) return new int[]{};
             
