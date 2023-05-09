e654436d82e5b174efed56f9a004f5365a4f5b7e
qiangddu@gmail.com <qiangddu@gmail.com>
2023-05-09 12:49:31 -0700

    add more

--- src/hatecode/_2000_2999/_2673MakeCostsOfPathsEqualInABinaryTree.java
+++ src/hatecode/_2000_2999/_2673MakeCostsOfPathsEqualInABinaryTree.java
@@ -19,6 +19,31 @@ public class _2673MakeCostsOfPathsEqualInABinaryTree {
     Input: n = 7, cost = [1,5,2,2,3,3,1]
     Output: 6
     */
+    /*
+     * thinking process: O(n)/O(lgN)
+     * 
+     * the problem is to say: given one perfect bianry tree, and one integer array, 
+     * integer array is each node cost starting from 1st index. 
+     * 
+     * to make sum from root to leaf node, make sure every path have same sum, you can add any integer to the node, 
+     * 
+     * return the min number of increment you have to make to make the sum of all path are the same
+     * 
+     * all tree can simplified to one tree as below:
+     * 
+     *          3 
+     *        /
+     *       1
+     *     /   \
+     *    5     4
+     * 
+     * since they have same parent, so the sum to 1 will be the same then difference will come from its child sum,
+     * here wile be |sum(left) - sum(right)|, so you will have to increment 4 here to have min increment, 
+     * 
+     * because on common path we will have min increment, 
+     *  
+     * when we back to node 3, we need max from "1" tree, so it will be max(left, right)
+     */
         
         int res = 0;
         public int minIncrements(int n, int[] A) {
@@ -29,10 +54,10 @@ public class _2673MakeCostsOfPathsEqualInABinaryTree {
         private int helper(int i, int[] A) {
             if (i >= A.length) return 0;
             
-            int a = helper(2* i+1, A), b = helper(2*i + 2, A);
+            int l = helper(2* i+1, A), r = helper(2*i + 2, A);
             
-            res += Math.abs(a -b);
+            res += Math.abs(l -r);
             
-            return A[i] + Math.max(a, b);
+            return A[i] + Math.max(l, r);
         } 
     }
