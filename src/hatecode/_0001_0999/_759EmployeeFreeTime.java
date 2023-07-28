a3f549415df7d4e3f6dcdf1a1cf2c4bcadcc64bc
dqdx <dqdxby@gmail.com>
2023-07-28 15:40:48 -0700

    rename

--- src/hatecode/_0001_0999/_759EmployeeFreeTime.java
+++ src/hatecode/_0001_0999/_759EmployeeFreeTime.java
@@ -14,7 +14,25 @@ import java.util.Queue;
  *     Interval(int s, int e) { start = s; end = e; }
  * }
  */
-class EmployeeFreeTime {
+public class _759EmployeeFreeTime {
+    /*
+759. Employee Free Time
+
+     * We are given a list schedule of employees, which represents the working time for each employee.
+
+Each employee has a list of non-overlapping Intervals, and these intervals are in sorted order.
+
+Return the list of finite intervals representing common, positive-length free time for all employees, also in sorted order.
+
+(Even though we are representing Intervals in the form [x, y], the objects inside are Intervals, not lists or arrays. For example, schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined).  Also, we wouldn't include intervals like [5, 5] in our answer, as they have zero length.
+
+ 
+
+Example 1:
+
+Input: schedule = [[[1,2],[5,6]],[[1,3]],[[4,10]]]
+Output: [[3,4]]
+     */
     //interview friendly but maybe the mid part will be asked to remove in interview, see
     //employeeFreeTime2 for the best answer
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
