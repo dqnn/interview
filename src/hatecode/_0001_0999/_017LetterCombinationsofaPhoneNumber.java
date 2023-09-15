b94b5baa97223bb5b05c9d695035e78a2bbc9a8e
dqdx <dqdxby@gmail.com>
2023-09-15 12:29:42 -0700

    more comments

--- src/hatecode/_0001_0999/_017LetterCombinationsofaPhoneNumber.java
+++ src/hatecode/_0001_0999/_017LetterCombinationsofaPhoneNumber.java
@@ -3,10 +3,12 @@ package hatecode._0001_0999;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 /**
  * Project Name : Leetcode
@@ -77,15 +79,18 @@ public class _017LetterCombinationsofaPhoneNumber {
         if (digits == null || digits.length() < 1) {
             return res;
         }
-        Map<String, List<String>> map = new HashMap<>();
-        map.put("2", new ArrayList<>(Arrays.asList("a", "b", "c")));
-        map.put("3", new ArrayList<>(Arrays.asList("d", "e", "f")));
-        map.put("4", new ArrayList<>(Arrays.asList("g", "h", "i")));
-        map.put("5", new ArrayList<>(Arrays.asList("j", "k", "l")));
-        map.put("6", new ArrayList<>(Arrays.asList("m", "n", "o")));
-        map.put("7", new ArrayList<>(Arrays.asList("p", "q", "r", "s")));
-        map.put("8", new ArrayList<>(Arrays.asList("t", "u", "v")));
-        map.put("9", new ArrayList<>(Arrays.asList("w", "x", "y", "z")));
+        Map<String, List<String>> map = new HashMap<>() {
+            {
+                put("2", new ArrayList<>(Arrays.asList("a", "b", "c")));
+                put("3", new ArrayList<>(Arrays.asList("d", "e", "f")));
+                put("4", new ArrayList<>(Arrays.asList("g", "h", "i")));
+                put("5", new ArrayList<>(Arrays.asList("j", "k", "l")));
+                put("6", new ArrayList<>(Arrays.asList("m", "n", "o")));
+                put("7", new ArrayList<>(Arrays.asList("p", "q", "r", "s")));
+                put("8", new ArrayList<>(Arrays.asList("t", "u", "v")));
+                put("9", new ArrayList<>(Arrays.asList("w", "x", "y", "z")));
+            }
+        };
         
         helper(res, new StringBuilder(), digits, map, 0);
             
@@ -101,6 +106,40 @@ public class _017LetterCombinationsofaPhoneNumber {
             sb.append(c);
             helper(res, sb, str, map, pos+1);
             sb.deleteCharAt(sb.length() -1);
+        }
+    }
+
+
+    //the 3rd vesion without removing elements in list
+    public List<String> letterCombinations_v3(String s) {
+        if(s == null || s.length() < 1) return new ArrayList<>();
+        
+        Map<Character, Set<Character>> map = new HashMap<>(){{
+            put('2', new HashSet<>(Arrays.asList('a', 'b', 'c')));  
+            put('3', new HashSet<>(Arrays.asList('d', 'e', 'f')));  
+            put('4', new HashSet<>(Arrays.asList('g', 'h', 'i')));  
+            put('5', new HashSet<>(Arrays.asList('j', 'k', 'l')));  
+            put('6', new HashSet<>(Arrays.asList('m', 'n', 'o')));  
+            put('7', new HashSet<>(Arrays.asList('p', 'q', 'r', 's')));  
+            put('8', new HashSet<>(Arrays.asList('t', 'u', 'v')));  
+            put('9', new HashSet<>(Arrays.asList('w', 'x', 'y', 'z')));            
+        }};
+        
+        List<String> res = new ArrayList<>();
+        helper(s, 0, map, res, "");
+        
+        return res;
+    }
+    
+    
+    private void helper(String s, int pos, Map<Character, Set<Character>> map, List<String> res, String cur) {
+        if (pos == s.length()) {
+            res.add(cur);
+            return;
+        }
+        
+        for(char next: map.get(s.charAt(pos))) {
+            helper(s, pos+1, map, res, cur+next);
         }
     }
 
