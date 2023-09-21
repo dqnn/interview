4be19da52b59727f125b48d8d074877a3160ac97
dqdx <dqdxby@gmail.com>
2023-09-21 15:39:04 -0700

    add more comments

--- src/hatecode/_0001_0999/_739DailyTemperatures.java
+++ src/hatecode/_0001_0999/_739DailyTemperatures.java
@@ -19,7 +19,7 @@ T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be
 
 */
     //interview friendly
-    //should remember how we handle the two dimension, value and index
+    //should remembe r how we handle the two dimension, value and index
     //thinking process:
     
     //given an integer array, each value means the temperature of i-th day, 
