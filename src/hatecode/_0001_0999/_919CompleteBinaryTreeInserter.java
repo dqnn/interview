13a02bbbd35b2d9afea72c97c91db72f33133c11
dqdx <dqdxby@gmail.com>
2023-09-26 22:45:32 -0700

    add more ocmments

--- src/hatecode/_0001_0999/_919CompleteBinaryTreeInserter.java
+++ src/hatecode/_0001_0999/_919CompleteBinaryTreeInserter.java
@@ -4,12 +4,14 @@ import java.util.*;
 public class _919CompleteBinaryTreeInserter {
 /*
  * 919. Complete Binary Tree Inserter
-A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes are as far left as possible.
+A complete binary tree is a binary tree in which every level, except possibly the last, 
+is completely filled, and all nodes are as far left as possible.
 
 Write a data structure CBTInserter that is initialized with a complete binary tree and supports the following operations:
 
 CBTInserter(TreeNode root) initializes the data structure on a given tree with head node root;
-CBTInserter.insert(int v) will insert a TreeNode into the tree with value node.val = v so that the tree remains complete, and returns the value of the parent of the inserted TreeNode;
+CBTInserter.insert(int v) will insert a TreeNode into the tree with value node.val = v so that the tree remains complete, 
+and returns the value of the parent of the inserted TreeNode;
 CBTInserter.get_root() will return the head node of the tree.
  
 
@@ -18,6 +20,13 @@ Example 1:
 Input: inputs = ["CBTInserter","insert","get_root"], inputs = [[[1]],[2],[]]
 Output: [null,1,[1,2]]
  */
+
+ /*
+  * thinking process: O()
+
+
+
+  */
     TreeNode root;
     Queue<TreeNode> queue;
     public _919CompleteBinaryTreeInserter(TreeNode root) {
@@ -53,7 +62,6 @@ Output: [null,1,[1,2]]
         else {
             queue.poll();
             node.right= newNode;
-            
         }
 
         return node.val;
