419448b1014b9aa94ac69fe4e1a26ab075cd66f6
dqdx <dqdxby@gmail.com>
2023-10-02 11:03:44 -0700

    add more comments

--- src/hatecode/_2000_2999/_2289StepsToMakeArrayNondecreasing.java
+++ src/hatecode/_2000_2999/_2289StepsToMakeArrayNondecreasing.java
@@ -34,9 +34,22 @@ another good example: [1,5,4,1,2,3] -- > 3, note 5 eat 4 and 4 eat 1 at the same
  * next round start from left and the same process, return how many rounds needed to make A non-decreasing 
  * 
  * 
+ * [a, count], count means for number a, how many numbers are cmpacted by the stack most close biggest left side number
+ * [5,3,4,4,7,3,6,11,8,5,11]
+ * 
+ * [5, 0],[4, 3], 
+ * [11,1]
+ * 
+ * max =3å
+ * 
+ * so when we meet 7, all previous will be evicted, and we will start another round of it 
+ * 
+ * if [1,5,4,1,2,3]
+ * 
+ * 
  * 
  */
-    public int totalSteps(int[] A) {
+    public static int totalSteps(int[] A) {
         if(A == null || A.length < 1) return 0;
         
         Stack<int[]> st = new Stack<>();
@@ -52,5 +65,10 @@ another good example: [1,5,4,1,2,3] -- > 3, note 5 eat 4 and 4 eat 1 at the same
         }
         
         return max;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(totalSteps(new int[]{5,3,4,4,7,3,6,11,8,5,11}));
     }
 }
