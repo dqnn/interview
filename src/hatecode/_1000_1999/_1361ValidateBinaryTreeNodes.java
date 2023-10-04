88fdc1fcea12c08188185bbcbec08a1d0d5f18c5
dqdx <dqdxby@gmail.com>
2023-10-04 11:15:36 -0700

    more comments

--- src/hatecode/_1000_1999/_1361ValidateBinaryTreeNodes.java
+++ src/hatecode/_1000_1999/_1361ValidateBinaryTreeNodes.java
@@ -18,57 +18,89 @@ Note that the nodes have no values and that we only use the node numbers in this
          \  /
           3
 */
-    //thinking process: O(n)/O(n)
+    //thinking process: O(n)/O(n), interview friendly
     //the problem is to say: given n nodes,0 - n-1, two array l and r which means
-    //l[i]m r[i] means its left and right child
-    
+    //l[i] r[i] means its left and right child
+    /*
+     *  you need to validate the tree and return true or false
+     * 
+     * this is directed graph, so we should think about indegree
+     * 
+     *  there are 3 key points in the problem:
+     * 1. in binary tree, every node should have indegree = 1 except root, root is 0
+     * 2. there should be only 1 binary tree, no forest there
+     * 3. no circle in the binary tree
+     * 
+     * 
+     * we have 3 steps to validate
+     * 1. indgree all smaller than 1
+     * 2. find root, root should not be -1 
+     * 3, start from root, count how many nodes in the tree, so there is no forest 
+     * 
+     * 
+     * step 1 and 2 will make sure there is no circle, also step can partially help if there are multiple forests 
+     * step 3 will make sure there is no forest
+     * 
+     * some edge cases:
+     * a.   1 <----> 0 
+     *   here in =[1, 1], every node will have 1, pass check 1, will fail in check 2, root will be -1
+     * 
+     * b.  
+     *      1           0
+     *     /             \
+     *    2               3
+     *  it will pass 1 since 2 trees, each one is legit binary tree
+     * 
+     *  c.
+     * 1<--->0,    2--->3
+     * 
+     * it will pass 1 and 2 since it will only have 1 root (2), but it will be caught in step 3
+     * 
+     * so we have to have 3 steps
+     */
     
     public boolean validateBinaryTreeNodes(int n, int[] l, int[] r) {
+        if(n <=1) return true;
+        
         int[] in = new int[n];
-        // check nodes has more than 1 in degree, but we do not check root
-        /*
-        4
-[1,-1,3,-1]
-[2,-1,-1,-1]
-        */
-        for(int i = 0; i< n; i++) {
-            //
-           if(l[i] != - 1 && in[l[i]]++ == 1) { return false; }
-           if(r[i] != - 1 && in[r[i]]++ == 1)  { return false; }
-            
+        for(int i = 0;i<n; i++) {
+            if(l[i] != -1) in[l[i]]++;
+            if(r[i] != -1) in[r[i]]++;
         }
         
-        //check we have forests
-        /*
-        6
-[1,-1,-1,4,-1,-1]
-[2,-1,-1,5,-1,-1]
-
-2
-[1,0]
-[-1,-1]
-
-        */
+        for(int i = 0; i<n; i++){
+            if(in[i] > 1) return false;
+        }
         
         int root = -1;
-        for (int i = 0; i < n; i++) {
-            if (in[i] == 0) {
-                if (root != -1) return false; // Multiple root
-                root = i;
+        for(int i = 0; i<n; i++) {
+            if(in[i] == 0) {
+                if(root == -1) {
+                    root = i;
+                } else return false;
             }
         }
         
         
+        if(root == -1) return false;
+        
         Set<Integer> visited = new HashSet<>();
-        return helper(visited, 0, n, l, r);
+        visited.add(root);
+        helper(root, l, r, visited);
+        
+        return visited.size() == n;
     }
     
-    private boolean helper(Set<Integer> visited, int pos, int n, int[] l, int[] r) {
-        if (pos ==-1 ) return true;
-        
-        if (visited.contains(pos)) return false;
-        visited.add(pos);
-        //System.out.println(visited);
-        return helper(visited, l[pos], n, l, r) && helper(visited, r[pos], n, l, r);
+    private void helper(int s, int[] l, int[] r, Set<Integer> visited) {
+        if(s == -1) return;
+        int next = l[s];
+        if (next != -1 && !visited.contains(next)) {
+            visited.add(next);
+            helper(next,l, r, visited);
+        }
+        next = r[s];
+        if (next != -1 && !visited.contains(next)) {
+            visited.add(next);
+            helper(next,l, r, visited);
+        }
     }
-}
