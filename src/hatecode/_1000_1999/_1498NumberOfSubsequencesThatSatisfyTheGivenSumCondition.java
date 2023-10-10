0af3918a4bdad402f9bcfa9d4af954124e02528b
dqdx <dqdxby@gmail.com>
2023-10-09 23:41:57 -0700

    more comments

--- src/hatecode/_1000_1999/_1498NumberOfSubsequencesThatSatisfyTheGivenSumCondition.java
+++ src/hatecode/_1000_1999/_1498NumberOfSubsequencesThatSatisfyTheGivenSumCondition.java
@@ -23,14 +23,12 @@ Explanation: There are 4 subsequences that satisfy the condition.
     /*
      * thinking process: O(nlng)/O(n)
      * 
-     * 1. order does not matter for this problem becuase we only care about 
-     * min and max, 
-     * take A[i] as the minimum element in one subsequence, 
-     * suppose the number of all other elements that is <= target - A[i] 
-     * is k, then the total number subsequences these elements makes is `2^k`. 
-     * it is obvious that we only need to find the number `k`,
-     *  we don;t really need to know how those `k` elements sits together 
-     *  with A[i].
+     * there are two keys in the problem;
+     * 
+     * 1. sort won't bother the results, because we only care subsequence min and max, so it equals to any elements set 
+     * 2. [a, b,c,d,e] sorted, a + e <= target, then every sequence with a will be a valid sequence,  for example
+     * [a], [a, b], [a,b,c], [a, c] it will be 2^4 = 16 sequences, a is always included 
+     * 3. for java, it we use Power, it may have issues on overflow, so we have to preprocess 
      *  
      *  For example, [3,5,6,7], target = 9 is like:
 [3]
@@ -38,14 +36,15 @@ Explanation: There are 4 subsequences that satisfy the condition.
 [3,5,6]
 [3,6]
 
-A different order such as [7,6,3,5], target = 9 will be like:
+A different order such as [5,7,6,3], target = 9 will be like:
 [3]
 [3,5]
 [6,3,5]
 [6,3]
 
     */
-    //pow is used to handle 1e9
+    //thinking process: O(nlgn)/O(n)
+    //pow is used to handle 1e9 mod issues, if we use Math.pow(2, r-l), there will be overflow
     public int numSubseq(int[] A, int target) {
         Arrays.sort(A);
         
@@ -54,11 +53,12 @@ A different order such as [7,6,3,5], target = 9 will be like:
         int res = 0; 
         int[] pows = new int[A.length];
         pows[0] = 1;
-        for (int i = 1 ; i < A.length ; ++i)
-            pows[i] = pows[i - 1] * 2 % mod;
+        for (int i = 1 ; i < A.length ; ++i) pows[i] = pows[i - 1] * 2 % mod;
+        //l = r need count
         while(l <= r) {
             if (A[l] + A[r] > target) r--;
             else {
+                // first element will always included, so no + 1
                 res = (res + pows[r-l]) % mod;
                 l++;
             }
