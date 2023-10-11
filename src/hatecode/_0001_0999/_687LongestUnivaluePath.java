f9e29da73b526b37157b5e7303042b1b09d392fc
dqdx <dqdxby@gmail.com>
2023-10-11 13:58:25 -0700

    add more ocmments

--- src/hatecode/_0001_0999/_687LongestUnivaluePath.java
+++ src/hatecode/_0001_0999/_687LongestUnivaluePath.java
@@ -14,12 +14,23 @@ Input:
              / \
             4   5
            / \   \
-          1   1   5
+          4   4   5
 Output:
 
 2
 */
-    //the same longest path, biggest sum, templates code
+    /*
+     * interview friendly: O(n)/O(n)
+     * 
+     * this problem is tricky because it only count edges, one naive way is to build the maps and 
+     * start from every nodes, that's doable but result in high complexity 
+     * 
+     * if we only thinking about traverse trees with post order, we know that root node have chance to know its sub nodes status, 
+     * so here we if we know its left, right child edges there, 
+     * 
+     * then in root node, if they are not equals, then we have mark left count as 0, res will be stored as it won't matter here 
+     * also same to right child 
+     */
     int max  = 0;
     public int longestUnivaluePath(TreeNode root) {
         helper(root);
@@ -40,6 +51,7 @@ Output:
             right = 0;
         }
         
+        //here we only count edges, not nodes,
         max = Math.max(max, left + right);
         return Math.max(left, right) + 1;
     }
