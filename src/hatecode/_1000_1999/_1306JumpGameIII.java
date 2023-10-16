e183b53c2917ec6fa8c4b5847393fe727ff69a6d
dqdx <dqdxby@gmail.com>
2023-10-16 10:31:16 -0700

    add more comments

--- src/hatecode/_1000_1999/_1306JumpGameIII.java
+++ src/hatecode/_1000_1999/_1306JumpGameIII.java
@@ -3,11 +3,10 @@ package hatecode._1000_1999;
 public class _1306JumpGameIII {
     /*
     1306. Jump Game III
-    Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+    Given an array of non-negative integers arr, you are initially positioned at start index of the array. 
+    When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
     
     Notice that you can not jump outside of the array at any time.
-    
-     
     
     Example 1:
     
