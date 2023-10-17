e0022ccf3e6cc560346832e26941eeb171e6f957
dqdx <dqdxby@gmail.com>
2023-10-17 12:05:41 -0700

    add more comments

--- src/hatecode/_0001_0999/_743NetworkDelayTime.java
+++ src/hatecode/_0001_0999/_743NetworkDelayTime.java
@@ -6,7 +6,8 @@ public class _743NetworkDelayTime {
 743. Network Delay Time
 There are N network nodes, labelled 1 to N.
 
-Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node, v is the target node, and w is the time it takes for a signal to travel from source to target.
+Given times, a list of travel times as directed edges times[i] = (u, v, w), where u is the source node, v is the target node, 
+and w is the time it takes for a signal to travel from source to target.
 
 Now, we send a signal from a certain node K. How long will it take for all nodes to receive the signal? If it is impossible, return -1.
 
