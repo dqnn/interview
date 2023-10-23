f01b73cb64741b108312cb6dadbc2d27ae165acb
dqdx <dqdxby@gmail.com>
2023-10-23 11:02:27 -0700

    more comments

--- src/hatecode/_0001_0999/_490TheMaze.java
+++ src/hatecode/_0001_0999/_490TheMaze.java
@@ -75,26 +75,26 @@ Output: true
     }
     
     //BFS, the key is use while to implement rolling 
-    public boolean hasPath2(int[][] maze, int[] start, int[] destination) {
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
+    public boolean hasPath2(int[][] A, int[] s, int[] e) {
+        boolean[][] visited = new boolean[A.length][A[0].length];
         int[][] dirs={{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
         Queue < int[] > queue = new LinkedList < > ();
-        queue.add(start);
-        visited[start[0]][start[1]] = true;
+        queue.add(s);
+        visited[s[0]][s[1]] = true;
         while (!queue.isEmpty()) {
-            int[] s = queue.remove();
-            if (s[0] == destination[0] && s[1] == destination[1])
+            int[] tmp = queue.remove();
+            if (tmp[0] == e[0] && tmp[1] == e[1])
                 return true;
-            for (int[] dir: dirs) {
-                int x = s[0] + dir[0];
-                int y = s[1] + dir[1];
-                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
-                    x += dir[0];
-                    y += dir[1];
+            for (int[] d: dirs) {
+                int x = tmp[0] + d[0];
+                int y = tmp[1] + d[1];
+                while (x >= 0 && y >= 0 && x < A.length && y < A[0].length && A[x][y] == 0) {
+                    x += d[0];
+                    y += d[1];
                 }
-                if (!visited[x - dir[0]][y - dir[1]]) {
-                    queue.add(new int[] {x - dir[0], y - dir[1]});
-                    visited[x - dir[0]][y - dir[1]] = true;
+                if (!visited[x - d[0]][y - d[1]]) {
+                    queue.add(new int[] {x - d[0], y - d[1]});
+                    visited[x - d[0]][y - d[1]] = true;
                 }
             }
         }
