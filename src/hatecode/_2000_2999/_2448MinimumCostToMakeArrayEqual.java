2ad2b29299355dadc78ef91db6198a251f2001bc
dqdx <dqdxby@gmail.com>
2023-11-19 18:57:59 -0800

    add more comments

--- src/hatecode/_2000_2999/_2448MinimumCostToMakeArrayEqual.java
+++ src/hatecode/_2000_2999/_2448MinimumCostToMakeArrayEqual.java
@@ -21,6 +21,12 @@ public class _2448MinimumCostToMakeArrayEqual {
     
     Input: nums = [1,3,5,2], cost = [2,3,1,14]
     Output: 8
+    Explanation: We can make all the elements equal to 2 in the following way:
+    - Increase the 0th element one time. The cost is 2.
+    - Decrease the 1st element one time. The cost is 3.
+    - Decrease the 2nd element three times. The cost is 1 + 1 + 1 = 3.
+    The total cost is 2 + 3 + 3 = 8.
+    It can be shown that we cannot make the array equal with a smaller cost.
     */
 
         /*
